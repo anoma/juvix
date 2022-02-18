df39676db8577abf3d8bc28538f407ee4002ef46
Jan Mas Rovira <janmasrovira@gmail.com>
2022-02-18 16:02:40 +0100

    remove unused file

--- src/MiniJuvix/Syntax/Concrete/BindingSite.hs
+++
@@ -1,8 +0,0 @@
-module MiniJuvix.Syntax.Concrete.BindingSite where
-
-import MiniJuvix.Prelude
-import MiniJuvix.Syntax.Concrete.Language
-
-data BindingSite (s :: Stage) =
-  BindConstructorDef (InductiveConstructorDef s)
-  | BindPattern (PatternType s)
