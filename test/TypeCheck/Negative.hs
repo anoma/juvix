615f7d11c03e6dcb11a8bde4269e4b15df7d3a5e
Paul Cadman <git@paulcadman.dev>
2022-03-29 18:01:19 +0100

    [test] Adds negative typecheck test for constructor

---
+++ test/TypeCheck/Negative.hs
@@ -0,0 +1,48 @@
+module TypeCheck.Negative (allTests) where
+
+import Base
+import MiniJuvix.Syntax.MicroJuvix.Error
+import qualified MiniJuvix.Syntax.MicroJuvix.TypeChecker as T
+import qualified MiniJuvix.Translation.AbstractToMicroJuvix as A
+
+type FailMsg = String
+
+data NegTest = NegTest
+  { name :: String,
+    relDir :: FilePath,
+    file :: FilePath,
+    checkErr :: TypeCheckerError -> Maybe FailMsg }
+
+testDescr :: NegTest -> TestDescr
+testDescr NegTest {..} = TestDescr {
+  testName = name,
+  testRoot = root </> relDir,
+  testAssertion = Single $ do
+      p <- (parseModuleIO file)
+           >>= scopeModuleIO
+           >>= translateModuleIO
+           >>= (return . A.translateModule)
+           >>= (return . T.checkModule)
+
+      case p of
+        Left err -> whenJust (checkErr err) assertFailure
+        Right _ -> assertFailure "The type checker did not find an error."
+  }
+
+allTests :: TestTree
+allTests = testGroup "TypeCheck negative tests"
+  (map (mkTest . testDescr) tests)
+
+root :: FilePath
+root = "tests/negative"
+
+wrongError :: Maybe FailMsg
+wrongError = Just "Incorrect error"
+
+tests :: [NegTest]
+tests = [
+  NegTest "Constructor in pattern type error" "MicroJuvix"
+    "PatternConstructor.mjuvix" $ \case
+      ErrWrongConstructorType {} -> Nothing
+      _ -> wrongError
+        ]
