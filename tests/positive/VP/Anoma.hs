721ea4612da7f9e334c21aa59f41018576058c97
Paul Cadman <git@paulcadman.dev>
2022-04-08 16:45:19 +0100

    [test] Add Anoma Haskell backend for VP

---
+++ tests/positive/VP/Anoma.hs
@@ -0,0 +1,16 @@
+module Anoma where
+
+import Prelude
+
+readPre :: String -> Int
+readPre "change1-key" = 100
+readPre "change2-key" = 90
+readPre _ = -1
+
+readPost :: String -> Int
+readPost "change1-key" = 90
+readPost "change2-key" = 100
+readPost _ = -1
+
+isBalanceKey :: String -> String -> String
+isBalanceKey _ _ = "owner-address"
