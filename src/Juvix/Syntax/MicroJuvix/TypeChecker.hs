14ad43ed3202d22012d894bc88555a6e43d74613
Jonathan Cubides <jonathan.cubides@uib.no>
2022-07-19 12:20:48 +0200

    Add more test and revisions

--- src/Juvix/Syntax/MicroJuvix/TypeChecker.hs
+++ src/Juvix/Syntax/MicroJuvix/TypeChecker.hs
@@ -284,6 +284,103 @@ checkConstructorReturnType indType ctor = do
         )
     )
 
+
+type ErrorReference = Maybe Expression
+
+type RecursionLimit = Int
+
+checkStrictlyPositiveOccurrences ::
+  Members '[Reader InfoTable, Error TypeCheckerError, State NegativeTypeParameters] r =>
+  InductiveDef ->
+  ConstrName ->
+  Name ->
+  RecursionLimit ->
+  ErrorReference ->
+  Expression ->
+  Sem r ()
+checkStrictlyPositiveOccurrences ty ctorName name recLimit ref = helper False
+  where
+    indName :: Name
+    indName = ty ^. inductiveName
+    -- The `helper` function determines if there is any negative occurence of
+    -- the symbol `name` in the given expression. The `inside` flag used below
+    -- indicates whether the current search is performed on the left of an inner
+    -- arrow or not.
+    helper ::
+      Members '[Reader InfoTable, Error TypeCheckerError, State NegativeTypeParameters] r =>
+      Bool ->
+      Expression ->
+      Sem r ()
+
+    helper inside expr = case expr of
+      ExpressionIden (IdenInductive ty') -> when (inside && name == ty') (strictlyPositivityError expr)
+      ExpressionIden (IdenVar name') ->
+        when inside $
+          if
+              | name == name' -> strictlyPositivityError expr
+              | InductiveParameter name' `elem` ty ^. inductiveParameters -> modify (HashSet.insert name')
+              | otherwise -> return ()
+      ExpressionFunction (Function l r) -> helper True (l ^. paramType) >> helper False r
+      ExpressionApplication tyApp -> do
+        let (hdExpr, args) = unfoldApplication tyApp
+        case hdExpr of
+          ExpressionIden (IdenInductive ty') -> do
+            when (inside && name == ty') (strictlyPositivityError expr)
+            InductiveInfo indTy' <- lookupInductive ty'
+
+            -- We now need to know whether `name` negatively occurs at `indTy'`
+            -- or not. The way to know is by checking that the type ty'
+            -- preserves the positivity condition, i.e., its type parameters
+            -- are no negative.
+
+            unless (indTy' ^. inductiveNoPositivity) $
+              let paramsTy' = indTy' ^. inductiveParameters
+                  go ::
+                    Members '[Reader InfoTable, Error TypeCheckerError, State NegativeTypeParameters] r =>
+                    InductiveDef ->
+                    [(InductiveParameter, Expression)] ->
+                    Sem r ()
+                  go typ = \case
+                    [] -> return ()
+                    ((InductiveParameter pName, arg) : ps) -> do
+                      negParms :: NegativeTypeParameters <- get
+                      when
+                        (nameInExpression name arg)
+                        ( do
+                            when (HashSet.member pName negParms) (strictlyPositivityError arg)
+                            when
+                              (recLimit > 0)
+                              ( forM_ (typ ^. inductiveConstructors) $ \ctor' ->
+                                  do
+                                    mapM_
+                                      ( checkStrictlyPositiveOccurrences
+                                          typ
+                                          (ctor' ^. inductiveConstructorName)
+                                          pName
+                                          (recLimit - 1)
+                                          (Just (fromMaybe arg ref))
+                                      )
+                                      (ctor' ^. inductiveConstructorParameters)
+                                    >> modify (HashSet.insert pName)
+                              )
+                        )
+                      go ty ps
+               in go indTy' (zip paramsTy' (toList args))
+          _ -> return ()
+      _ -> return ()
+
+    strictlyPositivityError :: Members '[Error TypeCheckerError] r => Expression -> Sem r ()
+    strictlyPositivityError expr = do
+      let errLoc = fromMaybe expr ref
+      throw
+        ( ErrNoStrictPositivity $
+            NoStrictPositivity
+              { _noStrictPositivityType = indName,
+                _noStrictPositivityConstructor = ctorName,
+                _noStrictPositivityArgument = errLoc
+              }
+        )
+
 -------------------------------------------------------------------------------
 
 inferExpression ::
