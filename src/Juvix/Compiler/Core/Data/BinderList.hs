e61f33191e220c4d275b6ba0dd27d7252342abec
Jan Mas Rovira <janmasrovira@gmail.com>
2022-09-06 09:19:23 +0200

    implement some instances for BinderList

--- src/Juvix/Compiler/Core/Data/BinderList.hs
+++ src/Juvix/Compiler/Core/Data/BinderList.hs
@@ -1,7 +1,6 @@
 module Juvix.Compiler.Core.Data.BinderList where
 
 import Data.HashMap.Strict qualified as HashMap
-import Data.List qualified as List
 import Juvix.Compiler.Core.Language.Base
 
 data BinderList a = BinderList
@@ -12,25 +11,49 @@ data BinderList a = BinderList
 makeLenses ''BinderList
 
 fromList :: [a] -> BinderList a
-fromList l = BinderList (List.length l) (HashMap.fromList (zip [0 ..] l))
+fromList l = BinderList (length l) (HashMap.fromList (zip [0 ..] l))
 
-toList :: BinderList a -> [a]
-toList bl =
-  List.map snd $
-    sortBy (\x y -> compare (fst y) (fst x)) $
-      HashMap.toList (bl ^. blMap)
+instance Foldable BinderList where
+  foldr :: (a -> b -> b) -> b -> BinderList a -> b
+  foldr f acc bl = foldr f acc (toList bl)
 
-empty :: BinderList a
-empty = BinderList 0 mempty
+  null :: BinderList a -> Bool
+  null = (== 0) . (^. blLength)
 
-length :: BinderList a -> Int
-length = (^. blLength)
+  length :: BinderList a -> Int
+  length = (^. blLength)
+
+  toList :: BinderList a -> [a]
+  toList bl =
+    map snd $
+      sortBy (compare `on` fst) $
+        HashMap.toList (bl ^. blMap)
 
 lookup :: Index -> BinderList a -> a
 lookup idx bl =
-  fromMaybe
-    (error "invalid binder lookup")
-    (HashMap.lookup (bl ^. blLength - 1 - idx) (bl ^. blMap))
+  fromMaybe err (HashMap.lookup target (bl ^. blMap))
+  where
+    target = bl ^. blLength - 1 - idx
+    err :: a
+    err =
+      error
+        ( "invalid binder lookup. Got index "
+            <> show idx
+            <> " that targets "
+            <> show target
+            <> " and the length is "
+            <> show (bl ^. blLength)
+        )
+
+instance Semigroup (BinderList a) where
+  a <> b = prepend (toList a) b
+
+instance Monoid (BinderList a) where
+  mempty =
+    BinderList
+      { _blLength = 0,
+        _blMap = mempty
+      }
 
 extend :: a -> BinderList a -> BinderList a
 extend a bl =
@@ -38,8 +61,9 @@ extend a bl =
     (bl ^. blLength + 1)
     (HashMap.insert (bl ^. blLength) a (bl ^. blMap))
 
-map :: (a -> b) -> BinderList a -> BinderList b
-map f bl = bl {_blMap = HashMap.map f (bl ^. blMap)}
+instance Functor BinderList where
+  fmap :: (a -> b) -> BinderList a -> BinderList b
+  fmap f = over blMap (fmap f)
 
 prepend :: [a] -> BinderList a -> BinderList a
 prepend l bl = foldr extend bl l
