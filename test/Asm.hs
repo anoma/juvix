c3e5846a08b9dc9bd85a5f231ba41acc09875e6f
Lukasz Czajka <lukasz@heliax.dev>
2022-09-20 18:19:05 +0200

    validate negative tests

--- test/Asm.hs
+++ test/Asm.hs
@@ -1,7 +1,8 @@
 module Asm where
 
 import Asm.Run qualified as Run
+import Asm.Validate qualified as Validate
 import Base
 
 allTests :: TestTree
-allTests = testGroup "JuvixAsm tests" [Run.allTests]
+allTests = testGroup "JuvixAsm tests" [Validate.allTests, Run.allTests]
---
+++ test/Asm/Validate/Negative.hs
@@ -0,0 +1,53 @@
+module Asm.Validate.Negative where
+
+
+import Base
+import Asm.Validate.Base
+
+data NegTest = NegTest
+  { _name :: String,
+    _relDir :: FilePath,
+    _file :: FilePath
+  }
+
+root :: FilePath
+root = "tests/Asm/negative"
+
+testDescr :: NegTest -> TestDescr
+testDescr NegTest {..} =
+  let tRoot = root </> _relDir
+   in TestDescr
+        { _testName = _name,
+          _testRoot = tRoot,
+          _testAssertion = Steps $ asmValidateErrorAssertion _file
+        }
+
+allTests :: TestTree
+allTests =
+  testGroup
+    "JuvixAsm validate negative tests"
+    (map (mkTest . testDescr) tests)
+
+tests :: [NegTest]
+tests =
+  [ NegTest
+      "Wrong stack height on exit"
+      "."
+      "vtest001.jva",
+    NegTest
+      "Arithmetic type mismatch"
+      "."
+      "vtest002.jva",
+    NegTest
+      "Function type mismatch"
+      "."
+      "vtest003.jva",
+    NegTest
+      "Not enough function arguments"
+      "."
+      "vtest004.jva",
+    NegTest
+      "Missing return"
+      "."
+      "vtest005.jva"
+  ]
