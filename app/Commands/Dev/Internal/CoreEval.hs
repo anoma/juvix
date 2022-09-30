4e32401bef67b770b46c5aa538ca0e5d1a73cc89
Paul Cadman <git@paulcadman.dev>
2022-09-30 17:34:56 +1000

    Apply an hlint fix

--- app/Commands/Dev/Internal/CoreEval.hs
+++ app/Commands/Dev/Internal/CoreEval.hs
@@ -10,6 +10,4 @@ import Juvix.Compiler.Core.Translation
 runCommand :: Members '[Embed IO, App] r => InternalCoreEvalOptions -> Sem r ()
 runCommand localOpts = do
   tab <- (^. coreResultTable) <$> runPipeline (localOpts ^. internalCoreEvalInputFile) upToCore
-  case (tab ^. infoMain) >>= ((tab ^. identContext) HashMap.!?) of
-    Just node -> evalNode localOpts tab node
-    Nothing -> return ()
+  forM_ ((tab ^. infoMain) >>= ((tab ^. identContext) HashMap.!?)) (evalNode localOpts tab)
