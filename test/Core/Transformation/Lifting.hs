afe19ac10847833a1ea1699883b17c791ab79866
Jan Mas Rovira <janmasrovira@gmail.com>
2022-10-03 16:35:08 +0200

    adapt lambda lifting tests to use evaluator

--- test/Core/Transformation/Lifting.hs
+++ test/Core/Transformation/Lifting.hs
@@ -6,36 +6,27 @@ import Core.Transformation.Base
 import Juvix.Compiler.Core.Transformation
 
 allTests :: TestTree
-allTests = testGroup "Lambda lifting" tests
+allTests = testGroup "Lambda lifting" (mapMaybe liftTest Eval.tests)
 
 pipe :: [TransformationId]
 pipe = [LambdaLifting]
 
-dir :: FilePath
-dir = "lambda-lifting"
-
-liftTest :: String -> FilePath -> FilePath -> TestTree
-liftTest _testName _testCoreFile _testExpectedFile =
-  fromTest
-    Test
-      { _testTransformations = pipe,
-        _testAssertion = assertExpectedOutput expectedFile,
-        _testEval =
-          Eval.PosTest
-            { _name = _testName,
-              _relDir = dir </> _testCoreFile,
-              _file = _testCoreFile,
-              _expectedFile = _testExpectedFile
+liftTest :: Eval.PosTest -> Maybe TestTree
+liftTest _testEval@Eval.PosTest {..}
+  | _name `elem` excluded = Nothing
+  | otherwise =
+      Just $
+        fromTest
+          Test
+            { _testTransformations = pipe,
+              _testAssertion = \i -> unless (isLifted i) (error ""),
+              _testEval
             }
-      }
-  where
-    expectedFile = dir </> _testExpectedFile
 
-tests :: [TestTree]
-tests =
-  [ liftTest
-      ("Lambda lifting without let rec " <> i)
-      ("test" <> i <> ".jvc")
-      ("test" <> i <> ".out")
-    | i <- map show [1 :: Int .. 3]
+excluded :: [String]
+excluded =
+  [ "Functions returning functions with variable capture",
+    "Higher-order recursive functions",
+    "Ackermann function (higher-order definition)",
+    "LetRec"
   ]
