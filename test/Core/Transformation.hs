65bf1f75ca93dc4a5739715bf3a4d1ea7e7acfcd
Jan Mas Rovira <janmasrovira@gmail.com>
2022-10-04 22:06:57 +0200

    add Identity transformation

--- test/Core/Transformation.hs
+++ test/Core/Transformation.hs
@@ -3,9 +3,13 @@ module Core.Transformation where
 import Base
 import Core.Transformation.Lifting qualified as Lifting
 import Core.Transformation.TopEtaExpand qualified as TopEtaExpand
+import Core.Transformation.Identity qualified as Identity
 
 allTests :: TestTree
 allTests =
   testGroup
     "JuvixCore transformations"
-    [Lifting.allTests, TopEtaExpand.allTests]
+    [Lifting.allTests,
+     TopEtaExpand.allTests,
+     Identity.allTests
+    ]
