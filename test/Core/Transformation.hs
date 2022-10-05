c47d6c91915e8ecea995ff68842f91057938eeaf
Jan Mas Rovira <janmasrovira@gmail.com>
2022-10-05 12:25:08 +0200

    remove topeta from branch

--- test/Core/Transformation.hs
+++ test/Core/Transformation.hs
@@ -3,13 +3,11 @@ module Core.Transformation where
 import Base
 import Core.Transformation.Identity qualified as Identity
 import Core.Transformation.Lifting qualified as Lifting
-import Core.Transformation.TopEtaExpand qualified as TopEtaExpand
 
 allTests :: TestTree
 allTests =
   testGroup
     "JuvixCore transformations"
     [ Lifting.allTests,
-      TopEtaExpand.allTests,
       Identity.allTests
     ]
