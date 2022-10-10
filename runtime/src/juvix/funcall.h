ec0777f1cd79ffe55dfc9479e16b64f9cd557d84
Lukasz Czajka <lukasz@heliax.dev>
2022-10-10 19:23:37 +0200

    TAIL_CALL_CLOSURES

--- runtime/src/juvix/funcall.h
+++ runtime/src/juvix/funcall.h
@@ -71,55 +71,77 @@
     STACK_PUSH(sp, argm);
     ...
     STACK_PUSH(sp, arg1);
-    CALL_CLOSURES(m, sp, mp, unique_label);
+    CALL_CLOSURES(m, unique_label);
     STACK_POP(sp, varn);
     ...
     STACK_POP(sp, var1);
     STACK_LEAVE(sp);
 */
 
-#define CALL_CLOSURES(m, sp, mp, label)           \
+#define CALL_CLOSURES(m, label)                   \
     do {                                          \
         SET_ARG(0, m);                            \
         juvix_ccl_return = GET_LABEL_ADDR(label); \
         goto juvix_ccl_start;                     \
     label:                                        \
+        --sp;                                     \
     } while (0)
 
-#define DECL_CALL_CLOSURES(sp, mp)                                             \
-    void *juvix_ccl_return;                                                    \
-    juvix_ccl_start:                                                           \
-    do {                                                                       \
-        size_t juvix_ccl_sargs = GET_ARG(0);                                   \
-    juvix_ccl_continue:                                                        \
-        while (juvix_ccl_sargs > 0) {                                          \
-            word_t juvix_ccl_closure;                                          \
-            STACK_POP(sp, juvix_ccl_closure);                                  \
-            size_t juvix_ccl_nargs = get_closure_nargs(juvix_ccl_closure);     \
-            size_t juvix_ccl_nfields = get_nfields(juvix_ccl_closure);         \
-            void *juvix_ccl_addr = get_closure_addr(juvix_ccl_closure);        \
-            size_t juvix_ccl_largs = juvix_ccl_nfields - juvix_ccl_nargs;      \
-            if (juvix_ccl_largs <= juvix_ccl_sargs) {                          \
-                STACK_PUSH(sp, GET_LABEL_ADDR(juvix_ccl_continue));            \
-                SET_ARG(0, juvix_ccl_addr);                                    \
-                juvix_ccl_sargs -= juvix_ccl_largs;                            \
-                STORED_GOTO(juvix_ccl_dispatch[juvix_ccl_largs]);              \
-            } else {                                                           \
-                EXTEND_CLOSURE(                                                \
-                    GET_ARG(0), juvix_ccl_closure, juvix_ccl_sargs, mp,        \
-                    {                                                          \
-                        STACK_SAVE(sp);                                        \
-                        STACK_PUSH(sp, juvix_ccl_closure);                     \
-                    },                                                         \
-                    { STACK_POP(sp, juvix_ccl_closure); });                    \
-                for (size_t juvix_idx = 0; juvix_idx < juvix_ccl_sargs;        \
-                     ++juvix_idx) {                                            \
-                    STACK_POP(sp, GET_FIELD(GET_ARG(0),                        \
-                                            juvix_idx + juvix_ccl_nargs + 3)); \
-                }                                                              \
-                STORED_GOTO(juvix_ccl_return);                                 \
-            }                                                                  \
-        }                                                                      \
+#define TAIL_CALL_CLOSURES(m)    \
+    do {                         \
+        SET_ARG(0, m);           \
+        juvix_ccl_return = NULL; \
+        goto juvix_ccl_start;    \
+    } while (0)
+
+#define DECL_CALL_CLOSURES(sp, mp)                                         \
+    void *juvix_ccl_return;                                                \
+    juvix_ccl_start:                                                       \
+    do {                                                                   \
+        size_t juvix_ccl_sargs = GET_ARG(0);                               \
+    juvix_ccl_continue:                                                    \
+        word_t juvix_ccl_closure;                                          \
+        STACK_POP(sp, juvix_ccl_closure);                                  \
+        size_t juvix_ccl_nargs = get_closure_nargs(juvix_ccl_closure);     \
+        size_t juvix_ccl_nfields = get_nfields(juvix_ccl_closure);         \
+        void *juvix_ccl_addr = get_closure_addr(juvix_ccl_closure);        \
+        size_t juvix_ccl_largs = juvix_ccl_nfields - juvix_ccl_nargs;      \
+        if (juvix_ccl_largs <= juvix_ccl_sargs) {                          \
+            SET_ARG(0, juvix_ccl_addr);                                    \
+            juvix_ccl_sargs -= juvix_ccl_largs;                            \
+            if (juvix_ccl_sargs > 0) {                                     \
+                STACK_PUSH(sp, GET_LABEL_ADDR(juvix_ccl_continue));        \
+            } else if (juvix_ccl_return != NULL) {                         \
+                STACK_PUSH(sp, juvix_ccl_return);                          \
+            }                                                              \
+            STORED_GOTO(juvix_ccl_dispatch[juvix_ccl_largs]);              \
+        } else {                                                           \
+            EXTEND_CLOSURE(                                                \
+                GET_ARG(0), juvix_ccl_closure, juvix_ccl_sargs, mp,        \
+                {                                                          \
+                    STACK_SAVE(sp);                                        \
+                    STACK_PUSH(sp, juvix_ccl_closure);                     \
+                },                                                         \
+                { STACK_POP(sp, juvix_ccl_closure); });                    \
+            for (size_t juvix_idx = 0; juvix_idx < juvix_ccl_sargs;        \
+                 ++juvix_idx) {                                            \
+                STACK_POP(sp, GET_FIELD(GET_ARG(0),                        \
+                                        juvix_idx + juvix_ccl_nargs + 3)); \
+            }                                                              \
+            break;                                                         \
+        }                                                                  \
+        if (juvix_ccl_return == NULL) {                                    \
+            RETURN(sp);                                                    \
+        } else {                                                           \
+            STORED_GOTO(juvix_ccl_return);                                 \
+        }                                                                  \
+    } while (0)
+
+#define RETURN(sp)                   \
+    do {                             \
+        word_t juvix_return;         \
+        STACK_POP(sp, juvix_return); \
+        STORED_GOTO(juvix_return);   \
     } while (0)
 
 #endif
