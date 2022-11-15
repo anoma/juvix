8a3136ae305f0a3d8b0051ecf5998123742d5d04
Lukasz Czajka <lukasz@heliax.dev>
2022-11-15 10:38:40 +0100

    fix builtin constructor UIDs

--- src/Juvix/Compiler/Backend/C/Translation/FromReg/Base.hs
+++ src/Juvix/Compiler/Backend/C/Translation/FromReg/Base.hs
@@ -10,8 +10,20 @@ import Juvix.Prelude
 getFunctionName :: Reg.ExtraInfo -> Reg.Symbol -> Text
 getFunctionName info sym = ((info ^. Reg.extraInfoTable . Reg.infoFunctions) HashMap.! sym) ^. Reg.functionName
 
+-- Make sure this corresponds with juvix/object/object.h
+getBuiltinUID :: Reg.BuiltinDataTag -> Int
+getBuiltinUID = \case
+  Reg.TagFalse -> 0
+  Reg.TagTrue -> 1
+  Reg.TagReturn -> 4
+  Reg.TagBind -> 5
+  Reg.TagWrite -> 6
+  Reg.TagReadLn -> 7
+
 getUID :: Reg.ExtraInfo -> Reg.Tag -> Int
-getUID info tag = fromJust $ HashMap.lookup tag (info ^. Reg.extraInfoUIDs)
+getUID info tag = case tag of
+  Reg.BuiltinTag builtin -> getBuiltinUID builtin
+  Reg.UserTag {} -> fromJust $ HashMap.lookup tag (info ^. Reg.extraInfoUIDs)
 
 getFUID :: Reg.ExtraInfo -> Reg.Symbol -> Int
 getFUID info sym = fromJust $ HashMap.lookup sym (info ^. Reg.extraInfoFUIDs)
