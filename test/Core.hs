a658871dce12d38846b76612bf074643400037b8
Lukasz Czajka <lukasz@heliax.dev>
2022-12-22 12:47:31 +0100

    compilation tests

--- test/Core.hs
+++ test/Core.hs
@@ -2,8 +2,9 @@ module Core where
 
 import Base
 import Core.Asm qualified as Asm
+import Core.Compile qualified as Compile
 import Core.Eval qualified as Eval
 import Core.Transformation qualified as Transformation
 
 allTests :: TestTree
-allTests = testGroup "JuvixCore tests" [Eval.allTests, Transformation.allTests, Asm.allTests]
+allTests = testGroup "JuvixCore tests" [Eval.allTests, Transformation.allTests, Asm.allTests, Compile.allTests]
