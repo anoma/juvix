61e7d475d9e2f947c66d53b0564506f30ac9281b
Jan Mas Rovira <janmasrovira@gmail.com>
2023-01-27 18:52:13 +0100

    format

--- src/Juvix/Compiler/Concrete/Translation/FromSource.hs
+++ src/Juvix/Compiler/Concrete/Translation/FromSource.hs
@@ -548,7 +548,8 @@ inductiveDef _inductiveBuiltin = do
     optional (kw kwColon >> parseExpressionAtoms)
       P.<?> "<type annotation e.g. ': Type'>"
   kw kwAssign P.<?> "<assignment symbol ':='>"
-  _inductiveConstructors <- pipeSep1 constructorDef
+  _inductiveConstructors <-
+    pipeSep1 constructorDef
       P.<?> "<constructor definition>"
   return InductiveDef {..}
 
