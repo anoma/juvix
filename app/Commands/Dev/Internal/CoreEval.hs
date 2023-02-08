7f128b1e6cf8a2c138239701f2b5885e4ac5271e
Paul Cadman <git@paulcadman.dev>
2023-02-08 11:35:11 +0000

    Add internal core-eval option to evaluate named function identifier
    By default the main function identifier is evaluated by `juvix dev
    internal core-eval`. When debugging it's often useful to evaluate some
    other function in a module.
    
    ```
    module Symbol;
    
    open import Stdlib.Prelude;
    
    f : Nat;
    f := 1;
    
    main : IO;
    main := printNatLn f;
    
    end;
    ```
    
    With core-eval you can now evaluate `f`:
    
    For example:
    ```
    $ juvix dev internal core-eval Symbol.juvix -s f
    suc zero
    ```

--- app/Commands/Dev/Internal/CoreEval.hs
+++ app/Commands/Dev/Internal/CoreEval.hs
@@ -2,14 +2,31 @@ module Commands.Dev.Internal.CoreEval where
 
 import Commands.Base
 import Commands.Dev.Internal.CoreEval.Options
+import Data.HashMap.Internal.Strict (elems)
 import Data.HashMap.Strict qualified as HashMap
 import Evaluator
 import Juvix.Compiler.Core.Data.InfoTable
+import Juvix.Compiler.Core.Language.Base
 import Juvix.Compiler.Core.Transformation qualified as Core
 import Juvix.Compiler.Core.Translation
+import Safe (headMay)
+
+getSymbol :: forall r. Member App r => InfoTable -> Text -> Sem r Symbol
+getSymbol tab name = maybe failAction (return . (^. identifierSymbol)) mIdent
+  where
+    identifiers :: [IdentifierInfo]
+    identifiers = elems (tab ^. infoIdentifiers)
+
+    mIdent :: Maybe IdentifierInfo
+    mIdent = headMay (filter ((== name) . (^. identifierName)) identifiers)
+
+    failAction :: Sem r a
+    failAction = exitMsg (ExitFailure 1) (name <> " is not a function identifier")
 
 runCommand :: (Members '[Embed IO, App] r) => InternalCoreEvalOptions -> Sem r ()
 runCommand localOpts = do
   tab <- (^. coreResultTable) <$> runPipeline (localOpts ^. internalCoreEvalInputFile) upToCore
   let tab' = Core.applyTransformations (project localOpts ^. internalCoreEvalTransformations) tab
-  forM_ ((tab' ^. infoMain) >>= ((tab' ^. identContext) HashMap.!?)) (evalAndPrint localOpts tab')
+  ms <- mapM (getSymbol tab') (localOpts ^. internalCoreEvalSymbolName)
+  let symbol = ms <|> (tab' ^. infoMain)
+  forM_ (symbol >>= ((tab' ^. identContext) HashMap.!?)) (evalAndPrint localOpts tab')
