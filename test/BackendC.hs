38b304c2fb6d4435eb4fceb8579e12fcf0c600eb
Lukasz Czajka <lukasz@heliax.dev>
2023-02-10 18:52:44 +0100

    remove BackendC tests

--- test/BackendC.hs
+++
@@ -1,8 +0,0 @@
-module BackendC where
-
-import BackendC.Examples qualified as E
-import BackendC.Positive qualified as P
-import Base
-
-allTests :: TestTree
-allTests = testGroup "Backend C tests" [P.allTests, E.allTests]
