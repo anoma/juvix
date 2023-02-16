be8b0bf3f21ce67a92ab4d2a7f0e50d08333a0fa
Lukasz Czajka <lukasz@heliax.dev>
2023-02-16 16:34:14 +0100

    apply EtaExpandApps to all nodes (including types in the InfoTable)

--- src/Juvix/Compiler/Core/Transformation/ConvertBuiltinTypes.hs
+++ src/Juvix/Compiler/Core/Transformation/ConvertBuiltinTypes.hs
@@ -22,31 +22,6 @@ convertNode tab = umap go
           ii = fromJust $ tab ^. infoInductives . at _typeConstrSymbol
       _ -> node
 
-convertIdent :: InfoTable -> IdentifierInfo -> IdentifierInfo
-convertIdent tab ii =
-  ii
-    { _identifierType = convertNode tab (ii ^. identifierType),
-      _identifierArgsInfo = map (over argumentType (convertNode tab)) (ii ^. identifierArgsInfo)
-    }
-
-convertConstructor :: InfoTable -> ConstructorInfo -> ConstructorInfo
-convertConstructor tab = over constructorType (convertNode tab)
-
-convertInductive :: InfoTable -> InductiveInfo -> InductiveInfo
-convertInductive tab ii =
-  ii
-    { _inductiveKind = convertNode tab (ii ^. inductiveKind),
-      _inductiveParams = map (over paramKind (convertNode tab)) (ii ^. inductiveParams),
-      _inductiveConstructors = map (convertConstructor tab) (ii ^. inductiveConstructors)
-    }
-
-convertAxiom :: InfoTable -> AxiomInfo -> AxiomInfo
-convertAxiom tab = over axiomType (convertNode tab)
-
 convertBuiltinTypes :: InfoTable -> InfoTable
 convertBuiltinTypes tab =
-  mapAxioms (convertAxiom tab) $
-    mapInductives (convertInductive tab) $
-      mapConstructors (convertConstructor tab) $
-        mapIdents (convertIdent tab) $
-          mapT (const (convertNode tab)) tab
+  mapAllNodes (convertNode tab) tab
