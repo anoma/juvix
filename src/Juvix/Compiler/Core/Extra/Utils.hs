f66e929a1fcb5f826b6e31e911bc0dab2621abcd
Lukasz Czajka <lukasz@heliax.dev>
2023-02-16 18:28:50 +0100

    squash apps in types

--- src/Juvix/Compiler/Core/Extra/Utils.hs
+++ src/Juvix/Compiler/Core/Extra/Utils.hs
@@ -185,6 +185,18 @@ convertClosures = umap go
 convertRuntimeNodes :: Node -> Node
 convertRuntimeNodes = convertClosures
 
+squashApps :: Node -> Node
+squashApps = dmap go
+  where
+    go :: Node -> Node
+    go n =
+      let (l, args) = unfoldApps' n
+       in case l of
+            NCtr (Constr i tag args') -> mkConstr i tag (args' ++ args)
+            NBlt (BuiltinApp i op args') -> mkBuiltinApp i op (args' ++ args)
+            NTyp (TypeConstr i sym args') -> mkTypeConstr i sym (args' ++ args)
+            _ -> n
+
 argumentInfoFromBinder :: Binder -> ArgumentInfo
 argumentInfoFromBinder i =
   ArgumentInfo
