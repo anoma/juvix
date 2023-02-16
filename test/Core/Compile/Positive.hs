c50089471c547bd466ac71a93fe0f1fcde8f68b6
Lukasz Czajka <lukasz@heliax.dev>
2023-02-16 17:36:52 +0100

    change the Core match test to use arithmetic modulo, avoiding big integers which don't fit into 31 bits

--- test/Core/Compile/Positive.hs
+++ test/Core/Compile/Positive.hs
@@ -7,7 +7,7 @@ import Core.Eval.Positive qualified as Eval
 allTests :: TestTree
 allTests = testGroup "JuvixCore compilation tests" (map liftTest (Eval.filterOutTests ignoredTests Eval.tests))
 
--- Arbitrary precision integers and general pattern matching not yet supported
+-- Arbitrary precision integers not yet supported
 ignoredTests :: [String]
 ignoredTests =
   [ "Tail recursion: Fibonacci numbers in linear time",
