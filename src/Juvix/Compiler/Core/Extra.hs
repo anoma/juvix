52c22579d2c6e16259c1bb4a595d47ceb7e6ea0b
Lukasz Czajka <lukasz@heliax.dev>
2023-02-20 12:53:02 +0100

    make ormolu happy

--- src/Juvix/Compiler/Core/Extra.hs
+++ src/Juvix/Compiler/Core/Extra.hs
@@ -1,8 +1,8 @@
 module Juvix.Compiler.Core.Extra
   ( module Juvix.Compiler.Core.Extra.Utils,
-    module Juvix.Compiler.Core.Extra.IdentDependencyInfo
+    module Juvix.Compiler.Core.Extra.IdentDependencyInfo,
   )
 where
 
-import Juvix.Compiler.Core.Extra.Utils
 import Juvix.Compiler.Core.Extra.IdentDependencyInfo
+import Juvix.Compiler.Core.Extra.Utils
--- src/Juvix/Compiler/Core/Extra/IdentDependencyInfo.hs
+++ src/Juvix/Compiler/Core/Extra/IdentDependencyInfo.hs
@@ -2,10 +2,10 @@ module Juvix.Compiler.Core.Extra.IdentDependencyInfo where
 
 import Data.HashMap.Strict qualified as HashMap
 import Data.HashSet qualified as HashSet
-import Juvix.Data.DependencyInfo
-import Juvix.Compiler.Core.Language
 import Juvix.Compiler.Core.Data.InfoTable
 import Juvix.Compiler.Core.Extra.Utils
+import Juvix.Compiler.Core.Language
+import Juvix.Data.DependencyInfo
 
 type IdentDependencyInfo = DependencyInfo Symbol
 
@@ -15,9 +15,10 @@ createIdentDependencyInfo tab = createDependencyInfo graph startVertices
     graph :: HashMap Symbol (HashSet Symbol)
     graph =
       fmap
-        (\IdentifierInfo {..} ->
-          HashSet.map (\Ident{..} -> _identSymbol) $
-            getIdents (fromJust $ HashMap.lookup _identifierSymbol (tab ^. identContext)))
+        ( \IdentifierInfo {..} ->
+            HashSet.map (\Ident {..} -> _identSymbol) $
+              getIdents (fromJust $ HashMap.lookup _identifierSymbol (tab ^. identContext))
+        )
         (tab ^. infoIdentifiers)
 
     startVertices :: HashSet Symbol
