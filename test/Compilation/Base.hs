9243ed8aff295779a1d6a5a589667e7b3fe5be22
Lukasz Czajka <lukasz@heliax.dev>
2023-02-22 12:50:00 +0100

    fix tests with big integers

--- test/Compilation/Base.hs
+++ test/Compilation/Base.hs
@@ -9,14 +9,16 @@ import Juvix.Compiler.Core.Translation.FromInternal.Data qualified as Core
 import Juvix.Compiler.Pipeline
 
 compileAssertion ::
+  Bool ->
   Path Abs File ->
   Path Abs File ->
   (String -> IO ()) ->
   Assertion
-compileAssertion mainFile expectedFile step = do
+compileAssertion onlyEval mainFile expectedFile step = do
   step "Translate to JuvixCore"
   cwd <- getCurrentDir
   let entryPoint = defaultEntryPoint cwd mainFile
   tab <- (^. Core.coreResultTable) . snd <$> runIO' iniState entryPoint upToCore
   coreEvalAssertion' (Core.toEval tab) mainFile expectedFile step
-  coreCompileAssertion' tab mainFile expectedFile step
+  unless onlyEval $
+    coreCompileAssertion' tab mainFile expectedFile step
