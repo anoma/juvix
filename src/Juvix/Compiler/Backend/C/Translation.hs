d0cdc005a2eea5f76683fbcbbec353ceee399b8e
Lukasz Czajka <lukasz@heliax.dev>
2023-02-23 15:38:05 +0100

    MiniCResult

--- src/Juvix/Compiler/Backend/C/Translation.hs
+++ src/Juvix/Compiler/Backend/C/Translation.hs
@@ -1,6 +1,8 @@
 module Juvix.Compiler.Backend.C.Translation
   ( module Juvix.Compiler.Backend.C.Translation.FromReg,
+    module Juvix.Compiler.Backend.C.Data.Types
   )
 where
 
 import Juvix.Compiler.Backend.C.Translation.FromReg
+import Juvix.Compiler.Backend.C.Data.Types
