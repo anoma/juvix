a3cd0795b06650d95913e8b1c464f4eebbd015ea
Lukasz Czajka <lukasz@heliax.dev>
2023-03-03 14:47:33 +0100

    move 'prime' to Prelude

--- src/Juvix/Compiler/Core/Transformation/DisambiguateBinderNames.hs
+++ src/Juvix/Compiler/Core/Transformation/DisambiguateBinderNames.hs
@@ -3,12 +3,10 @@ module Juvix.Compiler.Core.Transformation.DisambiguateBinderNames where
 import Data.HashMap.Strict qualified as HashMap
 import Data.List qualified as List
 import Data.List.NonEmpty qualified as NonEmpty
-import Data.Text qualified as Text
 import Juvix.Compiler.Core.Data.BinderList qualified as BL
 import Juvix.Compiler.Core.Extra
 import Juvix.Compiler.Core.Info.NameInfo (setInfoName)
 import Juvix.Compiler.Core.Transformation.Base
-import Text.Read qualified as Text
 
 disambiguateNodeBinderNames :: InfoTable -> Node -> Node
 disambiguateNodeBinderNames tab = dmapL go
@@ -76,14 +74,6 @@ disambiguateNodeBinderNames tab = dmapL go
               disambiguate bl (prime name)
           | otherwise ->
               name
-
-    prime :: Text -> Text
-    prime "" = "_X"
-    prime "?" = "_X"
-    prime name = case Text.splitOn "'" name of
-      [name', ""] -> name' <> "'0"
-      [name', num] -> name' <> "'" <> maybe (num <> "'") (show . (+ 1)) (Text.readMaybe (unpack num) :: Maybe Word)
-      _ -> name <> "'"
 
 disambiguateBinderNames :: InfoTable -> InfoTable
 disambiguateBinderNames tab =
