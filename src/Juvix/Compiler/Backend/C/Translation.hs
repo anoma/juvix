5a3001181fd9da2315c7318b09a46020bb5c04d9
Lukasz Czajka <lukasz@heliax.dev>
2023-03-13 19:06:52 +0100

    make ormolu happy

--- src/Juvix/Compiler/Backend/C/Translation.hs
+++ src/Juvix/Compiler/Backend/C/Translation.hs
@@ -1,8 +1,8 @@
 module Juvix.Compiler.Backend.C.Translation
   ( module Juvix.Compiler.Backend.C.Translation.FromReg,
-    module Juvix.Compiler.Backend.C.Data.Types
+    module Juvix.Compiler.Backend.C.Data.Types,
   )
 where
 
-import Juvix.Compiler.Backend.C.Translation.FromReg
 import Juvix.Compiler.Backend.C.Data.Types
+import Juvix.Compiler.Backend.C.Translation.FromReg
