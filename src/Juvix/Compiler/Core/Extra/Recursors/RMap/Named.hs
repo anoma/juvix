eee842c066115f7bf7728a2794dda1064e55c051
Lukasz Czajka <lukasz@heliax.dev>
2023-03-14 17:24:14 +0100

    comments

--- src/Juvix/Compiler/Core/Extra/Recursors/RMap/Named.hs
+++ src/Juvix/Compiler/Core/Extra/Recursors/RMap/Named.hs
@@ -40,16 +40,49 @@ where
   go :: ([BinderChange] -> Level -> Node -> Node) -> Node -> Node
   go recur k node = case node of
     NLam Lambda {..} ->
-      mkLet (ConstInt k) (cont [BCAdd 1, BCRemove (BinderRemove _lambdaBinder mkVar' 0)] k _lambdaBody)
+      mkLet
+        _lambdaInfo
+        (over binderType (cont []) _lambdaBinder)
+        (mkConstant' (ConstInteger k))
+        (cont [BCAdd 1, BCRemove (BinderRemove _lambdaBinder (mkVar' 0))] _lambdaBody)
     _ ->
       recur [] node
-
-  cont :: [BinderChange] -> Level -> Node -> Node
-  cont bcs = go (recur . (bcs ++))
+    where
+      cont :: [BinderChange] -> Node -> Node
+      cont bcs = go (recur . (bcs ++)) k
 ```
 produces
 ```
 let x := 0 in let y := 1 in x$1 + y$0
 ```
 
+The invocation
+```
+rmap go (\x \z \y \_ x$3 + y$1 + z$2)
+where
+  go :: ([BinderChange] -> Node -> Node) -> Node -> Node
+  go recur node = case node of
+    NLam lam1@(Lambda _ _ (NLam lam2)) ->
+      mkLambda
+        (lam1 ^. lambdaInfo)
+        (over lambdaBinder (over binderType (cont [])) lam1)
+        (cont
+          [
+            BCKeep (lam1 ^. lambdaBinder),
+            BCRemove (BinderRemove (lam2 ^. lambdaBinder) (mkVar' 0))
+          ]
+          (lam2 ^. lambdaBody))
+    _ ->
+      recur [] node
+    where
+      cont :: [BinderChange] -> Node -> Node
+      cont bcs = go (recur . (bcs ++))
+```
+produces
+```
+\x \y x$1 + y$0 + x$1
+```
+
+For the meaning of the suffixes to `rmap` see the comments in
+Core.Extra.Recursors.Map.Named
 -}
