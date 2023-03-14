15c1a0382184c3d1a318518cbb0086c953c81492
Paul Cadman <git@paulcadman.dev>
2023-03-14 19:38:58 +0000

    Format cmd: error when passed file is unformatted

--- app/Commands/Format.hs
+++ app/Commands/Format.hs
@@ -1,93 +1,49 @@
 {-# LANGUAGE QuasiQuotes #-}
+
 module Commands.Format where
 
 import Commands.Base
 import Commands.Format.Options
+import Data.String.Interpolate (i)
 import Data.Text qualified as T
 import Juvix.Compiler.Concrete.Language
 import Juvix.Compiler.Concrete.Print (ppOutDefault)
 import Juvix.Compiler.Concrete.Translation.FromParsed.Analysis.Scoping qualified as Scoper
 import Juvix.Prelude.Pretty (toPlainText)
-import Data.String.Interpolate (i)
 
 runCommand :: forall r. Members '[Embed IO, App] r => FormatOptions -> Sem r ()
-runCommand opts = mapM checkFile (filesToFormat opts)
-  -- res :: Scoper.ScoperResult <- runPipeline (opts ^. formatInputFile) upToScoping
-  -- let modules :: NonEmpty (Module 'Scoped 'ModuleTop) = res ^. Scoper.resultModules
-  --     formattedFile :: Text = formatFile (res ^. Scoper.comments) modules
-  -- inputFile' :: Path Abs File <- inputFile
-  -- fileContents <- embed (readFile (toFilePath (inputFile')))
-  -- unless (fileContents == formattedFile) exitError
-  -- where
-  --   exitError :: Sem r ()
-  --   exitError = do
-  --     inputFile' <- toFilePath <$> inputFile
-  --     exitMsg (ExitFailure 1) [i|File #{inputFile'} is not formatted|]
-
-  --   inputFile :: Sem r (Path Abs File)
-  --   inputFile = do
-  --     invokeDir <- askInvokeDir
-  --     return (someBaseToAbs invokeDir (opts ^. formatInputFile . pathPath))
-
-filesToFormat :: forall r. Member App r => FormatOptions -> Sem r [Path Abs File]
-filesToFormat opts = case opts ^. formatInputFile of
-  Just fp -> return <$> inputFile fp
-  Nothing -> pkgFiles
+runCommand opts = case opts ^. formatInputFile of
+  Nothing -> error "not implemented"
+  Just p -> unlessM (runFilesIO (isFileFormatted p)) (err p)
   where
-    inputFile :: AppPath File -> Sem r (Path Abs File)
-    inputFile appFile = do
-      invokeDir <- askInvokeDir
-      return (someBaseToAbs invokeDir (appFile ^. pathPath))
+    err :: AppPath File -> Sem r ()
+    err p = exitMsg (ExitFailure 1) [i|File #{p} is not formatted|]
 
-    pkgFiles :: Sem r [Path Abs File]
-    pkgFiles = undefined
-
-checkFile' :: forall r. Members '[Embed IO, App] r => Path Abs File -> Sem r ()
-checkFile' fp = do
-  let appFile = AppPath fp True
-  undefined
-
-checkFile :: forall r. Members '[Embed IO, App] r => AppPath File -> Sem r ()
-checkFile appFile = do
-  inputFilePath <- inputFile
-  res :: Scoper.ScoperResult <- runPipeline appFile upToScoping
-  let modules :: NonEmpty (Module 'Scoped 'ModuleTop) = res ^. Scoper.resultModules
-      formattedFile :: Text = formatFile (res ^. Scoper.comments) modules
-      fp :: FilePath = toFilePath inputFilePath
-  fileContents <- embed (readFile (toFilePath inputFilePath))
-  unless (fileContents == formattedFile) (exitError fp)
+formatAbsFile :: Member App r => Path Abs File -> Sem r Text
+formatAbsFile p = formatAppFile appFile
   where
-    exitError :: FilePath -> Sem r ()
-    exitError fp = exitMsg (ExitFailure 1) [i|File #{fp} is not formatted|]
+    appFile :: AppPath File
+    appFile =
+      AppPath
+        { _pathPath = Abs p,
+          _pathIsInput = True
+        }
 
-    inputFile :: Sem r (Path Abs File)
-    inputFile = do
-      invokeDir <- askInvokeDir
-      return (someBaseToAbs invokeDir (appFile ^. pathPath))
+isFileFormatted :: Members '[Files, App] r => AppPath File -> Sem r Bool
+isFileFormatted appFile = do
+  p <- someBaseToAbs' (appFile ^. pathPath)
+  actual <- readFile' p
+  expected <- formatAppFile appFile
+  return (actual == expected)
 
-
-
-
-formatModule :: Comments -> Module 'Scoped 'ModuleTop -> Text
-formatModule cs m = toPlainText (ppOutDefault cs m)
-
-formatFile :: Comments -> NonEmpty (Module 'Scoped 'ModuleTop) -> Text
-formatFile cs = T.concat . toList . fmap (formatModule cs)
-
-showDiff :: forall r. Members '[Embed IO, App] r => Text -> Text -> Sem r ()
-showDiff t1 t2 = do
-  hasCommand <- checkCommand "diff"
-  unless (isNothing hasCommand) go
+formatAppFile :: Member App r => AppPath File -> Sem r Text
+formatAppFile appFile = do
+  res <- runPipeline appFile upToScoping
+  let cs = res ^. Scoper.comments
+      formattedModules = run (runReader cs (mapM formatTopModule (res ^. Scoper.resultModules)))
+  return (T.concat (toList formattedModules))
   where
-    go :: Sem r ()
-    go = writeFiles $(mkAbsDir "/tmp")
-
-    writeFiles :: Path Abs Dir -> Sem r ()
-    writeFiles tmpDir = do
-      let f1 = tmpDir <//> relFile "f1"
-      let f2 = tmpDir <//> relFile "f2"
-      embed (putStrLn (pack (toFilePath f1)))
-      embed (writeFile (toFilePath f1) t1)
-      embed (writeFile (toFilePath f2) t2)
-      res <- runExternalCommand "diff" [pack (toFilePath f1), pack (toFilePath f2)]
-      embed $ putStrLn (res ^. runCmdResStdout)
+    formatTopModule :: Member (Reader Comments) r => Module 'Scoped 'ModuleTop -> Sem r Text
+    formatTopModule m = do
+      cs <- ask
+      return (toPlainText (ppOutDefault cs m))
