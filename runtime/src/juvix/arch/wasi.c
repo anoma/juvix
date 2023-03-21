2a8585ede04941c3b48fcd0789b447de47878052
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2023-03-21 15:34:46 +0100

    Fix bug in IO runtime (#1906)
    Co-authored-by: Paul Cadman <git@paulcadman.dev>

--- runtime/src/juvix/arch/wasi.c
+++ runtime/src/juvix/arch/wasi.c
@@ -5,11 +5,15 @@
 
 _Noreturn void exit(int code) { proc_exit(code); }
 
-void puts(const char *msg) {
+void puts_nonl(const char *msg) {
     size_t n = 0;
     while (msg[n]) ++n;
     ciovec_t vec = {.buf = (uint8_t *)msg, .buf_len = n};
     fd_write(1, &vec, 1, 0);
+}
+
+void puts(const char *msg) {
+    puts_nonl(msg);
     uint8_t c = '\n';
     ciovec_t vec1 = {.buf = &c, .buf_len = 1};
     fd_write(1, &vec1, 1, 0);
