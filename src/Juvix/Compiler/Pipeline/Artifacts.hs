52d270cc87b76039da96d46a5bb381d62dd092da
Paul Cadman <git@paulcadman.dev>
2023-03-22 17:28:57 +0000

    Revert "Runtime error when Artifacts function is used incorrectly"
    This reverts commit 56caf77131143c6530304cfc81fc111660d0397d.

--- src/Juvix/Compiler/Pipeline/Artifacts.hs
+++ src/Juvix/Compiler/Pipeline/Artifacts.hs
@@ -79,9 +79,6 @@ runReaderArtifacts l m = do
   s <- gets (^. l)
   runReader s m
 
-forbidStateArtifacts :: Members '[State Artifacts] r => Sem r a -> Sem r a
-forbidStateArtifacts = intercept @(State Artifacts) (\ _ -> error "internal error: invalid usage of State Artifacts")
-
 runStateLikeArtifacts ::
   Members '[State Artifacts] r =>
   (field -> Sem (stateEff ': r) a -> Sem r (field, a)) ->
@@ -90,6 +87,6 @@ runStateLikeArtifacts ::
   Sem r a
 runStateLikeArtifacts runEff l m = do
   s <- gets (^. l)
-  (s', a) <- forbidStateArtifacts (runEff s m)
+  (s', a) <- runEff s m
   modify' (set l s')
   return a
