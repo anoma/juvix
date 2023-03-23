41ba69da8296ea27df80cee32912ebe64501ce86
Paul Cadman <git@paulcadman.dev>
2023-03-23 16:00:07 +0000

    Add tests for remaining examples

--- test/Examples/Positive.hs
+++ test/Examples/Positive.hs
@@ -49,11 +49,35 @@ tests =
       $(mkRelFile "Fibonacci/expected.golden")
       "25\n",
     posTest
+      "Hello World"
+      $(mkRelDir "milestone/HelloWorld")
+      $(mkRelFile "HelloWorld.juvix")
+      $(mkRelFile "HelloWorld/expected.golden")
+      "",
+    posTest
       "Collatz calculator"
       $(mkRelDir "milestone/Collatz")
       $(mkRelFile "Collatz.juvix")
       $(mkRelFile "Collatz/expected.golden")
       "123\n",
+    posTest
+      "Pascal's triangle"
+      $(mkRelDir "milestone/PascalsTriangle")
+      $(mkRelFile "PascalsTriangle.juvix")
+      $(mkRelFile "PascalsTriangle/expected.golden")
+      "",
+    posTest
+      "Demo"
+      $(mkRelDir "demo")
+      $(mkRelFile "Demo.juvix")
+      $(mkRelFile "Demo/expected.golden")
+      "",
+    posTest
+      "TicTacToe"
+      $(mkRelDir "milestone/TicTacToe")
+      $(mkRelFile "CLI/TicTacToe.juvix")
+      $(mkRelFile "TicTacToe/expected.golden")
+      "aaa\n0\n10\n1\n2\n3\n3\n4\n5\n6\n7\n8\n9\n",
     posTest
       "Towers of Hanoi"
       $(mkRelDir "milestone/Hanoi")
