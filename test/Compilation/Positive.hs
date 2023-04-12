4177fe460826ecbe254ce4db26f5db2c38e19bb3
Lukasz Czajka <lukasz@heliax.dev>
2023-04-12 20:43:29 +0200

    nested pattern matching with integers test

--- test/Compilation/Positive.hs
+++ test/Compilation/Positive.hs
@@ -304,5 +304,10 @@ tests =
       "Test049: Builtin Int"
       $(mkRelDir ".")
       $(mkRelFile "test049.juvix")
-      $(mkRelFile "out/test049.out")
+      $(mkRelFile "out/test049.out"),
+    posTest
+      "Test050: Pattern matching with integers"
+      $(mkRelDir ".")
+      $(mkRelFile "test050.juvix")
+      $(mkRelFile "out/test050.out")
   ]
