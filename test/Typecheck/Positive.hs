ef6905a2f33372560043321a6e45f27019e8cc2a
Jan Mas Rovira <janmasrovira@gmail.com>
2023-05-08 23:27:49 +0200

    typo

--- test/Typecheck/Positive.hs
+++ test/Typecheck/Positive.hs
@@ -26,7 +26,7 @@ testDescr PosTest {..} =
       _testRoot = _dir,
       _testAssertion = Single $ do
         entryPoint <- defaultEntryPointCwdIO _file
-        (void . runIO' entryPoint) upInternalTyped
+        (void . runIO' entryPoint) upToInternalTyped
     }
 
 --------------------------------------------------------------------------------
