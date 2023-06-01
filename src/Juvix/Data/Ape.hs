035496b1d9f649e36ac6692e13e3b71d5e45edfc
Jan Mas Rovira <janmasrovira@gmail.com>
2023-06-01 22:35:10 +0200

    use ape print

--- src/Juvix/Data/Ape.hs
+++ src/Juvix/Data/Ape.hs
@@ -1,8 +1,8 @@
 module Juvix.Data.Ape
-  ( module Juvix.Data.Ape.Pretty,
+  ( module Juvix.Data.Ape.Print,
     module Juvix.Data.Ape.Base,
   )
 where
 
 import Juvix.Data.Ape.Base
-import Juvix.Data.Ape.Pretty
+import Juvix.Data.Ape.Print
