9db16ca87feb8ec3704a65c97d5ea120ea4e333d
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2023-06-06 13:26:43 +0200

    Fix 'function not found' error in `juvix eval` (#2178)
    * Closes #2176
    
    ---------
    
    Co-authored-by: Jan Mas Rovira <janmasrovira@gmail.com>

--- app/Commands/Eval.hs
+++ app/Commands/Eval.hs
@@ -4,6 +4,7 @@ import Commands.Base
 import Commands.Eval.Options
 import Evaluator qualified as Eval
 import Juvix.Compiler.Core qualified as Core
+import Juvix.Extra.Strings qualified as Str
 
 runCommand :: (Members '[Embed IO, App] r) => EvalOptions -> Sem r ()
 runCommand opts@EvalOptions {..} = do
@@ -15,11 +16,16 @@ runCommand opts@EvalOptions {..} = do
             runError @JuvixError $
               (Core.toEval' _coreResultTable :: Sem '[Error JuvixError, Reader Core.CoreOptions] Core.InfoTable)
   tab <- getRight r
-  let evalNode =
+  let mevalNode =
         if
-            | isJust (_evalSymbolName) -> getNode' tab (selInfo tab)
-            | otherwise -> getNode' tab (mainInfo tab)
-  Eval.evalAndPrint opts tab evalNode
+            | isJust _evalSymbolName -> getNode tab (selInfo tab)
+            | otherwise -> getNode tab (mainInfo tab)
+  case mevalNode of
+    Just evalNode ->
+      Eval.evalAndPrint opts tab evalNode
+    Nothing -> do
+      let name = fromMaybe Str.main _evalSymbolName
+      printFailureExit ("function not found: " <> name)
   where
     mainInfo :: Core.InfoTable -> Maybe Core.IdentifierInfo
     mainInfo tab = do
@@ -31,11 +37,5 @@ runCommand opts@EvalOptions {..} = do
       s <- _evalSymbolName
       find (^. Core.identifierName . to (== s)) (tab ^. Core.infoIdentifiers)
 
-    getNode' :: Core.InfoTable -> Maybe Core.IdentifierInfo -> Core.Node
-    getNode' tab m = fromMaybe err (getNode tab m)
-
     getNode :: Core.InfoTable -> Maybe Core.IdentifierInfo -> Maybe Core.Node
     getNode tab m = m >>= \i -> tab ^. Core.identContext . at (i ^. Core.identifierSymbol)
-
-    err :: a
-    err = error "function not found"
