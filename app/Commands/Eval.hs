f92d91858a99494f90bb7ffec39edf2f14995f0b
Lukasz Czajka <lukasz@heliax.dev>
2023-06-06 11:02:10 +0200

    fix 'function not found' error

--- app/Commands/Eval.hs
+++ app/Commands/Eval.hs
@@ -15,11 +15,16 @@ runCommand opts@EvalOptions {..} = do
             runError @JuvixError $
               (Core.toEval' _coreResultTable :: Sem '[Error JuvixError, Reader Core.CoreOptions] Core.InfoTable)
   tab <- getRight r
-  let evalNode =
+  let mevalNode =
         if
-            | isJust (_evalSymbolName) -> getNode' tab (selInfo tab)
-            | otherwise -> getNode' tab (mainInfo tab)
-  Eval.evalAndPrint opts tab evalNode
+            | isJust (_evalSymbolName) -> getNode tab (selInfo tab)
+            | otherwise -> getNode tab (mainInfo tab)
+  case mevalNode of
+    Just evalNode ->
+      Eval.evalAndPrint opts tab evalNode
+    Nothing -> do
+      let name = fromMaybe "main" _evalSymbolName
+      printFailureExit ("function not found: " <> name)
   where
     mainInfo :: Core.InfoTable -> Maybe Core.IdentifierInfo
     mainInfo tab = do
@@ -31,11 +36,5 @@ runCommand opts@EvalOptions {..} = do
       s <- _evalSymbolName
       find (^. Core.identifierName . to (== s)) (tab ^. Core.infoIdentifiers)
 
-    getNode' :: Core.InfoTable -> Maybe Core.IdentifierInfo -> Core.Node
-    getNode' tab m = fromMaybe err (getNode tab m)
-
     getNode :: Core.InfoTable -> Maybe Core.IdentifierInfo -> Maybe Core.Node
     getNode tab m = m >>= \i -> tab ^. Core.identContext . at (i ^. Core.identifierSymbol)
-
-    err :: a
-    err = error "function not found"
