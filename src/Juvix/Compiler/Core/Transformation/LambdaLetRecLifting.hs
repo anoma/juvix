1aa16855801602dbe01b964076577d00703e4c4d
Lukasz Czajka <lukasz@heliax.dev>
2023-06-26 11:08:12 +0200

    remove outdated comment

--- src/Juvix/Compiler/Core/Transformation/LambdaLetRecLifting.hs
+++ src/Juvix/Compiler/Core/Transformation/LambdaLetRecLifting.hs
@@ -159,8 +159,7 @@ lambdaLiftNode aboveBl top =
                   ]
           declareTopSyms
 
-          let -- TODO it can probably be simplified, and it's wrong
-              shiftHelper :: Node -> NonEmpty (Node, Binder) -> Node
+          let shiftHelper :: Node -> NonEmpty (Node, Binder) -> Node
               shiftHelper b = goShift 0
                 where
                   goShift :: Int -> NonEmpty (Node, Binder) -> Node
