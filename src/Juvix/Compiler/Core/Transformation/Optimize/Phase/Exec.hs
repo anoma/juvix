5b44b2e6545cb2ae79a57bb5c6defcd02ab4d246
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2023-06-27 15:37:11 +0200

    Use specialization pragmas in the standard library (#2230)
    * Adds `specialize` pragmas for folds, maps, etc.
    * Fixes two bugs in `specializeArgs`

--- src/Juvix/Compiler/Core/Transformation/Optimize/Phase/Exec.hs
+++ src/Juvix/Compiler/Core/Transformation/Optimize/Phase/Exec.hs
@@ -5,12 +5,14 @@ import Juvix.Compiler.Core.Transformation.Base
 import Juvix.Compiler.Core.Transformation.LambdaLetRecLifting
 import Juvix.Compiler.Core.Transformation.Optimize.LetFolding
 import Juvix.Compiler.Core.Transformation.Optimize.Phase.Main qualified as Main
+import Juvix.Compiler.Core.Transformation.TopEtaExpand
 
 optimize :: Member (Reader CoreOptions) r => InfoTable -> Sem r InfoTable
 optimize tab = do
   opts <- ask
   withOptimizationLevel' tab 1 $
     return
+      . topEtaExpand
       . letFolding
       . lambdaLetRecLifting
       . Main.optimize' opts
