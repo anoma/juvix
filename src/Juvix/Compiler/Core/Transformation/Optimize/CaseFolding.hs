2fff65030b7f53464ff19b0b3ad64194a8e5cac2
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2023-06-27 12:29:26 +0200

    Case folding (#2229)
    Fold constant cases, i.e., convert
    ```
    case C a b
    | A := ..
    | B x := ..
    | C x y := M
    ```
    to
    ```
    let x := a;
         y := b;
    in
    M
    ```

---
+++ src/Juvix/Compiler/Core/Transformation/Optimize/CaseFolding.hs
@@ -0,0 +1,32 @@
+module Juvix.Compiler.Core.Transformation.Optimize.CaseFolding (caseFolding) where
+
+import Juvix.Compiler.Core.Extra.Utils
+import Juvix.Compiler.Core.Transformation.Base
+
+convertNode :: Node -> Node
+convertNode = dmap go
+  where
+    go :: Node -> Node
+    go node = case node of
+      NCase Case {..} ->
+        case _caseValue of
+          NCtr Constr {..} ->
+            case find ((== _constrTag) . (^. caseBranchTag)) _caseBranches of
+              Just CaseBranch {..} ->
+                goBranch 0 _caseBranchBinders _constrArgs _caseBranchBody
+              Nothing ->
+                fromMaybe node _caseDefault
+          _ -> node
+      _ -> node
+
+    goBranch :: Int -> [Binder] -> [Node] -> Node -> Node
+    goBranch k bs args body = case (bs, args) of
+      ([], []) ->
+        body
+      (b : bs', arg : args') ->
+        mkLet mempty b (shift k arg) (goBranch (k + 1) bs' args' body)
+      _ ->
+        impossible
+
+caseFolding :: InfoTable -> InfoTable
+caseFolding = mapAllNodes convertNode
--- src/Juvix/Compiler/Core/Transformation/Optimize/Phase/Main.hs
+++ src/Juvix/Compiler/Core/Transformation/Optimize/Phase/Main.hs
@@ -3,11 +3,12 @@ module Juvix.Compiler.Core.Transformation.Optimize.Phase.Main where
 import Juvix.Compiler.Core.Data.IdentDependencyInfo
 import Juvix.Compiler.Core.Options
 import Juvix.Compiler.Core.Transformation.Base
+import Juvix.Compiler.Core.Transformation.Optimize.CaseFolding
 import Juvix.Compiler.Core.Transformation.Optimize.FilterUnreachable
 import Juvix.Compiler.Core.Transformation.Optimize.Inlining
 import Juvix.Compiler.Core.Transformation.Optimize.LambdaFolding
 import Juvix.Compiler.Core.Transformation.Optimize.LetFolding
-import Juvix.Compiler.Core.Transformation.Optimize.SpecializeArgs (specializeArgs)
+import Juvix.Compiler.Core.Transformation.Optimize.SpecializeArgs
 
 optimize' :: CoreOptions -> InfoTable -> InfoTable
 optimize' CoreOptions {..} tab =
@@ -17,6 +18,7 @@ optimize' CoreOptions {..} tab =
       ( compose 2 (letFolding' (isInlineableLambda _optInliningDepth))
           . lambdaFolding
           . inlining' _optInliningDepth (recursiveIdents tab)
+          . caseFolding
           . letFolding' (isInlineableLambda _optInliningDepth)
           . lambdaFolding
           . specializeArgs
