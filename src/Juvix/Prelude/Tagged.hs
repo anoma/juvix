661f53219f7310c639197f7bf433e978fc1ad9c3
Jan Mas Rovira <janmasrovira@gmail.com>
2023-06-29 13:12:02 +0200

    ormolu

--- src/Juvix/Prelude/Tagged.hs
+++ src/Juvix/Prelude/Tagged.hs
@@ -3,9 +3,9 @@
 -- | This module requires AllowAmbiguousTypes, so it is separated from Base
 module Juvix.Prelude.Tagged where
 
+import Polysemy
 import Polysemy.Tagged hiding (tag)
 import Polysemy.Tagged qualified as Polysemy
-import Polysemy
 
 -- | We rename it to ptag to avoid clashes with the commonly used `tag` identifier.
 ptag :: forall k e r a. Member (Tagged k e) r => Sem (e ': r) a -> Sem r a
