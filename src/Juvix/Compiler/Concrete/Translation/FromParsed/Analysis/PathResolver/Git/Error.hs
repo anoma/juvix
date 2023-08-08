c818186d0671659a5f5e3dfe630f5b55171dad88
Paul Cadman <git@paulcadman.dev>
2023-08-08 08:46:32 +0100

    Add generic error implementations for GitError

--- src/Juvix/Compiler/Concrete/Translation/FromParsed/Analysis/PathResolver/Git/Error.hs
+++ src/Juvix/Compiler/Concrete/Translation/FromParsed/Analysis/PathResolver/Git/Error.hs
@@ -1,5 +1,6 @@
 module Juvix.Compiler.Concrete.Translation.FromParsed.Analysis.PathResolver.Git.Error where
 
+import Juvix.Data.PPOutput
 import Juvix.Prelude
 
 data GitCmdErrorDetails = GitCmdErrorDetails
@@ -18,7 +19,49 @@ data GitError
 makeLenses ''GitCmdErrorDetails
 
 instance ToGenericError GitCmdErrorDetails where
-  genericError e = undefined
+  genericError e =
+    return
+      GenericError
+        { _genericErrorMessage = ppOutput (pretty msg),
+          _genericErrorLoc = i,
+          _genericErrorIntervals = [i]
+        }
+    where
+      i = getLoc e
+      msg :: Text
+      msg = "error when executing the git command"
 
 instance ToGenericError GitError where
-  genericError e = undefined
+  genericError :: (Member (Reader GenericOptions) r) => GitError -> Sem r GenericError
+  genericError e = case e of
+    GitCmdError ce -> genericError ce
+    GitCmdNotFound {} -> return (err "git command not found")
+    NotAGitClone p -> return (err ("path " <> show p <> " is not a git clone"))
+    NotAnEmptyDir p -> return (err ("path " <> show p <> " is not an empty dir"))
+    where
+      err :: Text -> GenericError
+      err msg =
+        GenericError
+          { _genericErrorMessage = ppOutput (pretty msg),
+            _genericErrorLoc = i,
+            _genericErrorIntervals = [i]
+          }
+      i = getLoc e
+
+instance HasLoc GitCmdErrorDetails where
+  getLoc (GitCmdErrorDetails {}) = fromMaybe defaultLoc Nothing
+    where
+      defaultLoc :: Interval
+      defaultLoc = singletonInterval (mkInitialLoc sourcePath)
+
+      sourcePath :: Path Abs File
+      sourcePath = $(mkAbsFile "/<yaml>")
+
+instance HasLoc GitError where
+  getLoc _ = fromMaybe defaultLoc Nothing
+    where
+      defaultLoc :: Interval
+      defaultLoc = singletonInterval (mkInitialLoc sourcePath)
+
+      sourcePath :: Path Abs File
+      sourcePath = $(mkAbsFile "/<yaml>")
