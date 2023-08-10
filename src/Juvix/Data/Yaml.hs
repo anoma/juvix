20332b25d445656a4466cec3a5a2342dffe95bf9
Lukasz Czajka <lukasz@heliax.dev>
2023-08-10 18:15:10 +0200

    error on duplicate keys in YAML

--- src/Juvix/Data/Yaml.hs
+++ src/Juvix/Data/Yaml.hs
@@ -5,14 +5,18 @@ module Juvix.Data.Yaml
   )
 where
 
-import Data.Aeson.BetterErrors hiding ((<|>))
-import Data.Yaml (FromJSON (..))
+import Data.Aeson.BetterErrors hiding (mapError, (<|>))
+import Data.Aeson.Internal (formatError)
+import Data.Yaml (FromJSON (..), ParseException (..), prettyPrintParseException)
+import Data.Yaml.Internal (Warning (..), decodeHelper)
+import GHC.IO (unsafePerformIO)
 import Juvix.Prelude.Base
+import Text.Libyaml qualified as Y
 
 type YamlError = Text
 
 -- | Check that all keys are in the given list.
-checkYamlKeys :: [Text] -> Parse Text ()
+checkYamlKeys :: [Text] -> Parse YamlError ()
 checkYamlKeys keys = do
   forEachInObject
     ( \k ->
@@ -20,3 +24,19 @@ checkYamlKeys keys = do
           throwCustomError ("unknown key: " <> k)
     )
   return ()
+
+decodeEither :: FromJSON a => ByteString -> Either ParseException a
+decodeEither =
+  either Left splitEither
+    . unsafePerformIO
+    . decodeHelper
+    . Y.decode
+  where
+    splitEither :: ([Warning], Either String a) -> Either ParseException a
+    splitEither (_, Left s) = Left (AesonException s)
+    splitEither (w : _, Right _) = Left (AesonException (prettyPrintWarning w))
+    splitEither ([], Right x) = Right x
+
+prettyPrintWarning :: Warning -> String
+prettyPrintWarning = \case
+  DuplicateKey a -> formatError a "duplicate key"
