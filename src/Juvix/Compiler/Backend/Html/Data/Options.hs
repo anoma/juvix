3c5304ff9df704fc8d3dfa6ac518e7cd5bbb5d73
Jan Mas Rovira <janmasrovira@gmail.com>
2023-08-31 16:53:10 +0200

    Show all available html themes in the CLI automatically (#2322)

--- src/Juvix/Compiler/Backend/Html/Data/Options.hs
+++ src/Juvix/Compiler/Backend/Html/Data/Options.hs
@@ -1,6 +1,7 @@
 module Juvix.Compiler.Backend.Html.Data.Options where
 
 import Juvix.Prelude
+import Prelude (show)
 
 data HtmlOptions = HtmlOptions
   { _htmlOptionsKind :: HtmlKind,
@@ -15,7 +16,22 @@ data HtmlOptions = HtmlOptions
 data Theme
   = Nord
   | Ayu
-  deriving stock (Show, Enum, Bounded, Data)
+  deriving stock (Enum, Eq, Ord, Bounded, Data)
+
+instance Show Theme where
+  show = \case
+    Nord -> "nord"
+    Ayu -> "ayu"
+
+data ThemeLight
+  = Dark
+  | Light
+  deriving stock (Show, Eq, Enum, Ord, Bounded, Data)
+
+themeLight :: Theme -> ThemeLight
+themeLight = \case
+  Nord -> Dark
+  Ayu -> Light
 
 data HtmlKind
   = HtmlDoc
