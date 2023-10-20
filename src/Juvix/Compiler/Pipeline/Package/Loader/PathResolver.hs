8c431c809571e2b39cc06773e8260e5581872e90
Paul Cadman <git@paulcadman.dev>
2023-10-20 18:47:54 +0100

    Give error if Package.juvix tries to import an unresolvable file

--- src/Juvix/Compiler/Pipeline/Package/Loader/PathResolver.hs
+++ src/Juvix/Compiler/Pipeline/Package/Loader/PathResolver.hs
@@ -22,20 +22,23 @@ runPackagePathResolver rootPath sem = do
       RegisterDependencies {} -> pureT ()
       ExpectedModulePath _ m -> do
         let relPath = topModulePathToRelativePath' m
-        pureT (Just ((mkPackageRoot' relPath) <//> relPath))
+        pureT ((<//> relPath) <$> (mkPackageRoot' relPath))
       WithPath m a -> do
         let relPath = topModulePathToRelativePath' m
-            x :: (Path Abs Dir, Path Rel File)
-            x = (mkPackageRoot' relPath, relPath)
-        runTSimple (return (Right x)) >>= bindTSimple a
+            x :: Either PathResolverError (Path Abs Dir, Path Rel File)
+            x = case mkPackageRoot' relPath of
+              Just p -> Right (p, relPath)
+              Nothing -> Left (ErrPackageInvalidImport PackageInvalidImport {_packageInvalidImport = m})
+        runTSimple (return x) >>= bindTSimple a
     )
     sem
   where
-    mkPackageRoot :: HashSet (Path Rel File) -> Path Abs Dir -> Path Abs Dir -> Path Rel File -> Path Abs Dir
+    mkPackageRoot :: HashSet (Path Rel File) -> Path Abs Dir -> Path Abs Dir -> Path Rel File -> Maybe (Path Abs Dir)
     mkPackageRoot pkgFiles globalPackageDir globalStdlib relPath
-      | parent preludePath `isProperPrefixOf` relPath = globalStdlib
-      | relPath `HashSet.member` pkgFiles = globalPackageDir
-      | otherwise = rootPath
+      | parent preludePath `isProperPrefixOf` relPath = Just globalStdlib
+      | relPath `HashSet.member` pkgFiles = Just globalPackageDir
+      | relPath == packageFilePath = Just rootPath
+      | otherwise = Nothing
 
 runPackagePathResolver' :: (Members '[Files] r) => Path Abs Dir -> Sem (PathResolver ': r) a -> Sem r (ResolverState, a)
 runPackagePathResolver' root eff = do
