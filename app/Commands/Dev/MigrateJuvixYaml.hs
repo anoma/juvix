9c7defad92b94cd0347f3543fc164068060eef81
Paul Cadman <git@paulcadman.dev>
2023-11-07 14:50:56 +0000

    Run formatter

--- app/Commands/Dev/MigrateJuvixYaml.hs
+++ app/Commands/Dev/MigrateJuvixYaml.hs
@@ -1,8 +1,8 @@
 module Commands.Dev.MigrateJuvixYaml where
 
 import Commands.Base
-import Commands.Extra.Package
 import Commands.Dev.MigrateJuvixYaml.Options
+import Commands.Extra.Package
 import Juvix.Extra.Paths
 
 runCommand :: forall r. (Members '[Embed IO, Files, App] r) => MigrateJuvixYamlOptions -> Sem r ()
@@ -13,8 +13,8 @@ runCommand MigrateJuvixYamlOptions {..} = do
   pkgFileExists <- fileExists' pkgFilePath
   pkg <- askPackage
   if
-    | isGlobalPackage -> exitMsg (ExitFailure 1) "No Package file found"
-    | not pkgFileExists || _migrateJuvixYamlOptionsForce -> do
-        writePackageFile pkgDir pkg
-        removeFile' (pkgDir <//> juvixYamlFile)
-    | otherwise -> exitMsg (ExitFailure 1) (show pkgFilePath <> " already exists.")
+      | isGlobalPackage -> exitMsg (ExitFailure 1) "No Package file found"
+      | not pkgFileExists || _migrateJuvixYamlOptionsForce -> do
+          writePackageFile pkgDir pkg
+          removeFile' (pkgDir <//> juvixYamlFile)
+      | otherwise -> exitMsg (ExitFailure 1) (show pkgFilePath <> " already exists.")
