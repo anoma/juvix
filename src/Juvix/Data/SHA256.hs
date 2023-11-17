90313a8e48002f8a2898b312d943bd0b583f9e86
Paul Cadman <git@paulcadman.dev>
2023-11-17 09:50:18 +0000

    Add a function to compute a SHA256 digest of a file

---
+++ src/Juvix/Data/SHA256.hs
@@ -0,0 +1,9 @@
+module Juvix.Data.SHA256 where
+
+import Crypto.Hash.SHA256 qualified as SHA256
+import Data.ByteString.Base16 qualified as Base16
+import Juvix.Prelude
+
+-- | Create a HEX encoded, SHA256 digest of the contents of a file.
+digestFile :: (Member Files r) => Path Abs File -> Sem r Text
+digestFile = fmap (decodeUtf8Lenient . Base16.encode . SHA256.hash) . readFileBS'
