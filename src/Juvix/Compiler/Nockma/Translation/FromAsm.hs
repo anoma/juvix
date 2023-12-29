790783c94f8d56e02c6c8bab9712cb3b4da5c0bb
Jan Mas Rovira <janmasrovira@gmail.com>
2023-12-29 13:11:17 +0100

    skeleton

--- src/Juvix/Compiler/Nockma/Translation/FromAsm.hs
+++ src/Juvix/Compiler/Nockma/Translation/FromAsm.hs
@@ -10,13 +10,12 @@ import Juvix.Prelude hiding (Atom, Path)
 
 type FunctionPaths = HashMap Text Path
 
-data CompilerFunction = CompilerFunction {
-  _compilerFunctionName :: Text,
-  _compilerFunction :: Sem '[Compiler,  Reader FunctionPaths] ()
-                                     }
+type Offset = Natural
 
-fromAsm :: Asm.Symbol -> Asm.InfoTable -> Term Natural
-fromAsm = undefined
+data CompilerFunction = CompilerFunction
+  { _compilerFunctionName :: Text,
+    _compilerFunction :: Sem '[Compiler, Reader FunctionPaths] ()
+  }
 
 data StackId
   = CurrentFunction
@@ -30,17 +29,42 @@ data StackId
 data StdlibFunction
   = StdlibDec
   | StdlibAdd
+  | StdlibSub
+  | StdlibMul
+  | StdlibDiv
+  | StdlibMod
+  | StdlibLt
+  | StdlibLe
+    --   Asm.IntSub -> callStdlib undefined
+    -- Asm.IntMul -> callStdlib undefined
+    -- Asm.IntDiv -> callStdlib undefined
+    -- Asm.IntMod -> callStdlib undefined
+    -- Asm.IntLt -> callStdlib undefined
+    -- Asm.IntLe -> callStdlib undefined
+
 
 stdlibNumArgs :: StdlibFunction -> Natural
 stdlibNumArgs = \case
   StdlibDec -> 1
   StdlibAdd -> 2
+  StdlibSub -> 2
+  StdlibMul -> 2
+  StdlibMod -> 2
+  StdlibDiv -> 2
+  StdlibLe -> 2
+  StdlibLt -> 2
 
 stdlibPath :: StdlibFunction -> Path
 stdlibPath =
   decodePath' . EncodedPath . \case
     StdlibDec -> 342
     StdlibAdd -> 20
+    StdlibSub -> 47
+    StdlibMul -> 4
+    StdlibDiv -> 170
+    StdlibMod -> 46
+    StdlibLe -> 84
+    StdlibLt -> 343
 
 numStacks :: (Integral a) => a
 numStacks = fromIntegral (length (allElements @StackId))
@@ -51,6 +75,8 @@ data Compiler m a where
   PopFrom :: StackId -> Compiler m ()
   Call :: Text -> Natural -> Compiler m ()
   IncrementOn :: StackId -> Compiler m ()
+  PushArgRef :: Offset -> Compiler m ()
+  PushTempRef :: Offset -> Compiler m ()
   Branch :: m () -> m () -> Compiler m ()
   CallStdlib :: StdlibFunction -> Compiler m ()
   AsmReturn :: Compiler m ()
@@ -79,6 +105,87 @@ pathInStack s p = stackPath s ++ p
 
 makeSem ''Compiler
 makeLenses ''CompilerFunction
+
+fromAsm :: Asm.Symbol -> Asm.InfoTable -> Term Natural
+fromAsm = undefined
+
+compile :: forall r. Members '[Compiler] r => Asm.Code -> Sem r ()
+compile = mapM_ goCommand
+  where
+  goCommand :: Asm.Command -> Sem r ()
+  goCommand = \case
+    Asm.Instr i -> goCmdInstr i
+    Asm.Branch b -> goBranch b
+    Asm.Case c -> goCase c
+
+  goCase :: Asm.CmdCase -> Sem r ()
+  goCase = undefined
+
+  goBranch :: Asm.CmdBranch -> Sem r ()
+  goBranch Asm.CmdBranch {..} = branch (compile _cmdBranchTrue) (compile _cmdBranchFalse)
+
+  goBinop :: Asm.Opcode -> Sem r ()
+  goBinop = \case
+    Asm.IntAdd -> callStdlib StdlibAdd
+    Asm.IntSub -> callStdlib undefined
+    Asm.IntMul -> callStdlib undefined
+    Asm.IntDiv -> callStdlib undefined
+    Asm.IntMod -> callStdlib undefined
+    Asm.IntLt -> callStdlib undefined
+    Asm.IntLe -> callStdlib undefined
+    Asm.StrConcat -> stringsErr
+
+  goPush :: Asm.Value -> Sem r ()
+  goPush = undefined
+
+  goPushTemp :: Sem r ()
+  goPushTemp = undefined
+
+  goPrealloc :: Asm.InstrPrealloc -> Sem r ()
+  goPrealloc = undefined
+
+  goAllocConstr :: Asm.Tag -> Sem r ()
+  goAllocConstr = undefined
+
+  goAllocClosure :: Asm.InstrAllocClosure -> Sem r ()
+  goAllocClosure = undefined
+
+  goExtendClosure :: Asm.InstrExtendClosure -> Sem r ()
+  goExtendClosure = undefined
+
+  goCall :: Asm.InstrCall -> Sem r ()
+  goCall = undefined
+
+  goTailCall :: Asm.InstrCall -> Sem r ()
+  goTailCall = undefined
+
+  goCmdInstr :: Asm.CmdInstr -> Sem r ()
+  goCmdInstr Asm.CmdInstr {..} = case _cmdInstrInstruction of
+    Asm.Binop op -> goBinop op
+    Asm.Push p -> goPush p
+    Asm.Pop -> pop
+    Asm.PopTemp -> popFrom TempStack
+    Asm.PushTemp -> goPushTemp
+    Asm.Failure -> undefined
+    Asm.Prealloc i -> goPrealloc i
+    Asm.AllocConstr i -> goAllocConstr i
+    Asm.AllocClosure c -> goAllocClosure c
+    Asm.ExtendClosure c -> goExtendClosure c
+    Asm.Call c -> goCall c
+    Asm.TailCall c -> goTailCall c
+    Asm.Return -> asmReturn
+    Asm.ValShow -> stringsErr
+    Asm.StrToInt -> stringsErr
+    Asm.Trace -> unsupported "trace"
+    Asm.Dump -> unsupported "dump"
+    Asm.CallClosures {} -> impossible
+    Asm.TailCallClosures {} -> impossible
+
+unsupported :: Text -> a
+unsupported thing = error ("The Nockma backend does not support" <> thing)
+
+stringsErr :: a
+stringsErr = unsupported "strings"
 
 seqTerms :: [Term Natural] -> Term Natural
 seqTerms = foldl' step (OpAddress # emptyPath) . reverse
@@ -153,89 +260,113 @@ runCompilerWith :: [CompilerFunction] -> Sem (Compiler ': r) a -> Sem r (([Term 
 runCompilerWith funs sem = do
   (ts, a) <- runReader functionLocations (runOutputList (re (raiseUnder sem)))
   let compiledFuns :: [Term Natural]
-      compiledFuns = (# nockNil') <$>
-        (run . runReader functionLocations . mapM (execCompiler . (^. compilerFunction)) $ funs)
+      compiledFuns =
+        (# nockNil')
+          <$> (run . runReader functionLocations . mapM (execCompiler . (^. compilerFunction)) $ funs)
   return ((compiledFuns, seqTerms ts), a)
   where
-  functionLocations :: FunctionPaths
-  functionLocations =   hashMap
-    [ (_compilerFunctionName, indexInStack FunctionsLibrary i)
-      | (i, CompilerFunction {..}) <- zip [0 ..] funs
-    ]
+    functionLocations :: FunctionPaths
+    functionLocations =
+      hashMap
+        [ (_compilerFunctionName, indexInStack FunctionsLibrary i)
+          | (i, CompilerFunction {..}) <- zip [0 ..] funs
+        ]
+
+call' :: (Members '[Output (Term Natural), Reader FunctionPaths] r) => Text -> Natural -> Sem r ()
+call' funName funArgsNum = do
+  -- 1. Obtain the path to the function
+  -- 2.
+  --   i. Take a copy of the value stack without the arguments to the function
+  --   ii. Push this copy to the call stack
+  -- 3.
+  --  i. Take a copy of the function from the function library
+  --  ii. Replace its argument area with the arguments from the value stack
+  --  iii. Push this copy to the current function stack
+  -- 4. Replace the value stack with nil
+  -- 5. Evaluate the function in the context of the whole nock stack
+  --    The return instruction in the function compiles to:
+  --      i. Restore the previous value stack (created in 2.). i.e copy the previous value stack
+  --         from the call stack and push the result (the head of the current value stack) to it.
+  --      i. Pop the call stack and the current function stack.
+
+  funPath <- fromJust <$> asks @FunctionPaths (^. at funName)
+
+  -- Save the current state of the value stack
+  output (pushOnStack CallStack (OpAddress # pathInStack ValueStack (replicate funArgsNum R)))
+
+  -- Setup function to call with it arguments
+  output
+    ( pushOnStack
+        CurrentFunction
+        ( OpReplace
+            #
+            -- TODO: copy funArgs elements from the value stack
+            ( ([R] # stackTake ValueStack funArgsNum)
+                # OpAddress
+                # funPath
+            )
+        )
+    )
+
+  -- Init 'activation frame'
+  output (resetStack ValueStack)
+
+  -- call the function
+  output (OpCall # ((indexInStack CurrentFunction 0 ++ [L]) # (OpAddress # emptyPath)))
+
+incrementOn' :: (Members '[Output (Term Natural), Reader FunctionPaths] r) => StackId -> Sem r ()
+incrementOn' s = output (replaceOnStack s (OpInc # (OpAddress # pathInStack s [L])))
+
+callStdlib' :: (Members '[Output (Term Natural), Reader FunctionPaths] r) => StdlibFunction -> Sem r ()
+callStdlib' f = do
+  let fNumArgs = stdlibNumArgs f
+      fPath = stdlibPath f
+      decodeFn = OpCall # (fPath # (OpAddress # stackPath StandardLibrary))
+      arguments = OpSequence # (OpAddress # [R]) # stdlibStackTake ValueStack fNumArgs
+      extractResult = (OpAddress # [L]) # (OpAddress # [R, R])
+      callFn = OpPush # (OpCall # [L] # (OpReplace # ([R, L] # arguments) # (OpAddress # [L]))) # extractResult
+
+  output (OpPush # decodeFn # callFn)
+  output (replaceTopStackN fNumArgs ValueStack)
+  where
+    stdlibStackTake :: StackId -> Natural -> Term Natural
+    stdlibStackTake sn n = foldr1 (#) (take (fromIntegral n) [OpAddress # indexInStack sn i | i <- [0 ..]])
+
+asmReturn' :: (Members '[Output (Term Natural), Reader FunctionPaths] r) => Sem r ()
+asmReturn' = do
+  output (replaceStack ValueStack ((OpAddress # indexInStack ValueStack 0) # (OpAddress # indexInStack CallStack 0)))
+  -- discard the 'activation' frame
+  output (popStack CallStack)
+  output (popStack CurrentFunction)
+
+branch' ::
+  (Functor f, Member (Output (Term Natural)) r, Member (Reader FunctionPaths) r) =>
+  m () ->
+  m () ->
+  Sem (WithTactics Compiler f m r) (f ())
+branch' t f = do
+  termT <- runT t >>= raise . execCompiler . (pop >>)
+  termF <- runT f >>= raise . execCompiler . (pop >>)
+  (output >=> pureT) (OpIf # (OpAddress # pathInStack ValueStack [L]) # termT # termF)
+
+pushArgRef' :: (Members '[Output (Term Natural), Reader FunctionPaths] r) => Offset -> Sem r ()
+pushArgRef' = undefined
+
+pushTempRef' :: (Members '[Output (Term Natural), Reader FunctionPaths] r) => Offset -> Sem r ()
+pushTempRef' = undefined
 
 re :: (Member (Reader FunctionPaths) r) => Sem (Compiler ': r) a -> Sem (Output (Term Natural) ': r) a
 re = reinterpretH $ \case
   PushOnto s n -> outputT (pushOnStack s n)
   PopFrom s -> outputT (popStack s)
   Verbatim s -> outputT s
-  Call funName funArgsNum -> (pureT =<<) $ do
-    -- 1. Obtain the path to the function
-    -- 2.
-    --   i. Take a copy of the value stack without the arguments to the function
-    --   ii. Push this copy to the call stack
-    -- 3.
-    --  i. Take a copy of the function from the function library
-    --  ii. Replace its argument area with the arguments from the value stack
-    --  iii. Push this copy to the current function stack
-    -- 4. Replace the value stack with nil
-    -- 5. Evaluate the function in the context of the whole nock stack
-    --    The return instruction in the function compiles to:
-    --      i. Restore the previous value stack (created in 2.). i.e copy the previous value stack
-    --         from the call stack and push the result (the head of the current value stack) to it.
-    --      i. Pop the call stack and the current function stack.
-
-    funPath <- fromJust <$> asks @FunctionPaths (^. at funName)
-
-    -- Save the current state of the value stack
-    output (pushOnStack CallStack (OpAddress # pathInStack ValueStack (replicate funArgsNum R)))
-
-    -- Setup function to call with it arguments
-    output
-      ( pushOnStack
-          CurrentFunction
-          ( OpReplace
-              #
-              -- TODO: copy funArgs elements from the value stack
-              ( ([R] # stackTake ValueStack funArgsNum)
-                  # OpAddress
-                  # funPath
-              )
-          )
-      )
-
-    -- Init 'activation frame'
-    output (resetStack ValueStack)
-
-    -- call the function
-    output (OpCall # ((indexInStack CurrentFunction 0 ++ [L]) # (OpAddress # emptyPath)))
-  IncrementOn s -> (pureT =<<) $ do
-    output
-      ( replaceOnStack
-          s
-          (OpInc # (OpAddress # pathInStack s [L]))
-      )
-  Branch t f -> do
-    termT <- runT t >>= raise . execCompiler . (pop >>)
-    termF <- runT f >>= raise . execCompiler . (pop >>)
-    outputT (OpIf # (OpAddress # pathInStack ValueStack [L]) # termT # termF)
-  CallStdlib f -> (pureT =<<) $ do
-    let fNumArgs = stdlibNumArgs f
-        fPath = stdlibPath f
-        decodeFn = OpCall # (fPath # (OpAddress # stackPath StandardLibrary))
-        arguments = OpSequence # (OpAddress # [R]) # stdlibStackTake ValueStack fNumArgs
-        extractResult = (OpAddress # [L]) # (OpAddress # [R, R])
-        callFn = OpPush # (OpCall # [L] # (OpReplace # ([R, L] # arguments) # (OpAddress # [L]))) # extractResult
-
-    output (OpPush # decodeFn # callFn)
-    output (replaceTopStackN fNumArgs ValueStack)
-    where
-      stdlibStackTake :: StackId -> Natural -> Term Natural
-      stdlibStackTake sn n = foldr1 (#) (take (fromIntegral n) [OpAddress # indexInStack sn i | i <- [0 ..]])
-  AsmReturn -> (pureT =<<) $ do
-    output (replaceStack ValueStack ((OpAddress # indexInStack ValueStack 0) # (OpAddress # indexInStack CallStack 0)))
-    -- discard the 'activation' frame
-    output (popStack CallStack)
-    output (popStack CurrentFunction)
+  Call funName funArgsNum -> call' funName funArgsNum >>= pureT
+  PushArgRef off -> pushArgRef' off >>= pureT
+  PushTempRef off -> pushTempRef' off >>= pureT
+  IncrementOn s -> incrementOn' s >>= pureT
+  Branch t f -> branch' t f
+  CallStdlib f -> callStdlib' f >>= pureT
+  AsmReturn -> asmReturn' >>= pureT
   where
     outputT :: (Functor f, Member (Output (Term Natural)) r) => Term Natural -> Sem (WithTactics e f m r) (f ())
     outputT = pureT <=< output
--- test/Nockma/Compile/Positive.hs
+++ test/Nockma/Compile/Positive.hs
@@ -3,29 +3,24 @@
 module Nockma.Compile.Positive where
 
 import Base
+import Juvix.Compiler.Nockma.Evaluator
 import Juvix.Compiler.Nockma.Language
 import Juvix.Compiler.Nockma.Pretty
 import Juvix.Compiler.Nockma.Translation.FromAsm
 import Juvix.Compiler.Nockma.Translation.FromSource.QQ
 
+type Check = Sem '[Reader (Term Natural), Embed IO]
+
 data Test = Test
   { _testName :: Text,
-    _testExpected :: Term Natural,
+    _testCheck :: Check (),
     _testProgram :: Sem '[Compiler, Embed IO] ()
   }
 
 makeLenses ''Test
 
-debugProgPrint :: Sem '[Compiler, Embed IO] () -> IO ()
-debugProgPrint = debugProg >=> putStrLn . ppTrace
-
 debugProg :: Sem '[Compiler, Embed IO] () -> IO (Term Natural)
-debugProg m = runM $ runCompiledNock exampleFunctions takeValueStack
-  where
-    takeValueStack :: Sem '[Compiler, Embed IO] ()
-    takeValueStack = do
-      m
-      verbatim (OpAddress # stackPath ValueStack)
+debugProg = runM . runCompiledNock exampleFunctions
 
 exampleFunctions :: [CompilerFunction]
 exampleFunctions =
@@ -40,49 +35,59 @@ allTests = testGroup "Nockma compile unit positive" (map mk tests)
     mk :: Test -> TestTree
     mk Test {..} = testCase (unpack _testName) $ do
       n <- debugProg _testProgram
-      unless (n == _testExpected) (err n)
-      where
-        err :: Term Natural -> IO ()
-        err n = do
-          let msg =
-                "Expected:\n"
-                  <> ppTrace _testExpected
-                  <> "But got:\n"
-                  <> ppTrace n
-          assertFailure (unpack msg)
+      runM (runReader n _testCheck)
+
+getStack :: StackId -> Term Natural -> Term Natural
+getStack st m = fromRight' (run (runError @NockEvalError (subTerm m (stackPath st))))
+
+eqStack :: StackId -> Term Natural -> Check ()
+eqStack st expected = do
+  n <- getStack st <$> ask
+  unless (n == expected) (err n)
+  where
+    err :: Term Natural -> Check ()
+    err n = do
+      let msg =
+            "Expected: "
+              <> show st
+              <> "\n"
+              <> ppTrace expected
+              <> "But got:\n"
+              <> ppTrace n
+      assertFailure (unpack msg)
 
 tests :: [Test]
 tests =
-  [ Test "push" [nock| [1 5 0] |] $ do
+  [ Test "push" (eqStack ValueStack [nock| [1 5 0] |]) $ do
       pushNat 5
       pushNat 1,
-    Test "pop" [nock| [1 0] |] $ do
+    Test "pop" (eqStack ValueStack [nock| [1 0] |]) $ do
       pushNat 1
       pushNat 33
       pop,
-    Test "increment" [nock| [3 0] |] $ do
+    Test "increment" (eqStack ValueStack [nock| [3 0] |]) $ do
       pushNat 1
       increment
       increment,
-    Test "dec" [nock| [5 0] |] $ do
+    Test "dec" (eqStack ValueStack [nock| [5 0] |]) $ do
       pushNat 6
       dec,
-    Test "branch true" [nock| [5 0] |] $ do
+    Test "branch true" (eqStack ValueStack [nock| [5 0] |]) $ do
       push nockTrueLiteral
       branch (pushNat 5) (pushNat 666),
-    Test "branch false" [nock| [666 0] |] $ do
+    Test "branch false" (eqStack ValueStack [nock| [666 0] |]) $ do
       push nockFalseLiteral
       branch (pushNat 5) (pushNat 666),
-    Test "add" [nock| [5 0] |] $ do
+    Test "add" (eqStack ValueStack [nock| [5 0] |]) $ do
       pushNat 2
       pushNat 3
       add,
-    Test "call increment" [nock| [5 0] |] $ do
+    Test "call increment" (eqStack ValueStack [nock| [5 0] |]) $ do
       pushNat 2
       call "increment" 1
       call "increment" 1
       call "increment" 1,
-    Test "call increment indirectly" [nock| [5 0] |] $ do
+    Test "call increment indirectly" (eqStack ValueStack [nock| [5 0] |]) $ do
       pushNat 2
       call "increment" 1
       call "callInc" 1
