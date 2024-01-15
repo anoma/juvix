346d9e0392e745b572fd710c47302b6eaf01c6c6
Lukasz Czajka <lukasz@heliax.dev>
2024-01-15 10:40:39 +0100

    use

--- src/Juvix/Compiler/Asm/Transformation/TempHeight.hs
+++ src/Juvix/Compiler/Asm/Transformation/TempHeight.hs
@@ -1,6 +1,5 @@
 module Juvix.Compiler.Asm.Transformation.TempHeight where
 
-import Juvix.Compiler.Asm.Data.Stack
 import Juvix.Compiler.Asm.Transformation.Base
 
 computeFunctionTempHeight ::
@@ -9,10 +8,10 @@ computeFunctionTempHeight ::
   FunctionInfo ->
   Sem r FunctionInfo
 computeFunctionTempHeight tab fi = do
-  ps :: [Command] <- recurseFun sig fi
+  ps :: [Command] <- recurseS sig (fi ^. functionCode)
   return (set functionCode ps fi)
   where
-    sig :: RecursorSig Memory r Command
+    sig :: RecursorSig StackInfo r Command
     sig =
       RecursorSig
         { _recursorInfoTable = tab,
@@ -22,15 +21,15 @@ computeFunctionTempHeight tab fi = do
           _recurseSave = goSave
         }
 
-    goInstr :: Memory -> CmdInstr -> Sem r Command
-    goInstr mem cmd@(CmdInstr _ instr) = case instr of
+    goInstr :: StackInfo -> CmdInstr -> Sem r Command
+    goInstr si cmd@(CmdInstr _ instr) = case instr of
       Push (Ref (DRef (TempRef r))) ->
-        let h = mem ^. memoryTempStack . stackHeight
+        let h = si ^. stackInfoTempStackHeight
             r' = set refTempTempHeight (Just h) r
             instr' = Push (Ref (DRef (TempRef r')))
          in return (Instr (set cmdInstrInstruction instr' cmd))
       Push (Ref (ConstrRef field@Field {_fieldRef = TempRef r})) ->
-        let h = mem ^. memoryTempStack . stackHeight
+        let h = si ^. stackInfoTempStackHeight
             r' = set refTempTempHeight (Just h) r
             instr' =
               Push
@@ -44,7 +43,7 @@ computeFunctionTempHeight tab fi = do
          in return (Instr (set cmdInstrInstruction instr' cmd))
       _ -> return (Instr cmd)
 
-    goCase :: Memory -> CmdCase -> [Code] -> Maybe Code -> Sem r Command
+    goCase :: StackInfo -> CmdCase -> [Code] -> Maybe Code -> Sem r Command
     goCase _ cmd brs mdef =
       return
         ( Case
@@ -60,7 +59,7 @@ computeFunctionTempHeight tab fi = do
             | (oldBr, newCode) <- zipExact (cmd ^. cmdCaseBranches) brs
           ]
 
-    goBranch :: Memory -> CmdBranch -> Code -> Code -> Sem r Command
+    goBranch :: StackInfo -> CmdBranch -> Code -> Code -> Sem r Command
     goBranch _ cmd t f =
       return
         ( Branch
@@ -69,7 +68,7 @@ computeFunctionTempHeight tab fi = do
                 _cmdBranchFalse = f
               }
         )
-    goSave :: Memory -> CmdSave -> Code -> Sem r Command
+    goSave :: StackInfo -> CmdSave -> Code -> Sem r Command
     goSave _ cmd code = return (Save (set cmdSaveCode code cmd))
 
 computeTempHeight :: (Member (Error AsmError) r) => InfoTable -> Sem r InfoTable
