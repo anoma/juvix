bd513b10a325be0fc143ba152801a1d651c20db5
Paul Cadman <git@paulcadman.dev>
2024-01-16 16:50:29 +0000

    Add description for nockma-stdlib-parser script

--- scripts/nockma-stdlib-parser.sh
+++ scripts/nockma-stdlib-parser.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+#
+# Description:
+# A script that extracts the Nock locations of functions from comments in the the
+# anoma.hoon standard library:
+#
+# For example we want to extract the location "342" for the function dec.:
+# https://github.com/anoma/anoma/blob/9904ff81218c1a690027a481beb0b6d39e378a07/hoon/anoma.hoon#L12
+# ```
+# ++  dec  ::  +342
+# ```
+#
 # Usage:
 # chmod +x nockma-stdlib-parser.sh
-# ./nockma-stdlib-parser < anoma.hoon
+# ./nockma-stdlib-parser.sh < anoma.hoon
 
 # Use grep to find lines matching the pattern and awk to format the output
 # Reads from stdin
