5f224005b4a78a79e5f84ff4f9e78152bc501161
Lukasz Czajka <lukasz@heliax.dev>
2024-01-26 12:54:34 +0100

    options for read

--- app/Commands/Dev/Tree/Read.hs
+++ app/Commands/Dev/Tree/Read.hs
@@ -2,8 +2,11 @@ module Commands.Dev.Tree.Read where
 
 import Commands.Base
 import Commands.Dev.Tree.Read.Options
+import Juvix.Compiler.Tree.Data.InfoTable qualified as Tree
 import Juvix.Compiler.Tree.Pretty qualified as Tree
+import Juvix.Compiler.Tree.Transformation qualified as Tree
 import Juvix.Compiler.Tree.Translation.FromSource qualified as Tree
+import TreeEvaluator qualified as Eval
 
 runCommand :: forall r. (Members '[Embed IO, App] r) => TreeReadOptions -> Sem r ()
 runCommand opts = do
@@ -11,7 +14,20 @@ runCommand opts = do
   s <- readFile (toFilePath afile)
   case Tree.runParser (toFilePath afile) s of
     Left err -> exitJuvixError (JuvixError err)
-    Right tab -> renderStdOut (Tree.ppOutDefault tab tab)
+    Right tab -> do
+      tab' <- Tree.applyTransformations (project opts ^. treeReadTransformations) tab
+      unless (project opts ^. treeReadNoPrint) $
+        renderStdOut (Tree.ppOutDefault tab' tab')
+      doEval tab'
   where
     file :: AppPath File
     file = opts ^. treeReadInputFile
+
+    doEval :: Tree.InfoTable -> Sem r ()
+    doEval tab'
+      | project opts ^. treeReadEval = do
+          putStrLn "--------------------------------"
+          putStrLn "|            Eval              |"
+          putStrLn "--------------------------------"
+          Eval.evalTree tab'
+      | otherwise = return ()
