6649209d26937476a65d21bd6b1c19a109881b32
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2024-01-27 08:54:57 +0100

    Add the `juvix dev tree compile` command (#2590)
    * Adds the `juvix dev tree compile` CLI command.
    * Depends on #2589 
    * Depends on #2587 
    * Depends on #2583

---
+++ app/Commands/Dev/Tree/Compile/Base.hs
@@ -0,0 +1,98 @@
+module Commands.Dev.Tree.Compile.Base where
+
+import Commands.Base
+import Commands.Dev.Tree.Compile.Options
+import Commands.Extra.Compile qualified as Compile
+import Juvix.Compiler.Asm.Pretty qualified as Asm
+import Juvix.Compiler.Backend qualified as Backend
+import Juvix.Compiler.Backend.C qualified as C
+import Juvix.Compiler.Nockma.Pretty qualified as Nockma
+import Juvix.Compiler.Tree.Data.InfoTable qualified as Tree
+
+data PipelineArg = PipelineArg
+  { _pipelineArgOptions :: CompileOptions,
+    _pipelineArgFile :: Path Abs File,
+    _pipelineArgTable :: Tree.InfoTable
+  }
+
+getEntry :: (Members '[Embed IO, App, TaggedLock] r) => PipelineArg -> Sem r EntryPoint
+getEntry PipelineArg {..} = do
+  ep <- getEntryPoint (AppPath (preFileFromAbs _pipelineArgFile) True)
+  return $
+    ep
+      { _entryPointTarget = getTarget (_pipelineArgOptions ^. compileTarget),
+        _entryPointDebug = _pipelineArgOptions ^. compileDebug,
+        _entryPointUnsafe = _pipelineArgOptions ^. compileUnsafe,
+        _entryPointOptimizationLevel = fromMaybe defaultOptLevel (_pipelineArgOptions ^. compileOptimizationLevel),
+        _entryPointInliningDepth = _pipelineArgOptions ^. compileInliningDepth
+      }
+  where
+    getTarget :: CompileTarget -> Backend.Target
+    getTarget = \case
+      TargetWasm32Wasi -> Backend.TargetCWasm32Wasi
+      TargetNative64 -> Backend.TargetCNative64
+      TargetGeb -> Backend.TargetGeb
+      TargetVampIR -> Backend.TargetVampIR
+      TargetCore -> Backend.TargetCore
+      TargetAsm -> Backend.TargetAsm
+      TargetTree -> Backend.TargetTree
+      TargetNockma -> Backend.TargetNockma
+
+    defaultOptLevel :: Int
+    defaultOptLevel
+      | _pipelineArgOptions ^. compileDebug = 0
+      | otherwise = defaultOptimizationLevel
+
+runCPipeline ::
+  forall r.
+  (Members '[Embed IO, App, TaggedLock] r) =>
+  PipelineArg ->
+  Sem r ()
+runCPipeline pa@PipelineArg {..} = do
+  entryPoint <- getEntry pa
+  C.MiniCResult {..} <-
+    getRight
+      . run
+      . runReader entryPoint
+      . runError @JuvixError
+      $ treeToMiniC _pipelineArgTable
+  cFile <- inputCFile _pipelineArgFile
+  embed @IO (writeFile (toFilePath cFile) _resultCCode)
+  outfile <- Compile.outputFile _pipelineArgOptions _pipelineArgFile
+  Compile.runCommand
+    _pipelineArgOptions
+      { _compileInputFile = Just (AppPath (preFileFromAbs cFile) False),
+        _compileOutputFile = Just (AppPath (preFileFromAbs outfile) False)
+      }
+  where
+    inputCFile :: Path Abs File -> Sem r (Path Abs File)
+    inputCFile inputFileCompile = do
+      buildDir <- askBuildDir
+      ensureDir buildDir
+      return (buildDir <//> replaceExtension' ".c" (filename inputFileCompile))
+
+runAsmPipeline :: (Members '[Embed IO, App, TaggedLock] r) => PipelineArg -> Sem r ()
+runAsmPipeline pa@PipelineArg {..} = do
+  entryPoint <- getEntry pa
+  asmFile <- Compile.outputFile _pipelineArgOptions _pipelineArgFile
+  r <-
+    runReader entryPoint
+      . runError @JuvixError
+      . treeToAsm
+      $ _pipelineArgTable
+  tab' <- getRight r
+  let code = Asm.ppPrint tab' tab'
+  embed @IO (writeFile (toFilePath asmFile) code)
+
+runNockmaPipeline :: (Members '[Embed IO, App, TaggedLock] r) => PipelineArg -> Sem r ()
+runNockmaPipeline pa@PipelineArg {..} = do
+  entryPoint <- getEntry pa
+  nockmaFile <- Compile.outputFile _pipelineArgOptions _pipelineArgFile
+  r <-
+    runReader entryPoint
+      . runError @JuvixError
+      . treeToNockma
+      $ _pipelineArgTable
+  tab' <- getRight r
+  let code = Nockma.ppSerialize tab'
+  embed @IO (writeFile (toFilePath nockmaFile) code)
---
+++ app/Commands/Dev/Tree/Compile/Options.hs
@@ -0,0 +1,22 @@
+module Commands.Dev.Tree.Compile.Options
+  ( module Commands.Dev.Tree.Compile.Options,
+    module Commands.Extra.Compile.Options,
+  )
+where
+
+import Commands.Extra.Compile.Options
+import CommonOptions
+
+treeSupportedTargets :: NonEmpty CompileTarget
+treeSupportedTargets =
+  nonEmpty'
+    [ TargetWasm32Wasi,
+      TargetNative64,
+      TargetAsm
+    ]
+
+parseTreeCompileOptions :: Parser CompileOptions
+parseTreeCompileOptions =
+  parseCompileOptions
+    treeSupportedTargets
+    (parseInputFile FileExtJuvixTree)
