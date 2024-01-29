21a5568985514adf97a5ed1307deabecd5fd6af5
Jan Mas Rovira <janmasrovira@gmail.com>
2024-01-29 13:36:53 +0100

    Add `dev nockma format` command (#2593)
    Useful for formatting nockma code. 
    Usage:
    ```
    juvix dev nockma format input.nockma
    ```
    The formatted code is printed to stdout.

--- app/Commands/Dev/Nockma.hs
+++ app/Commands/Dev/Nockma.hs
@@ -1,11 +1,13 @@
 module Commands.Dev.Nockma where
 
 import Commands.Base
-import Commands.Dev.Nockma.Eval as FromAsm
+import Commands.Dev.Nockma.Eval as Eval
+import Commands.Dev.Nockma.Format as Format
 import Commands.Dev.Nockma.Options
 import Commands.Dev.Nockma.Repl as Repl
 
 runCommand :: forall r. (Members '[Embed IO, App] r) => NockmaCommand -> Sem r ()
 runCommand = \case
   NockmaRepl opts -> Repl.runCommand opts
-  NockmaEval opts -> FromAsm.runCommand opts
+  NockmaEval opts -> Eval.runCommand opts
+  NockmaFormat opts -> Format.runCommand opts
---
+++ app/Commands/Dev/Nockma/Format.hs
@@ -0,0 +1,17 @@
+module Commands.Dev.Nockma.Format where
+
+import Commands.Base hiding (Atom)
+import Commands.Dev.Nockma.Format.Options
+import Juvix.Compiler.Nockma.Pretty
+import Juvix.Compiler.Nockma.Translation.FromSource qualified as Nockma
+
+runCommand :: forall r. (Members '[Embed IO, App] r) => NockmaFormatOptions -> Sem r ()
+runCommand opts = do
+  afile <- fromAppPathFile file
+  parsedTerm <- Nockma.parseTermFile (toFilePath afile)
+  case parsedTerm of
+    Left err -> exitJuvixError (JuvixError err)
+    Right t -> putStrLn (ppPrint t)
+  where
+    file :: AppPath File
+    file = opts ^. nockmaFormatFile
