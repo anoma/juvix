ff94a32ce06428242ff82769e61b9c67845fc284
Jan Mas Rovira <janmasrovira@gmail.com>
2024-02-03 09:57:55 +0100

    use defaultModuleId

--- src/Juvix/Compiler/Pipeline/Artifacts.hs
+++ src/Juvix/Compiler/Pipeline/Artifacts.hs
@@ -63,7 +63,7 @@ runNameIdGenArtifacts ::
   (Members '[State Artifacts] r) =>
   Sem (NameIdGen ': r) a ->
   Sem r a
-runNameIdGenArtifacts = runStateLikeArtifacts (`runNameIdGen` (error "TODO" :: ModuleId)) artifactNameIdState
+runNameIdGenArtifacts = runStateLikeArtifacts (`runNameIdGen` defaultModuleId) artifactNameIdState
 
 runFunctionsTableArtifacts :: (Members '[State Artifacts] r) => Sem (State FunctionsTable ': r) a -> Sem r a
 runFunctionsTableArtifacts = runStateArtifacts artifactFunctions
