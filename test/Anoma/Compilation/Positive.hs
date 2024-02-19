085d03d3264e741632024ee6d38fa45c46ac447b
Paul Cadman <git@paulcadman.dev>
2024-02-19 19:37:59 +0000

    Use helper functions to check output

--- test/Anoma/Compilation/Positive.hs
+++ test/Anoma/Compilation/Positive.hs
@@ -34,6 +34,16 @@ mkAnomaCallTestNoTrace = mkAnomaCallTest' False
 mkAnomaCallTest :: Text -> Prelude.Path Rel Dir -> Prelude.Path Rel File -> [Term Natural] -> Check () -> TestTree
 mkAnomaCallTest = mkAnomaCallTest' True
 
+checkNatOutput :: [Natural] -> Check ()
+checkNatOutput = checkOutput . fmap toNock
+
+checkOutput :: [Term Natural] -> Check ()
+checkOutput expected = case unsnoc expected of
+  Nothing -> return ()
+  Just (xs, x) -> do
+    eqTraces xs
+    eqNock x
+
 allTests :: TestTree
 allTests =
   testGroup
@@ -43,37 +53,37 @@ allTests =
         $(mkRelDir ".")
         $(mkRelFile "test001.juvix")
         [nockNatLiteral 5]
-        (eqNock [nock| 11 |]),
+        (checkNatOutput [11]),
       mkAnomaCallTest
         "Test002: Arithmetic operators inside lambdas"
         $(mkRelDir ".")
         $(mkRelFile "test002.juvix")
         [nockNatLiteral 2]
-        (eqNock [nock| 11 |]),
+        (checkNatOutput [11]),
       mkAnomaCallTest
         "Test003: Integer arithmetic"
         $(mkRelDir ".")
         $(mkRelFile "test003.juvix")
         []
-        (eqTraces [[nock| 1 |], [nock| 4 |], [nock| 2 |], [nock| 4 |]] >> eqNock [nock| 0 |]),
+        (checkNatOutput [1, 4, 2, 4, 0]),
       mkAnomaCallTestNoTrace
         "Test003: Integer arithmetic - no trace"
         $(mkRelDir ".")
         $(mkRelFile "test003.juvix")
         []
-        (eqTraces [] >> eqNock [nock| 0 |]),
+        (checkNatOutput [0]),
       mkAnomaCallTest
         "Test005: Higher-order functions"
         $(mkRelDir ".")
         $(mkRelFile "test005.juvix")
         [nockNatLiteral 1]
-        (eqNock [nock| 6 |]),
+        (checkNatOutput [6]),
       mkAnomaCallTest
         "Test006: If-then-else and lazy boolean operators"
         $(mkRelDir ".")
         $(mkRelFile "test006.juvix")
         []
-        (eqTraces [[nock| 2 |], [nock| true |]] >> eqNock [nock| false |]),
+        (checkOutput [[nock| 2 |], [nock| true |], [nock| false |]]),
       mkAnomaCallTest
         "Test007: Pattern matching and lambda-case"
         $(mkRelDir ".")
@@ -81,8 +91,7 @@ allTests =
         []
         $ do
           let l :: Term Natural = [nock| [1 2 nil] |]
-          eqTraces [[nock| false |], [nock| true |], [nock| 0 |], [nock| [1 nil] |], [nock| 1 |], l, l]
-          eqNock l,
+          checkOutput [[nock| false |], [nock| true |], [nock| 0 |], [nock| [1 nil] |], [nock| 1 |], l, l, l],
       mkAnomaCallTest
         "Test008: Recursion"
         $(mkRelDir ".")
@@ -94,13 +103,33 @@ allTests =
         $(mkRelDir ".")
         $(mkRelFile "test009.juvix")
         [nockNatLiteral 1000]
-        $ do
-          eqTraces [[nock| 500500 |], [nock| 120 |], [nock| 3628800 |]]
-          eqNock [nock| 479001600 |],
+        $ checkNatOutput [500500, 120, 3628800, 479001600],
       mkAnomaCallTest
         "Test010: Let"
         $(mkRelDir ".")
         $(mkRelFile "test010.juvix")
         []
-        (eqNock [nock| 32 |])
+        (checkNatOutput [32]),
+      mkAnomaCallTest
+        "Test011: Tail recursion: Fibonacci numbers in linear time"
+        $(mkRelDir ".")
+        $(mkRelFile "test011.juvix")
+        []
+        $ do
+          let fib10 :: Natural = 55
+              fib100 :: Natural = 354224848179261915075
+              fib1000 :: Natural = 43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875
+          checkNatOutput [fib10, fib100, fib1000],
+      mkAnomaCallTest
+        "Test012: Trees"
+        $(mkRelDir ".")
+        $(mkRelFile "test012.juvix")
+        [nockNatLiteral 1000]
+        $ checkNatOutput
+          [ 13200200200,
+            21320020020013200200200,
+            3213200200200132002002002132002002001320020020021320020020013200200200,
+            13213200200200132002002002132002002001320020020021320020020013200200200,
+            21321320020020013200200200213200200200132002002002132002002001320020020013213200200200132002002002132002002001320020020021320020020013200200200
+          ]
     ]
