5af3f0d12dc558c52a11a6844a6eb43adb5cfdde
Lukasz Czajka <lukasz@heliax.dev>
2024-03-08 19:34:47 +0100

    scripts to run Cairo VM and Stone prover

---
+++ scripts/run_cairo_vm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+BASE=`basename "$1" .json`
+
+cairo-vm-cli "$1" --print_output --proof_mode --trace_file ${BASE}.trace --air_public_input=${BASE}_public_input.json --air_private_input=${BASE}_private_input.json --memory_file=${BASE}_memory.mem --layout=small
