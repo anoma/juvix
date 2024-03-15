4447203f712a0f372896a262c6d78301672bc561
Jan Mas Rovira <janmasrovira@gmail.com>
2024-03-15 10:43:48 +0100

    define opReplace

--- test/Nockma/Base.hs
+++ test/Nockma/Base.hs
@@ -6,19 +6,21 @@ import Juvix.Compiler.Nockma.Translation.FromTree
 
 -- | Call a function at the head of the subject using the Anoma calling convention
 anomaCall :: Term Natural -> [Term Natural] -> Term Natural
-anomaCall env args = case nonEmpty args of
-  Just args' -> helper (Just (OpReplace # (closurePath ArgsTuple # foldTerms args')))
+anomaCall functionsLib args = case nonEmpty args of
+  Just args' -> helper (Just (opReplace "anomaCall-args" (closurePath ArgsTuple) (foldTerms args')))
   Nothing -> helper Nothing
   where
     helper replaceArgs =
       opCall
         "anomaCall"
         (closurePath WrapperCode)
-        ( OpReplace
-            # (closurePath FunctionsLibrary # OpQuote # env)
-            # (repArgs (OpAddress # emptyPath))
+        ( opReplace
+            "anomaCall-functionsLibrary"
+            (closurePath FunctionsLibrary)
+            (OpQuote # functionsLib)
+            (repArgs (OpAddress # emptyPath))
         )
       where
         repArgs x = case replaceArgs of
           Nothing -> x
-          Just r -> r # x
+          Just r -> r x
