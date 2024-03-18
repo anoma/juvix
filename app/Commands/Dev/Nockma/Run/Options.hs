30bcad8fffe84f3040f6a1a188752b628e83695b
Paul Cadman <git@paulcadman.dev>
2024-03-18 18:04:44 +0000

    Fix formatting

--- app/Commands/Dev/Nockma/Run/Options.hs
+++ app/Commands/Dev/Nockma/Run/Options.hs
@@ -16,25 +16,25 @@ parseNockmaRunOptions :: Parser NockmaRunOptions
 parseNockmaRunOptions = do
   _nockmaRunFile <- parseInputFile FileExtNockma
   _nockmaRunArgs <- optional $ do
-          _pathPath <-
-            option
-              somePreFileOpt
-              ( long "args"
-                  <> metavar "ARGS_FILE"
-                  <> help "Path to file containing args"
-                  <> action "file"
-              )
-          pure AppPath {_pathIsInput = True, ..}
+    _pathPath <-
+      option
+        somePreFileOpt
+        ( long "args"
+            <> metavar "ARGS_FILE"
+            <> help "Path to file containing args"
+            <> action "file"
+        )
+    pure AppPath {_pathIsInput = True, ..}
   _nockmaRunEnvFile <- do
-          _pathPath <-
-            option
-              somePreFileOpt
-              ( long "env"
-                  <> metavar "ENV_FILE"
-                  <> help "Path to file containing Juvix environment"
-                  <> action "file"
-              )
-          pure AppPath {_pathIsInput = True, ..}
+    _pathPath <-
+      option
+        somePreFileOpt
+        ( long "env"
+            <> metavar "ENV_FILE"
+            <> help "Path to file containing Juvix environment"
+            <> action "file"
+        )
+    pure AppPath {_pathIsInput = True, ..}
   _nockmaRunProfile <-
     switch
       ( long "profile"
