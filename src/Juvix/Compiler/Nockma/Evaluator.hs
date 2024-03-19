4c54c289d06169ea3739487c44852e53dec8b92a
Paul Cadman <git@paulcadman.dev>
2024-03-19 09:00:42 +0000

    Remove redundant import

--- src/Juvix/Compiler/Nockma/Evaluator.hs
+++ src/Juvix/Compiler/Nockma/Evaluator.hs
@@ -9,7 +9,6 @@ import Juvix.Compiler.Nockma.Evaluator.Error
 import Juvix.Compiler.Nockma.Evaluator.Options
 import Juvix.Compiler.Nockma.Language
 import Juvix.Prelude hiding (Atom, Path)
-import Juvix.Prelude.Pretty
 
 -- newtype OpCounts = OpCounts
 --   { _opCountsMap :: HashMap NockOp Int
