dc422049e8d9b897c209538c93debe022fe79f40
Lukasz Czajka <lukasz@heliax.dev>
2024-03-26 14:28:00 +0100

    tests

--- test/Casm/Reg/Cairo.hs
+++ test/Casm/Reg/Cairo.hs
@@ -21,6 +21,18 @@ allTests =
     "JuvixReg to Cairo translation positive tests"
     ( map (mkTest . testDescr) $
         P.filterOutTests
-          []
+          [ "Test001: Arithmetic opcodes",
+            "Test013: Fibonacci numbers in linear time",
+            "Test014: Trees",
+            "Test016: Arithmetic",
+            "Test017: Closures as arguments",
+            "Test023: McCarthy's 91 function",
+            "Test024: Higher-order recursive functions",
+            "Test027: Fast exponentiation",
+            "Test030: Mutual recursion",
+            "Test031: Temporary stack with branching",
+            "Test036: Streams without memoization",
+            "Test038: Apply & argsnum"
+          ]
           P.tests
     )
