59f9b49a772a860c0626a04d0351537372ef4220
Paul Cadman <git@paulcadman.dev>
2024-04-02 16:40:07 +0100

    Capture scry id in the main function and use it when compiling anomaGet  (#2705)
    The purpose of this PR is to wrap the compiled main function with Nockma
    code that captures the argument tuple for use when compiling `anomaGet`
    calls.
    
    
    * The [Anoma system
    expects](https://github.com/anoma/anoma/blob/c7f2d69d1efe2e5516a65b8f4af6576e0703dc87/lib/anoma/node/executor/worker.ex#L20)
    to receive a function of type `ScryId -> Transaction`
     
    * The ScryId is only used to construct the argument to the Scry
    operation (i.e the anomaGet builtin in the Juvix frontend),
     
    * When the Juvix developer writes a function to submit to Anoma they use
    type `() -> Transaction`, the main function wrapper is used to capture
    the ScryId argument into the subject which is then used to construct
    OpScry arguments when anomaGet is compiled.
     
    * If the Anoma system expectation changes then the wrapper code must be
    changed.
    
    We could add a transformation that checks that the main function in the
    Anoma target has no arguments. However it is convenient to be able to
    write functions with arguments for testing and debugging (for example
    compiling directly to a logic function).
    
    ---------
    
    Co-authored-by: Jan Mas Rovira <janmasrovira@gmail.com>

--- src/Juvix/Compiler/Nockma/Evaluator/Storage.hs
+++ src/Juvix/Compiler/Nockma/Evaluator/Storage.hs
@@ -1,12 +1,55 @@
 module Juvix.Compiler.Nockma.Evaluator.Storage where
 
 import Juvix.Compiler.Nockma.Language
+import Juvix.Compiler.Nockma.Pretty.Base
 import Juvix.Prelude.Base
 
 newtype Storage a = Storage
-  {_storageKeyValueData :: HashMap (Term a) (Term a)}
+  {_storageKeyValueData :: HashMap (StorageKey a) (Term a)}
 
-emptyStorage :: (Hashable a) => Storage a
-emptyStorage = Storage {_storageKeyValueData = mempty}
+newtype StorageKey a = StorageKey {_storageKeyTerm :: Term a}
 
 makeLenses ''Storage
+makeLenses ''StorageKey
+
+stripMeta :: Term a -> Term a
+stripMeta = \case
+  TermAtom a ->
+    TermAtom
+      Atom
+        { _atom = a ^. atom,
+          _atomInfo = emptyAtomInfo
+        }
+  TermCell c ->
+    TermCell
+      Cell'
+        { _cellLeft = stripMeta (c ^. cellLeft),
+          _cellRight = stripMeta (c ^. cellRight),
+          _cellInfo = emptyCellInfo
+        }
+
+instance (NockmaEq a) => NockmaEq (StorageKey a) where
+  nockmaEq (StorageKey s1) (StorageKey s2) = nockmaEq s1 s2
+
+instance (NockmaEq a) => Eq (StorageKey a) where
+  (==) = nockmaEq
+
+instance (NockmaEq a, Hashable a) => Hashable (StorageKey a) where
+  hashWithSalt salt k = goTerm (k ^. storageKeyTerm)
+    where
+      goCell :: Cell a -> Int
+      goCell c = hashWithSalt salt (c ^. cellLeft, c ^. cellRight)
+
+      goAtom :: Atom a -> Int
+      goAtom a = hashWithSalt salt (a ^. atom)
+
+      goTerm :: Term a -> Int
+      goTerm = \case
+        TermAtom a -> goAtom a
+        TermCell c -> goCell c
+
+instance (PrettyCode a, NockNatural a) => PrettyCode (StorageKey a) where
+  ppCode k = ppCode (stripMeta (k ^. storageKeyTerm))
+
+emptyStorage :: (NockmaEq a, Hashable a) => Storage a
+emptyStorage = Storage {_storageKeyValueData = mempty}
