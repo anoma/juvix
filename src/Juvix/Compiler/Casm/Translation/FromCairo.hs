2e89e77bd3220e46d571bf9edec9a1407155aac1
Lukasz Czajka <lukasz@heliax.dev>
2024-04-08 10:38:36 +0200

    refactor

--- src/Juvix/Compiler/Casm/Translation/FromCairo.hs
+++ src/Juvix/Compiler/Casm/Translation/FromCairo.hs
@@ -21,60 +21,11 @@ fromCairo elems0 = Result mempty (go 0 [] elems0)
       Cairo.ElementHint {} : _ -> errorMsg addr "cannot disassemble a hint"
 
     goInstr :: Address -> Cairo.Instruction -> [Cairo.Element] -> (Instruction, Int)
-    goInstr addr i@Cairo.Instruction {..} elems = case _instrOpcode of
-      Cairo.Call
-        | (_instrPcUpdate == Cairo.PcUpdateJump || _instrPcUpdate == Cairo.PcUpdateJumpRel)
-            && _instrApUpdate == Cairo.ApUpdateRegular
-            && _instrDstReg == Ap
-            && _instrOffDst == 0 ->
-            (call, delta)
-        | otherwise ->
-            errorMsg addr ("invalid call: " <> show i)
-        where
-          call =
-            Call
-              InstrCall
-                { _instrCallRel = _instrPcUpdate == Cairo.PcUpdateJumpRel,
-                  _instrCallTarget = val
-                }
-          (rval, delta) = decodeRes
-          val = case rval of
-            Val v -> v
-            _ -> errorMsg addr ("invalid call: " <> show i)
-      Cairo.Ret
-        | _instrApUpdate == Cairo.ApUpdateRegular
-            && _instrPcUpdate == Cairo.PcUpdateJump
-            && _instrResLogic == Cairo.ResOp1
-            && _instrOp1Src == Cairo.Op1SrcFp
-            && _instrOffOp1 == -1
-            && _instrDstReg == Fp
-            && _instrOffDst == -2 ->
-            (Return, 0)
-        | otherwise ->
-            errorMsg addr ("invalid ret: " <> show i)
-      Cairo.AssertEq
-        | (_instrApUpdate == Cairo.ApUpdateInc || _instrApUpdate == Cairo.ApUpdateRegular)
-            && _instrPcUpdate == Cairo.PcUpdateRegular ->
-            (asng, delta)
-        | otherwise ->
-            errorMsg addr ("invalid assignment: " <> show i)
-        where
-          (rval, delta) = decodeRes
-          dst = decodeDst
-          asng =
-            Assign
-              InstrAssign
-                { _instrAssignResult = dst,
-                  _instrAssignValue = rval,
-                  _instrAssignIncAp = _instrApUpdate == Cairo.ApUpdateInc
-                }
-      Cairo.Nop -> case _instrPcUpdate of
-        Cairo.PcUpdateJnz -> undefined
-        Cairo.PcUpdateJump -> decodeJump False
-        Cairo.PcUpdateJumpRel -> decodeJump True
-        Cairo.PcUpdateRegular -> case _instrApUpdate of
-          Cairo.ApUpdateAdd -> undefined
-          _ -> errorMsg addr ("cannot disassemble: " <> show i)
+    goInstr addr instr1 elems1 = case instr1 ^. Cairo.instrOpcode of
+      Cairo.Call -> goCall instr1 elems1
+      Cairo.Ret -> goRet instr1 elems1
+      Cairo.AssertEq -> goAssertEq instr1 elems1
+      Cairo.Nop -> goNop instr1 elems1
       where
         getImm :: Cairo.Element -> Immediate
         getImm = \case
@@ -84,13 +35,13 @@ fromCairo elems0 = Result mempty (go 0 [] elems0)
         getOp1Val :: RValue -> Value
         getOp1Val = \case
           Val val -> val
-          _ -> errorMsg addr ("unexpected op1 load: " <> show i)
+          _ -> errorMsg addr ("unexpected op1 load: " <> show instr1)
 
-        decodeDst :: MemRef
-        decodeDst = MemRef _instrDstReg _instrOffDst
+        decodeDst :: Cairo.Instruction -> MemRef
+        decodeDst Cairo.Instruction {..} = MemRef _instrDstReg _instrOffDst
 
-        decodeRes :: (RValue, Int)
-        decodeRes = (res, delta)
+        decodeRes :: Cairo.Instruction -> [Cairo.Element] -> (RValue, Int)
+        decodeRes Cairo.Instruction {..} elems = (res, delta)
           where
             delta = if _instrOp1Src == Cairo.Op1SrcImm then 1 else 0
             op0 = MemRef _instrOp0Reg _instrOffOp0
@@ -104,14 +55,76 @@ fromCairo elems0 = Result mempty (go 0 [] elems0)
               Cairo.ResAdd -> Binop $ BinopValue FieldAdd op0 (getOp1Val op1)
               Cairo.ResMul -> Binop $ BinopValue FieldMul op0 (getOp1Val op1)
 
-        decodeJump :: Bool -> (Instruction, Int)
-        decodeJump isRel
+        goCall :: Cairo.Instruction -> [Cairo.Element] -> (Instruction, Int)
+        goCall i@Cairo.Instruction {..} elems
+          | (_instrPcUpdate == Cairo.PcUpdateJump || _instrPcUpdate == Cairo.PcUpdateJumpRel)
+              && _instrApUpdate == Cairo.ApUpdateRegular
+              && _instrDstReg == Ap
+              && _instrOffDst == 0 =
+              (call, delta)
+          | otherwise =
+              errorMsg addr ("invalid call: " <> show i)
+          where
+            call =
+              Call
+                InstrCall
+                  { _instrCallRel = _instrPcUpdate == Cairo.PcUpdateJumpRel,
+                    _instrCallTarget = val
+                  }
+            (rval, delta) = decodeRes i elems
+            val = case rval of
+              Val v -> v
+              _ -> errorMsg addr ("invalid call: " <> show i)
+
+        goRet :: Cairo.Instruction -> [Cairo.Element] -> (Instruction, Int)
+        goRet i@Cairo.Instruction {..} _
+          | _instrApUpdate == Cairo.ApUpdateRegular
+              && _instrPcUpdate == Cairo.PcUpdateJump
+              && _instrResLogic == Cairo.ResOp1
+              && _instrOp1Src == Cairo.Op1SrcFp
+              && _instrOffOp1 == -1
+              && _instrDstReg == Fp
+              && _instrOffDst == -2 =
+              (Return, 0)
+          | otherwise =
+              errorMsg addr ("invalid ret: " <> show i)
+
+        goAssertEq :: Cairo.Instruction -> [Cairo.Element] -> (Instruction, Int)
+        goAssertEq i@Cairo.Instruction {..} elems
+          | (_instrApUpdate == Cairo.ApUpdateInc || _instrApUpdate == Cairo.ApUpdateRegular)
+              && _instrPcUpdate == Cairo.PcUpdateRegular =
+              (asng, delta)
+          | otherwise =
+              errorMsg addr ("invalid assignment: " <> show i)
+          where
+            (rval, delta) = decodeRes i elems
+            dst = decodeDst i
+            asng =
+              Assign
+                InstrAssign
+                  { _instrAssignResult = dst,
+                    _instrAssignValue = rval,
+                    _instrAssignIncAp = _instrApUpdate == Cairo.ApUpdateInc
+                  }
+
+        goNop :: Cairo.Instruction -> [Cairo.Element] -> (Instruction, Int)
+        goNop i@Cairo.Instruction {..} elems =
+          case _instrPcUpdate of
+            Cairo.PcUpdateJnz -> undefined
+            Cairo.PcUpdateJump -> goJump False i elems
+            Cairo.PcUpdateJumpRel -> goJump True i elems
+            Cairo.PcUpdateRegular -> case _instrApUpdate of
+              Cairo.ApUpdateAdd -> undefined
+              _ -> errorMsg addr ("cannot disassemble: " <> show i)
+
+        goJump :: Bool -> Cairo.Instruction -> [Cairo.Element] -> (Instruction, Int)
+        goJump isRel i@Cairo.Instruction {..} elems
           | (_instrApUpdate == Cairo.ApUpdateInc || _instrApUpdate == Cairo.ApUpdateRegular) =
               (jmp, delta)
           | otherwise =
               errorMsg addr ("invalid jump: " <> show i)
           where
-            (res, delta) = decodeRes
+            (res, delta) = decodeRes i elems
             jmp =
               Jump
                 InstrJump
