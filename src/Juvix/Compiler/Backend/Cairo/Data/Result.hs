22c5a460613114df51f104ee37c30b90a9ed3cca
Lukasz Czajka <lukasz@heliax.dev>
2024-04-08 13:30:19 +0200

    parse cairo json

--- src/Juvix/Compiler/Backend/Cairo/Data/Result.hs
+++ src/Juvix/Compiler/Backend/Cairo/Data/Result.hs
@@ -1,9 +1,12 @@
 module Juvix.Compiler.Backend.Cairo.Data.Result where
 
 import Data.Aeson as Aeson hiding (Result)
+import Data.Aeson.Key qualified as Aeson
+import Data.Aeson.KeyMap qualified as KeyMap
 import Data.Aeson.Types hiding (Result)
 import Data.Vector qualified as V
 import Juvix.Prelude hiding ((.=))
+import Text.Read qualified as T
 
 data Result = Result
   { _resultData :: [Text],
@@ -67,3 +70,55 @@ instance ToJSON Result where
                       "reference_ids" .= object []
                     ]
               ]
+
+instance FromJSON Result where
+  parseJSON val = case val of
+    Object obj -> do
+      _resultData <- maybe (return []) parseJSON (KeyMap.lookup "data" obj)
+      _resultBuiltins <- maybe (return []) parseJSON (KeyMap.lookup "builtins" obj)
+      (_resultStart, _resultEnd, _resultMain) <- maybe (return (0, 0, 0)) parseIdents (KeyMap.lookup "identifiers" obj)
+      _resultHints <- maybe (return []) parseHints (KeyMap.lookup "hints" obj)
+      return Result {..}
+    _ ->
+      typeMismatch "Object" val
+    where
+      parseIdents :: Value -> Parser (Int, Int, Int)
+      parseIdents val' = case val' of
+        Object obj -> do
+          startPc <- maybe (return 0) parseIdentPc (KeyMap.lookup "__main__.__start__" obj)
+          endPc <- maybe (return 0) parseIdentPc (KeyMap.lookup "__main__.__end__" obj)
+          mainPc <- maybe (return 0) parseIdentPc (KeyMap.lookup "__main__.main" obj)
+          return (startPc, endPc, mainPc)
+        _ ->
+          typeMismatch "Object" val'
+
+      parseIdentPc :: Value -> Parser Int
+      parseIdentPc val' = case val' of
+        Object obj ->
+          maybe (return 0) parseJSON (KeyMap.lookup "pc" obj)
+        _ ->
+          typeMismatch "Object" val'
+
+      parseHints :: Value -> Parser [(Int, Text)]
+      parseHints val' = case val' of
+        Object obj -> do
+          lst <-
+            mapM
+              ( \(k, v) -> do
+                  v' <- parseHint v
+                  return (Aeson.toText k, v')
+              )
+              (KeyMap.toList obj)
+          mapM (firstM (maybe (typeMismatch "Integer" val') return . T.readMaybe . unpack)) lst
+        _ ->
+          typeMismatch "Object" val'
+
+      parseHint :: Value -> Parser Text
+      parseHint val' = case val' of
+        Array arr -> case toList arr of
+          [Object obj] ->
+            maybe (return "") parseJSON (KeyMap.lookup "code" obj)
+          _ ->
+            typeMismatch "singleton Array" val'
+        _ ->
+          typeMismatch "singleton Array" val'
