d3dc2a5c8400fc7a015d028f94bf7fe1275b4249
Jan Mas Rovira <janmasrovira@gmail.com>
2024-04-16 18:26:30 +0200

    rename S to Serial to improve readability

--- src/Juvix/Extra/Serialize.hs
+++ src/Juvix/Extra/Serialize.hs
@@ -3,7 +3,7 @@
 
 {-# HLINT ignore "Avoid restricted flags" #-}
 module Juvix.Extra.Serialize
-  ( module S,
+  ( module Serial,
     saveToFile,
     loadFromFile,
   )
@@ -11,7 +11,7 @@ where
 
 import Data.HashMap.Strict qualified as HashMap
 import Data.HashSet qualified as HashSet
-import Data.Serialize as S
+import Data.Serialize as Serial
 import Juvix.Data.Effect.Files
 import Juvix.Data.Effect.TaggedLock
 import Juvix.Prelude.Base
@@ -22,26 +22,26 @@ instance Serialize (Path Abs File)
 instance Serialize (Path Abs Dir)
 
 instance Serialize Text where
-  put txt = S.put (unpack txt)
+  put txt = Serial.put (unpack txt)
 
-  get = pack <$> S.get
+  get = pack <$> Serial.get
 
 instance (Serialize a) => Serialize (NonEmpty a)
 
 instance (Hashable k, Serialize k, Serialize a) => Serialize (HashMap k a) where
-  put m = S.put (HashMap.toList m)
+  put m = Serial.put (HashMap.toList m)
 
-  get = HashMap.fromList <$> S.get
+  get = HashMap.fromList <$> Serial.get
 
 instance (Hashable a, Serialize a) => Serialize (HashSet a) where
-  put s = S.put (HashSet.toList s)
+  put s = Serial.put (HashSet.toList s)
 
-  get = HashSet.fromList <$> S.get
+  get = HashSet.fromList <$> Serial.get
 
 saveToFile :: (Members '[Files, TaggedLock] r, Serialize a) => Path Abs File -> a -> Sem r ()
 saveToFile file a = withTaggedLockDir (parent file) $ do
   ensureDir' (parent file)
-  let bs = runPut (S.put a)
+  let bs = runPut (Serial.put a)
   writeFileBS file bs
 
 loadFromFile :: forall a r. (Members '[Files, TaggedLock] r, Serialize a) => Path Abs File -> Sem r (Maybe a)
@@ -50,7 +50,7 @@ loadFromFile file = withTaggedLockDir (parent file) $ do
   if
       | ex -> do
           bs <- readFileBS' file
-          case runGet (S.get @a) bs of
+          case runGet (Serial.get @a) bs of
             Left {} -> return Nothing
             Right a -> return (Just a)
       | otherwise ->
