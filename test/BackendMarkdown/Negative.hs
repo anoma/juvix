6f8b6f03179795a0d1ed9e897d4b25c5b2b12a8b
Paul Cadman <git@paulcadman.dev>
2024-04-17 14:39:18 +0100

    Add negative tests for extract-module-statements errors

--- test/BackendMarkdown/Negative.hs
+++ test/BackendMarkdown/Negative.hs
@@ -2,6 +2,9 @@ module BackendMarkdown.Negative where
 
 import Base
 import Juvix.Compiler.Backend.Markdown.Error
+import Juvix.Compiler.Backend.Markdown.Translation.FromTyped.Source
+import Juvix.Compiler.Concrete qualified as Concrete
+import Juvix.Compiler.Concrete.Translation.FromParsed.Analysis.Scoping qualified as Scoper
 import Juvix.Parser.Error
 
 type FailMsg = String
@@ -10,7 +13,7 @@ data NegTest = NegTest
   { _name :: String,
     _relDir :: Path Rel Dir,
     _file :: Path Rel File,
-    _checkErr :: ParserError -> Maybe FailMsg
+    _checkErr :: JuvixError -> Maybe FailMsg
   }
 
 testDescr :: NegTest -> TestDescr
@@ -22,12 +25,33 @@ testDescr NegTest {..} =
           _testRoot = tRoot,
           _testAssertion = Single $ do
             entryPoint <- testDefaultEntryPointIO tRoot file'
-            result <- testTaggedLockedToIO (runIOEither entryPoint upToParsing)
-            case mapLeft fromJuvixError result of
-              Left (Just err) -> whenJust (_checkErr err) assertFailure
-              Right _ -> assertFailure "Unexpected success."
-              Left Nothing -> assertFailure "Unexpected error."
+            result <- testTaggedLockedToIO (runIOEither entryPoint upToScoping)
+            case result of
+              Left err -> whenJust (_checkErr err) assertFailure
+              Right (_, pipelineRes) -> checkResult pipelineRes
         }
+  where
+    checkResult :: PipelineResult Scoper.ScoperResult -> IO ()
+    checkResult PipelineResult {..} = do
+      let m = _pipelineResult ^. Scoper.resultModule
+          opts =
+            ProcessJuvixBlocksArgs
+              { _processJuvixBlocksArgsConcreteOpts = Concrete.defaultOptions,
+                _processJuvixBlocksArgsUrlPrefix = "",
+                _processJuvixBlocksArgsIdPrefix = "",
+                _processJuvixBlocksArgsNoPath = True,
+                _processJuvixBlocksArgsExt = ".html",
+                _processJuvixBlocksArgsStripPrefix = "",
+                _processJuvixBlocksArgsComments = Comments mempty,
+                _processJuvixBlocksArgsFolderStructure = False,
+                _processJuvixBlocksArgsModule = m,
+                _processJuvixBlocksArgsOutputDir =
+                  root <//> $(mkRelDir "markdown")
+              }
+          res = fromJuvixMarkdown' opts
+      case res of
+        Left err -> whenJust (_checkErr (JuvixError err)) assertFailure
+        Right _ -> assertFailure "Unexpected success"
 
 allTests :: TestTree
 allTests =
@@ -38,8 +62,8 @@ allTests =
 root :: Path Abs Dir
 root = relToProject $(mkRelDir "tests/negative")
 
-wrongError :: Maybe FailMsg
-wrongError = Just "Incorrect error"
+wrongError :: JuvixError -> Maybe FailMsg
+wrongError e = Just ("unexpected error: " <> unpack (renderTextDefault e))
 
 tests :: [NegTest]
 tests =
@@ -47,14 +71,28 @@ tests =
       "Empty file"
       $(mkRelDir "Markdown")
       $(mkRelFile "Empty.juvix.md")
-      $ \case
-        ErrMarkdownBackend (ErrNoJuvixCodeBlocks _) -> Nothing
-        _ -> wrongError,
+      $ \e -> case fromJuvixError e of
+        Just (ErrMarkdownBackend (ErrNoJuvixCodeBlocks _)) -> Nothing
+        _ -> wrongError e,
     NegTest
       "No Juvix code blocks"
       $(mkRelDir "Markdown")
       $(mkRelFile "NoJuvixCodeBlocks.juvix.md")
-      $ \case
-        ErrMarkdownBackend (ErrNoJuvixCodeBlocks _) -> Nothing
-        _ -> wrongError
+      $ \e -> case fromJuvixError e of
+        Just (ErrMarkdownBackend (ErrNoJuvixCodeBlocks _)) -> Nothing
+        _ -> wrongError e,
+    NegTest
+      "extract-module-statements with no local module"
+      $(mkRelDir "Markdown")
+      $(mkRelFile "InvalidExtractModuleBlockNoModule.juvix.md")
+      $ \e -> case fromJuvixError e of
+        Just (ErrInvalidExtractModuleBlock _) -> Nothing
+        _ -> wrongError e,
+    NegTest
+      "extract-module-statements with no local module"
+      $(mkRelDir "Markdown")
+      $(mkRelFile "InvalidExtractModuleBlockNotJustModule.juvix.md")
+      $ \e -> case fromJuvixError e of
+        Just (ErrInvalidExtractModuleBlock _) -> Nothing
+        _ -> wrongError e
   ]
