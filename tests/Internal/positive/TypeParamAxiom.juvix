anoma/juvix

commit 75bce8f6654786c78661a3dd355b7d8215c33e1e

Per-module compilation (#2468)
* Closes #2392 

Changes checklist
-----------------
* [X] Abstract out data types for stored module representation
(`ModuleInfo` in `Juvix.Compiler.Store.Language`)
* [X] Adapt the parser to operate per-module
* [X] Adapt the scoper to operate per-module
* [X] Adapt the arity checker to operate per-module
* [X] Adapt the type checker to operate per-module
* [x] Adapt Core transformations to operate per-module
* [X] Adapt the pipeline functions in `Juvix.Compiler.Pipeline`
* [X] Add `Juvix.Compiler.Pipeline.Driver` which drives the per-module
compilation process
* [x] Implement module saving / loading in `Pipeline.Driver`
* [x] Detect cyclic module dependencies in `Pipeline.Driver`
* [x] Cache visited modules in memory in `Pipeline.Driver` to avoid
excessive disk operations and repeated hash re-computations
* [x] Recompile a module if one of its dependencies needs recompilation
and contains functions that are always inlined.
* [x] Fix identifier dependencies for mutual block creation in
`Internal.fromConcrete`
- Fixed by making textually later definitions depend on earlier ones.
- Now instances are used for resolution only after the textual point of
their definition.
- Similarly, type synonyms will be unfolded only after the textual point
of their definition.
* [x] Fix CLI
* [x] Fix REPL
* [x] Fix highlighting
* [x] Fix HTML generation
* [x] Adapt test suite

src/Juvix/Compiler/Internal/Pretty/Base.hs
@@ -6,7 +6,6 @@ module Juvix.Compiler.Internal.Pretty.Base
 where
 
 import Data.HashMap.Strict qualified as HashMap
-import Juvix.Compiler.Internal.Data.InfoTable.Base
 import Juvix.Compiler.Internal.Data.InstanceInfo (instanceInfoResult, instanceTableMap)
 import Juvix.Compiler.Internal.Data.LocalVars
 import Juvix.Compiler.Internal.Data.NameDependencyInfo
@@ -14,6 +13,7 @@ import Juvix.Compiler.Internal.Data.TypedHole
 import Juvix.Compiler.Internal.Language
 import Juvix.Compiler.Internal.Pretty.Options
 import Juvix.Compiler.Internal.Translation.FromInternal.Analysis.TypeChecking.CheckerNew.Arity qualified as New
+import Juvix.Compiler.Store.Internal.Data.InfoTable
 import Juvix.Data.CodeAnn
 import Juvix.Prelude
 
@@ -30,7 +30,7 @@ runPrettyCode :: (PrettyCode c) => Options -> c -> Doc Ann
 runPrettyCode opts = run . runReader opts . ppCode
 
 instance PrettyCode NameId where
-  ppCode (NameId k) = return (pretty k)
+  ppCode = return . pretty
 
 instance PrettyCode Name where
   ppCode n = do
@@ -274,7 +274,7 @@ instance PrettyCode PreLetStatement where
 
 instance PrettyCode Import where
   ppCode i = do
-    name' <- ppCode (i ^. importModule . moduleIxModule . moduleName)
+    name' <- ppCode (i ^. importModuleName)
     return $ kwImport <+> name'
 
 instance PrettyCode BuiltinAxiom where

app/Commands/Dev/Core/Compile/Base.hs
@@ -9,13 +9,13 @@ import Juvix.Compiler.Backend qualified as Backend
 import Juvix.Compiler.Backend.C qualified as C
 import Juvix.Compiler.Backend.Geb qualified as Geb
 import Juvix.Compiler.Backend.VampIR.Translation qualified as VampIR
-import Juvix.Compiler.Core.Data.InfoTable qualified as Core
+import Juvix.Compiler.Core.Data.Module qualified as Core
 import System.FilePath (takeBaseName)
 
 data PipelineArg = PipelineArg
   { _pipelineArgOptions :: CompileOptions,
     _pipelineArgFile :: Path Abs File,
-    _pipelineArgInfoTable :: Core.InfoTable
+    _pipelineArgModule :: Core.Module
   }
 
 getEntry :: (Members '[Embed IO, App, TaggedLock] r) => PipelineArg -> Sem r EntryPoint
@@ -51,7 +51,7 @@ runCPipeline ::
   Sem r ()
 runCPipeline pa@PipelineArg {..} = do
   entryPoint <- getEntry pa
-  C.MiniCResult {..} <- getRight (run (runReader entryPoint (runError (coreToMiniC _pipelineArgInfoTable :: Sem '[Error JuvixError, Reader EntryPoint] C.MiniCResult))))
+  C.MiniCResult {..} <- getRight (run (runReader entryPoint (runError (coreToMiniC _pipelineArgModule :: Sem '[Error JuvixError, Reader EntryPoint] C.MiniCResult))))
   cFile <- inputCFile _pipelineArgFile
   embed $ TIO.writeFile (toFilePath cFile) _resultCCode
   outfile <- Compile.outputFile _pipelineArgOptions _pipelineArgFile
@@ -84,7 +84,7 @@ runGebPipeline pa@PipelineArg {..} = do
                     { _lispPackageName = fromString $ takeBaseName $ toFilePath gebFile,
                       _lispPackageEntry = "*entry*"
                     }
-  Geb.Result {..} <- getRight (run (runReader entryPoint (runError (coreToGeb spec _pipelineArgInfoTable :: Sem '[Error JuvixError, Reader EntryPoint] Geb.Result))))
+  Geb.Result {..} <- getRight (run (runReader entryPoint (runError (coreToGeb spec _pipelineArgModule :: Sem '[Error JuvixError, Reader EntryPoint] Geb.Result))))
   embed $ TIO.writeFile (toFilePath gebFile) _resultCode
 
 runVampIRPipeline ::
@@ -95,14 +95,14 @@ runVampIRPipeline ::
 runVampIRPipeline pa@PipelineArg {..} = do
   entryPoint <- getEntry pa
   vampirFile <- Compile.outputFile _pipelineArgOptions _pipelineArgFile
-  VampIR.Result {..} <- getRight (run (runReader entryPoint (runError (coreToVampIR _pipelineArgInfoTable :: Sem '[Error JuvixError, Reader EntryPoint] VampIR.Result))))
+  VampIR.Result {..} <- getRight (run (runReader entryPoint (runError (coreToVampIR _pipelineArgModule :: Sem '[Error JuvixError, Reader EntryPoint] VampIR.Result))))
   embed $ TIO.writeFile (toFilePath vampirFile) _resultCode
 
 runAsmPipeline :: (Members '[Embed IO, App, TaggedLock] r) => PipelineArg -> Sem r ()
 runAsmPipeline pa@PipelineArg {..} = do
   entryPoint <- getEntry pa
   asmFile <- Compile.outputFile _pipelineArgOptions _pipelineArgFile
-  r <- runReader entryPoint $ runError @JuvixError (coreToAsm _pipelineArgInfoTable)
+  r <- runReader entryPoint $ runError @JuvixError (coreToAsm _pipelineArgModule)
   tab' <- getRight r
   let code = Asm.ppPrint tab' tab'
   embed $ TIO.writeFile (toFilePath asmFile) code

src/Juvix/Compiler/Asm/Pretty/Base.hs
@@ -398,7 +398,7 @@ instance PrettyCode InfoTable where
         HashMap.filter
           ( \ii -> case ii ^. inductiveConstructors of
               BuiltinTag _ : _ -> False
-              UserTag _ : _ -> True
+              UserTag _ _ : _ -> True
               [] -> True
           )
 

src/Juvix/Compiler/Concrete/Data/Scope/Base.hs
@@ -3,6 +3,7 @@ module Juvix.Compiler.Concrete.Data.Scope.Base where
 import Juvix.Compiler.Concrete.Data.NameSpace
 import Juvix.Compiler.Concrete.Data.ScopedName qualified as S
 import Juvix.Compiler.Concrete.Language
+import Juvix.Compiler.Store.Scoped.Language
 import Juvix.Prelude
 
 newtype SymbolInfo (n :: NameSpace) = SymbolInfo
@@ -22,13 +23,14 @@ data BindingStrategy
 data Scope = Scope
   { _scopePath :: S.AbsModulePath,
     _scopeSymbols :: HashMap Symbol (SymbolInfo 'NameSpaceSymbols),
+    -- | Local module symbols (excluding top modules associated with files)
     _scopeModuleSymbols :: HashMap Symbol (SymbolInfo 'NameSpaceModules),
     _scopeFixitySymbols :: HashMap Symbol (SymbolInfo 'NameSpaceFixities),
     -- | The map from S.NameId to Modules is needed because we support merging
     -- several imports under the same name. E.g.
     -- import A as X;
     -- import B as X;
-    _scopeTopModules :: HashMap TopModulePath (HashMap S.NameId (ModuleRef'' 'S.NotConcrete 'ModuleTop)),
+    _scopeTopModules :: HashMap TopModulePath (HashMap S.NameId ScopedModule),
     -- | Symbols that have been defined in the current scope level. Every symbol
     -- should map to itself. This is needed because we may query it with a
     -- symbol with a different location but we may want the location of the
@@ -39,25 +41,16 @@ data Scope = Scope
   }
 
 newtype ModulesCache = ModulesCache
-  { _cachedModules :: HashMap TopModulePath (ModuleRef'' 'S.NotConcrete 'ModuleTop)
+  { _cachedModules :: HashMap TopModulePath ScopedModule
   }
 
-data ScopeParameters = ScopeParameters
-  { -- | Used for import cycle detection.
-    _scopeTopParents :: [Import 'Parsed],
-    _scopeParsedModules :: HashMap TopModulePath (Module 'Parsed 'ModuleTop)
-  }
-
-data RecordInfo = RecordInfo
-  { _recordInfoConstructor :: S.Symbol,
-    _recordInfoSignature :: RecordNameSignature 'Parsed
+newtype ScopeParameters = ScopeParameters
+  { _scopeImportedModules :: HashMap TopModulePath ScopedModule
   }
 
 data ScoperState = ScoperState
-  { _scoperModulesCache :: ModulesCache,
-    -- | Local and top modules
-    _scoperModules :: HashMap S.ModuleNameId (ModuleRef' 'S.NotConcrete),
-    _scoperScope :: HashMap TopModulePath Scope,
+  { -- | Local and top modules currently in scope - used to look up qualified symbols
+    _scoperModules :: HashMap S.NameId ScopedModule,
     _scoperAlias :: HashMap S.NameId PreSymbolEntry,
     _scoperSignatures :: HashMap S.NameId (NameSignature 'Parsed),
     _scoperScopedSignatures :: HashMap S.NameId (NameSignature 'Scoped),
@@ -108,4 +101,3 @@ makeLenses ''ScoperSyntax
 makeLenses ''ScoperState
 makeLenses ''ScopeParameters
 makeLenses ''ModulesCache
-makeLenses ''RecordInfo

src/Juvix/Compiler/Concrete/Print/Base.hs
@@ -9,13 +9,13 @@ where
 
 import Data.HashMap.Strict qualified as HashMap
 import Data.List.NonEmpty.Extra qualified as NonEmpty
-import Juvix.Compiler.Concrete.Data.InfoTable
 import Juvix.Compiler.Concrete.Data.Scope.Base
 import Juvix.Compiler.Concrete.Data.ScopedName qualified as S
 import Juvix.Compiler.Concrete.Extra qualified as Concrete
 import Juvix.Compiler.Concrete.Keywords qualified as Kw
 import Juvix.Compiler.Concrete.Language
 import Juvix.Compiler.Concrete.Pretty.Options
+import Juvix.Compiler.Store.Scoped.Language (Alias, ModuleSymbolEntry, PreSymbolEntry (..), ScopedModule, SymbolEntry, aliasName, moduleEntry, scopedModuleName, symbolEntry)
 import Juvix.Data.Ape.Base
 import Juvix.Data.Ape.Print
 import Juvix.Data.CodeAnn (Ann, CodeAnn (..), ppStringLit)
@@ -91,18 +91,13 @@ ppSymbolType = case sing :: SStage s of
   SParsed -> ppCode
   SScoped -> ppCode
 
+ppModuleNameType :: forall s. (SingI s) => PrettyPrinting (ModuleNameType s)
+ppModuleNameType = case sing :: SStage s of
+  SParsed -> ppCode
+  SScoped -> ppCode
+
 ppIdentifierType :: forall s. (SingI s) => PrettyPrinting (IdentifierType s)
 ppIdentifierType = case sing :: SStage s of
-  SParsed -> ppCode
-  SScoped -> ppCode
-
-ppModuleRefType :: forall s. (SingI s) => PrettyPrinting (ModuleRefType s)
-ppModuleRefType = case sing :: SStage s of
-  SParsed -> ppCode
-  SScoped -> ppCode
-
-ppImportType :: forall s. (SingI s) => PrettyPrinting (ImportType s)
-ppImportType = case sing :: SStage s of
   SParsed -> ppCode
   SScoped -> ppCode
 
@@ -263,10 +258,6 @@ instance (SingI s) => PrettyPrint (Iterator s) where
 instance PrettyPrint S.AName where
   ppCode n = annotated (AnnKind (S.getNameKind n)) (noLoc (pretty (n ^. S.anameVerbatim)))
 
-instance PrettyPrint FunctionInfo where
-  ppCode = \case
-    FunctionInfo f -> ppCode f
-
 instance (SingI s) => PrettyPrint (List s) where
   ppCode List {..} = do
     let l = ppCode _listBracketL
@@ -383,7 +374,7 @@ withNameIdSuffix nid a = do
   when showNameId (noLoc "@" <> ppCode nid)
 
 instance PrettyPrint S.NameId where
-  ppCode (S.NameId k) = noLoc (pretty k)
+  ppCode = noLoc . pretty
 
 ppModuleHeader :: (SingI t, SingI s) => PrettyPrinting (Module s t)
 ppModuleHeader Module {..} = do
@@ -466,11 +457,8 @@ instance PrettyPrint QualifiedName where
     let symbols = _qualifiedPath ^. pathParts NonEmpty.|> _qualifiedSymbol
     dotted (ppSymbolType <$> symbols)
 
-instance (SingI t) => PrettyPrint (ModuleRef'' 'S.NotConcrete t) where
-  ppCode = ppCode @(ModuleRef' 'S.NotConcrete) . project
-
-instance PrettyPrint (ModuleRef'' 'S.Concrete t) where
-  ppCode m = ppCode (m ^. moduleRefName)
+instance PrettyPrint ScopedModule where
+  ppCode m = ppCode (m ^. scopedModuleName)
 
 instance PrettyPrint ScopedIden where
   ppCode = ppCode . (^. scopedIdenName)
@@ -1072,23 +1060,12 @@ instance (SingI s) => PrettyPrint (UsingItem s) where
         kwmodule = ppCode <$> (ui ^. usingModuleKw)
     kwmodule <?+> (sym' <+?> kwAs' <+?> alias')
 
-instance PrettyPrint (ModuleRef' 'S.NotConcrete) where
-  ppCode (ModuleRef' (t :&: m)) =
-    let path = m ^. moduleRefModule . modulePath
-        txt = case t of
-          SModuleTop -> annotate (AnnKind KNameTopModule) (pretty path)
-          SModuleLocal -> annotate (AnnKind KNameLocalModule) (pretty path)
-     in noLoc txt
-
-instance PrettyPrint ModuleRef where
-  ppCode (ModuleRef' (_ :&: ModuleRef'' {..})) = ppCode _moduleRefName
-
 instance (SingI s) => PrettyPrint (Import s) where
   ppCode :: forall r. (Members '[ExactPrint, Reader Options] r) => Import s -> Sem r ()
   ppCode i = do
     let open' = ppOpenModuleHelper Nothing <$> (i ^. importOpen)
     ppCode (i ^. importKw)
-      <+> ppImportType (i ^. importModule)
+      <+> ppModulePathType (i ^. importModulePath)
       <+?> ppAlias
       <+?> open'
     where
@@ -1097,9 +1074,9 @@ instance (SingI s) => PrettyPrint (Import s) where
         Nothing -> Nothing
         Just as -> Just (ppCode Kw.kwAs <+> ppModulePathType as)
 
-ppOpenModuleHelper :: (SingI s) => Maybe (ModuleRefType s) -> PrettyPrinting (OpenModuleParams s)
+ppOpenModuleHelper :: (SingI s) => Maybe (ModuleNameType s) -> PrettyPrinting (OpenModuleParams s)
 ppOpenModuleHelper modName OpenModuleParams {..} = do
-  let name' = ppModuleRefType <$> modName
+  let name' = ppModuleNameType <$> modName
       usingHiding' = ppCode <$> _openUsingHiding
       openkw = ppCode _openModuleKw
       public' = ppCode <$> _openPublicKw ^. unIrrelevant

commit d59d02ccb7260654d12a82716cfd74de0c37ec83

Bugfix: modules associated with inductive types should be declared after their inductive types (#2768)
* Closes #2763. 
* Fixes a bug in the scoper, likely introduced in
https://github.com/anoma/juvix/pull/2468 by making later declarations
depend on earlier ones. The problem was that the inductive modules were
always added at the beginning of a section, which resulted in an
incorrect definition dependency graph (an inductive type depended on its
associated projections).
* Now inductive modules are added just after a group of inductive
definitions, before the next function definition. This implies that
inductive type definitions which depend on each other cannot be
separated by function definitions. Existing Juvix code needs to be
adjusted.
* The behaviour is now equivalent to "manually" inserting module
declarations with projections after each group of inductive definitions.

include/package/PackageDescription/V1.juvix
@@ -17,13 +17,6 @@ type Package :=
     buildDir : Maybe String
   };
 
---- Construct a ;Package; with useful default arguments.
-defaultPackage {name : String := "my-project"} {version : SemVer := defaultVersion} {dependencies : List
-  Dependency := [defaultStdlib]} {main : Maybe
-  String := nothing} {buildDir : Maybe String := nothing}
-  : Package :=
-  mkPackage name version dependencies main buildDir;
-
 --- A ;SemVer; defines a version that conforms to the Semantic Versioning specification.
 type SemVer :=
   mkSemVer {
@@ -33,20 +26,6 @@ type SemVer :=
     release : Maybe String;
     meta : Maybe String
   };
-
---- Construct a ;SemVer; with useful default arguments.
-mkVersion (major minor patch : Nat) {release : Maybe
-  String := nothing} {meta : Maybe String := nothing}
-  : SemVer :=
-  mkSemVer
-    (major := major;
-    minor := minor;
-    patch := patch;
-    release := release;
-    meta := meta);
-
---- The default version used in `defaultPackage`.
-defaultVersion : SemVer := mkVersion 0 0 0;
 
 --- A ;Dependency; defines a Juvix package dependency.
 type Dependency :=
@@ -63,6 +42,27 @@ type Dependency :=
     }
   | --- The ;defaultStdlib; that is bundled with the Juvix compiler.
     defaultStdlib;
+
+--- Construct a ;Package; with useful default arguments.
+defaultPackage {name : String := "my-project"} {version : SemVer := defaultVersion} {dependencies : List
+  Dependency := [defaultStdlib]} {main : Maybe
+  String := nothing} {buildDir : Maybe String := nothing}
+  : Package :=
+  mkPackage name version dependencies main buildDir;
+
+--- Construct a ;SemVer; with useful default arguments.
+mkVersion (major minor patch : Nat) {release : Maybe
+  String := nothing} {meta : Maybe String := nothing}
+  : SemVer :=
+  mkSemVer
+    (major := major;
+    minor := minor;
+    patch := patch;
+    release := release;
+    meta := meta);
+
+--- The default version used in `defaultPackage`.
+defaultVersion : SemVer := mkVersion 0 0 0;
 
 --- Constructs a GitHub dependency.
 github (org repo ref : String) : Dependency :=

include/package/PackageDescription/V2.juvix
@@ -17,13 +17,6 @@ type Package :=
     buildDir : Maybe String
   };
 
---- Construct a ;Package; with useful default arguments.
-defaultPackage {name : String := "my-project"} {version : SemVer := defaultVersion} {dependencies : List
-  Dependency := [defaultStdlib]} {main : Maybe
-  String := nothing} {buildDir : Maybe String := nothing}
-  : Package :=
-  mkPackage name version dependencies main buildDir;
-
 --- A ;SemVer; defines a version that conforms to the Semantic Versioning specification.
 type SemVer :=
   mkSemVer {
@@ -33,20 +26,6 @@ type SemVer :=
     release : Maybe String;
     meta : Maybe String
   };
-
---- Construct a ;SemVer; with useful default arguments.
-mkVersion (major minor patch : Nat) {release : Maybe
-  String := nothing} {meta : Maybe String := nothing}
-  : SemVer :=
-  mkSemVer
-    (major := major;
-    minor := minor;
-    patch := patch;
-    release := release;
-    meta := meta);
-
---- The default version used in `defaultPackage`.
-defaultVersion : SemVer := mkVersion 0 0 0;
 
 --- A ;Dependency; defines a Juvix package dependency.
 type Dependency :=
@@ -63,6 +42,27 @@ type Dependency :=
     }
   | --- The ;defaultStdlib; that is bundled with the Juvix compiler.
     defaultStdlib;
+
+--- Construct a ;Package; with useful default arguments.
+defaultPackage {name : String := "my-project"} {version : SemVer := defaultVersion} {dependencies : List
+  Dependency := [defaultStdlib]} {main : Maybe
+  String := nothing} {buildDir : Maybe String := nothing}
+  : Package :=
+  mkPackage name version dependencies main buildDir;
+
+--- Construct a ;SemVer; with useful default arguments.
+mkVersion (major minor patch : Nat) {release : Maybe
+  String := nothing} {meta : Maybe String := nothing}
+  : SemVer :=
+  mkSemVer
+    (major := major;
+    minor := minor;
+    patch := patch;
+    release := release;
+    meta := meta);
+
+--- The default version used in `defaultPackage`.
+defaultVersion : SemVer := mkVersion 0 0 0;
 
 --- Constructs a GitHub dependency.
 github (org repo ref : String) : Dependency :=

src/Juvix/Compiler/Concrete/Translation/FromParsed/Analysis/Scoping.hs
@@ -1198,28 +1198,11 @@ checkSections ::
   (Members '[HighlightBuilder, Error ScoperError, Reader ScopeParameters, State Scope, State ScoperState, InfoTableBuilder, Reader InfoTable, NameIdGen, State ScoperSyntax, Reader EntryPoint] r) =>
   StatementSections 'Parsed ->
   Sem r (StatementSections 'Scoped)
-checkSections sec = do
-  (inductiveModules, sec' :: StatementSections 'Scoped) <- runOutputList (topBindings helper)
-  return $ case (fmap NonDefinitionModule) <$> nonEmpty inductiveModules of
-    Nothing -> sec'
-    Just im -> case sec' of
-      SectionsNonDefinitions d -> SectionsNonDefinitions (over nonDefinitionsSection (im <>) d)
-      SectionsEmpty ->
-        SectionsNonDefinitions
-          NonDefinitionsSection
-            { _nonDefinitionsSection = im,
-              _nonDefinitionsNext = Nothing
-            }
-      SectionsDefinitions d ->
-        SectionsNonDefinitions
-          NonDefinitionsSection
-            { _nonDefinitionsSection = im,
-              _nonDefinitionsNext = Just d
-            }
+checkSections sec = topBindings helper
   where
     helper ::
       forall r'.
-      (r' ~ Reader BindingStrategy ': Output (Module 'Scoped 'ModuleLocal) ': r) =>
+      (r' ~ Reader BindingStrategy ': r) =>
       Sem r' (StatementSections 'Scoped)
     helper = case sec of
       SectionsEmpty -> return SectionsEmpty
@@ -1243,17 +1226,82 @@ checkSections sec = do
               NonDefinitionOpenModule i -> NonDefinitionOpenModule <$> checkOpenModule i
 
         goDefinitions :: DefinitionsSection 'Parsed -> Sem r' (DefinitionsSection 'Scoped)
-        goDefinitions DefinitionsSection {..} = do
-          mapM_ reserveDefinition _definitionsSection
-          mapM_ scanAlias (_definitionsSection ^.. each . _DefinitionSyntax . _SyntaxAlias)
-          sec' <- mapM goDefinition _definitionsSection
-          next' <- mapM goNonDefinitions _definitionsNext
-          return
-            DefinitionsSection
-              { _definitionsNext = next',
-                _definitionsSection = sec'
-              }
+        goDefinitions DefinitionsSection {..} = goDefs [] [] (toList _definitionsSection)
           where
+            -- This functions goes through a section reserving definitions and
+            -- collecting inductive modules. It breaks a section when the
+            -- collected inductive modules are non-empty (there were some
+            -- inductive definitions) and the next definition is a function
+            -- definition.
+            -- `acc` holds the definitions in the section encountered up till
+            -- now (reversed)
+            -- `ms` holds the inductive modules for the inductive definitions in
+            -- the section up till now (reversed)
+            goDefs :: [Definition 'Parsed] -> [Module 'Parsed 'ModuleLocal] -> [Definition 'Parsed] -> Sem r' (DefinitionsSection 'Scoped)
+            goDefs acc ms = \case
+              -- if there were some inductive type definitions (the list `ms` of
+              -- corresponding inductive modules is not empty) and the next
+              -- definition is a function definition, then we need to break the
+              -- section and start a new one
+              def@DefinitionFunctionDef {} : defs
+                | not (null ms) -> do
+                    eassert (not (null acc))
+                    sec' <- goDefsSection (nonEmpty' (reverse acc))
+                    ms' <- goInductiveModules (nonEmpty' (reverse ms))
+                    next' <- goDefs [] [] (def : defs)
+                    let next'' =
+                          NonDefinitionsSection
+                            { _nonDefinitionsSection = ms',
+                              _nonDefinitionsNext = Just next'
+                            }
+                    return
+                      DefinitionsSection
+                        { _definitionsNext = Just next'',
+                          _definitionsSection = sec'
+                        }
+              def : defs -> do
+                -- `reserveDefinition` returns the created inductive module if
+                -- `def` is an inductive type definition
+                m <- reserveDefinition def
+                let ms' = maybeToList m ++ ms
+                goDefs (def : acc) ms' defs
+              [] -> do
+                eassert (not (null acc))
+                sec' <- goDefsSection (nonEmpty' (reverse acc))
+                next' <- case nonEmpty (reverse ms) of
+                  Nothing -> mapM goNonDefinitions _definitionsNext
+                  Just ms' ->
+                    case _definitionsNext of
+                      Nothing -> do
+                        ms'' <- goInductiveModules ms'
+                        return $
+                          Just
+                            NonDefinitionsSection
+                              { _nonDefinitionsSection = ms'',
+                                _nonDefinitionsNext = Nothing
+                              }
+                      Just nd -> do
+                        ms'' <- goInductiveModules ms'
+                        nd' <- goNonDefinitions nd
+                        return $ Just $ over nonDefinitionsSection (ms'' <>) nd'
+                return
+                  DefinitionsSection
+                    { _definitionsNext = next',
+                      _definitionsSection = sec'
+                    }
+
+            -- checks the local inductive modules generated for the inductive type definitions
+            goInductiveModules :: NonEmpty (Module 'Parsed 'ModuleLocal) -> Sem r' (NonEmpty (NonDefinition 'Scoped))
+            goInductiveModules ms = do
+              ms' <- mapM checkLocalModule ms
+              return $ fmap NonDefinitionModule ms'
+
+            -- checks the definitions in a section
+            goDefsSection :: NonEmpty (Definition 'Parsed) -> Sem r' (NonEmpty (Definition 'Scoped))
+            goDefsSection defs = do
+              mapM_ scanAlias (defs ^.. each . _DefinitionSyntax . _SyntaxAlias)
+              mapM goDefinition defs
+
             scanAlias :: AliasDef 'Parsed -> Sem r' ()
             scanAlias a = do
               aliasId <- gets (^?! scopeLocalSymbols . at (a ^. aliasDefName) . _Just . S.nameId)
@@ -1271,20 +1319,22 @@ checkSections sec = do
                       modify' (HashSet.insert i)
                       whenJustM (gets (^? scoperAlias . at i . _Just . preSymbolName . S.nameId)) go
 
-            reserveDefinition :: Definition 'Parsed -> Sem r' ()
+            reserveDefinition :: Definition 'Parsed -> Sem r' (Maybe (Module 'Parsed 'ModuleLocal))
             reserveDefinition = \case
-              DefinitionSyntax s -> resolveSyntaxDef s
-              DefinitionFunctionDef d -> void (reserveFunctionSymbol d)
-              DefinitionAxiom d -> void (reserveAxiomSymbol d)
-              DefinitionProjectionDef d -> void (reserveProjectionSymbol d)
-              DefinitionInductive d -> reserveInductive d
+              DefinitionSyntax s -> resolveSyntaxDef s $> Nothing
+              DefinitionFunctionDef d -> void (reserveFunctionSymbol d) >> return Nothing
+              DefinitionAxiom d -> void (reserveAxiomSymbol d) >> return Nothing
+              DefinitionProjectionDef d -> void (reserveProjectionSymbol d) >> return Nothing
+              DefinitionInductive d -> Just <$> reserveInductive d
               where
-                reserveInductive :: InductiveDef 'Parsed -> Sem r' ()
+                -- returns the module generated for the inductive definition
+                reserveInductive :: InductiveDef 'Parsed -> Sem r' (Module 'Parsed 'ModuleLocal)
                 reserveInductive d = do
                   i <- reserveInductiveSymbol d
                   constrs <- mapM reserveConstructor (d ^. inductiveConstructors)
-                  void (defineInductiveModule (head constrs) d)
+                  m <- defineInductiveModule (head constrs) d
                   ignoreFail (registerRecordType (head constrs) i)
+                  return m
                   where
                     reserveConstructor :: ConstructorDef 'Parsed -> Sem r' S.Symbol
                     reserveConstructor c = do
@@ -1323,10 +1373,9 @@ checkSections sec = do
               DefinitionInductive d -> DefinitionInductive <$> checkInductiveDef d
               DefinitionProjectionDef d -> DefinitionProjectionDef <$> checkProjectionDef d
 
-            defineInductiveModule :: S.Symbol -> InductiveDef 'Parsed -> Sem r' ()
+            defineInductiveModule :: S.Symbol -> InductiveDef 'Parsed -> Sem r' (Module 'Parsed 'ModuleLocal)
             defineInductiveModule headConstr i = do
-              m <- runReader (getLoc (i ^. inductiveName)) genModule
-              checkLocalModule m >>= output
+              runReader (getLoc (i ^. inductiveName)) genModule
               where
                 genModule :: forall s'. (Members '[Reader Interval, Reader EntryPoint, State Scope] s') => Sem s' (Module 'Parsed 'ModuleLocal)
                 genModule = do

src/Juvix/Compiler/Internal/Pretty/Base.hs
@@ -293,6 +293,12 @@ instance PrettyCode MutualStatement where
     StatementFunction d -> ppCode d
     StatementAxiom d -> ppCode d
 
+instance PrettyCode PreStatement where
+  ppCode = \case
+    PreInductiveDef d -> ppCode d
+    PreFunctionDef d -> ppCode d
+    PreAxiomDef d -> ppCode d
+
 instance PrettyCode MutualBlock where
   ppCode (MutualBlock funs) = ppMutual funs
 

test/Internal/Eval/Positive.hs
@@ -155,6 +155,11 @@ tests =
       $(mkRelFile "Universe.juvix")
       $(mkRelFile "out/Universe.out"),
     PosTest
+      "Parameterized type axioms"
+      $(mkRelDir ".")
+      $(mkRelFile "TypeParamAxiom.juvix")
+      $(mkRelFile "out/TypeParamAxiom.out"),
+    PosTest
       "Inductive type constructor"
       $(mkRelDir ".")
       $(mkRelFile "Inductive.juvix")

tests/Internal/positive/TypeParamAxiom.juvix
@@ -1,0 +1,12 @@
+module TypeParamAxiom;
+
+import Stdlib.Data.Nat open;
+
+axiom X : Type;
+axiom B : Type -> Type;
+
+type A := mkA {
+  a : B X;
+};
+
+main : Nat := 0;

tests/Internal/positive/out/TypeParamAxiom.out
@@ -1,0 +1,1 @@
+0