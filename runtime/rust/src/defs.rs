efe2141e7e51b15ecc206c09ce375cd3e7650202
Lukasz Czajka <lukasz@heliax.dev>
2024-05-15 14:32:29 +0200

    memory

---
+++ runtime/rust/src/defs.rs
@@ -0,0 +1,7 @@
+// Runtime definitions
+
+// Type of words
+pub type Word = u32;
+
+// Type of pointers
+pub type Pointer = Word;
---
+++ runtime/rust/src/memory.rs
@@ -0,0 +1,39 @@
+// Heap memory management
+
+use super::defs::*;
+
+pub struct Memory {
+    memory: Vec<Word>,
+}
+
+impl Memory {
+    pub fn new() -> Self {
+        Self { memory: Vec::new() }
+    }
+
+    pub fn alloc(&mut self, size: usize) -> Pointer {
+        let idx = self.memory.len();
+        self.memory.resize(idx + size, 0);
+        idx as Pointer
+    }
+}
+
+impl<Idx> std::ops::Index<Idx> for Memory
+where
+    Idx: std::slice::SliceIndex<[Word]>,
+{
+    type Output = Idx::Output;
+
+    fn index(&self, index: Idx) -> &Self::Output {
+        &self.memory[index]
+    }
+}
+
+impl<Idx> std::ops::IndexMut<Idx> for Memory
+where
+    Idx: std::slice::SliceIndex<[Word]>,
+{
+    fn index_mut(&mut self, index: Idx) -> &mut Self::Output {
+        &mut self.memory[index]
+    }
+}
