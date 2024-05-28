01e4107c5cfa9d8f87d4090d8dc6f18c59db6ba2
Jan Mas Rovira <janmasrovira@gmail.com>
2024-05-28 11:25:18 +0200

    restore progress log

--- src/Parallel/ProgressLog.hs
+++ src/Parallel/ProgressLog.hs
@@ -8,15 +8,11 @@ data ProgressLog :: Effect where
 
 makeSem ''ProgressLog
 
--- runProgressLogIO :: (Members '[EmbedIO] r) => Bool -> Sem (ProgressLog ': r) a -> Sem r a
-runProgressLogIO :: Bool -> Sem (ProgressLog ': r) a -> Sem r a
-runProgressLogIO _useColors = interpret $ \case
-  ProgressLog _l -> do
-    return ()
-
--- TODO restore when I know what's the best way to fix smoke tests
--- hRenderIO useColors stderr l
--- hRenderIO useColors stderr ("\n" :: Text)
+runProgressLogIO :: (Members '[EmbedIO] r) => Bool -> Sem (ProgressLog ': r) a -> Sem r a
+runProgressLogIO useColors = interpret $ \case
+  ProgressLog l -> do
+    hRenderIO useColors stderr l
+    hRenderIO useColors stderr ("\n" :: Text)
 
 ignoreProgressLog :: Sem (ProgressLog ': r) a -> Sem r a
 ignoreProgressLog = interpret $ \case
