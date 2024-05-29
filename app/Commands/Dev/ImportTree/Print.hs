4ca32e74f7bcba067101e248607bde91f9f1f025
Jan Mas Rovira <janmasrovira@gmail.com>
2024-05-29 13:30:08 +0200

    `juvix dev import-tree print` no longer ignores input file

--- app/Commands/Dev/ImportTree/Print.hs
+++ app/Commands/Dev/ImportTree/Print.hs
@@ -4,10 +4,15 @@ import Commands.Base
 import Commands.Dev.ImportTree.Print.Options
 import Juvix.Compiler.Concrete.Print
 import Juvix.Compiler.Pipeline.Loader.PathResolver
+import Juvix.Compiler.Pipeline.Loader.PathResolver.ImportTree
 
 runCommand :: (Members '[EmbedIO, App, TaggedLock] r) => PrintOptions -> Sem r ()
 runCommand PrintOptions {..} = runReader opts . runPipelineSetup $ do
-  tree <- ask
+  tree <- case _printInputFile of
+    Nothing -> ask
+    Just appInputFile -> do
+      inputFile <- fromAppPathFile appInputFile
+      mkImportTree (Just inputFile)
   renderStdOut (ppOutDefaultNoComments tree)
   when _printStats $ do
     let stats = mkImportTreeStats tree
