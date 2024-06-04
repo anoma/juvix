6621bf0a1ed844db5a520ed66591024ae631a7de
Jan Mas Rovira <janmasrovira@gmail.com>
2024-06-04 16:35:11 +0200

    fix generation of wildcards in RecordPattern

--- src/Juvix/Compiler/Internal/Translation/FromConcrete.hs
+++ src/Juvix/Compiler/Internal/Translation/FromConcrete.hs
@@ -1251,7 +1251,11 @@ goRecordPattern r = do
 
     mkPatterns :: Sem r [Internal.PatternArg]
     mkPatterns = do
-      sig <- asks (fromJust . HashMap.lookup (constr ^. Internal.nameId) . (^. S.infoConstructorSigs))
+      sig <-
+        asks $
+          fromJust
+            . HashMap.lookup (constr ^. Internal.nameId)
+            . (^. S.infoConstructorSigs)
       let maxIdx = length (sig ^. recordNames) - 1
       args <- IntMap.toAscList <$> byIndex
       execOutputList (go maxIdx 0 args)
@@ -1267,6 +1271,7 @@ goRecordPattern r = do
           | otherwise = do
               v <- Internal.freshVar loc ("x" <> show idx)
               output (Internal.patternArgFromVar Internal.Explicit v)
+              go maxIdx (idx + 1) args
 
 goAxiom :: (Members '[Reader DefaultArgsStack, Reader Pragmas, Error ScoperError, Builtins, NameIdGen, Reader S.InfoTable] r) => AxiomDef 'Scoped -> Sem r Internal.AxiomDef
 goAxiom a = do
