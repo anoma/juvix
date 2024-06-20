345500e8df3394bec83fac3a89a821ac8360709f
Lukasz Czajka <lukasz@heliax.dev>
2024-06-20 21:44:09 +0200

    compute functions depending on Cairo builtins

--- src/Juvix/Compiler/Reg/Extra/Blocks/Info.hs
+++ src/Juvix/Compiler/Reg/Extra/Blocks/Info.hs
@@ -4,8 +4,11 @@ module Juvix.Compiler.Reg.Extra.Blocks.Info
   )
 where
 
+import Data.HashMap.Strict qualified as HashMap
+import Juvix.Compiler.Reg.Data.Blocks.CallGraph
 import Juvix.Compiler.Reg.Data.Blocks.InfoTable
-import Juvix.Compiler.Reg.Language.Base
+import Juvix.Compiler.Reg.Extra.Blocks
+import Juvix.Compiler.Reg.Language.Blocks
 import Juvix.Compiler.Tree.Extra.Info
 
 data ExtraInfo = ExtraInfo
@@ -14,7 +17,10 @@ data ExtraInfo = ExtraInfo
     _extraInfoFUIDs :: HashMap Symbol Int,
     -- | IDs for constructor tags, consecutive starting from 0 for each
     -- inductive type separately
-    _extraInfoCIDs :: HashMap Tag Int
+    _extraInfoCIDs :: HashMap Tag Int,
+    -- | Symbols of functions which depend (directly or indirectly) on Cairo
+    -- builtins
+    _extraInfoCairoDepFuns :: HashSet Symbol
   }
 
 makeLenses ''ExtraInfo
@@ -24,5 +30,31 @@ computeExtraInfo tab =
   ExtraInfo
     { _extraInfoTable = tab,
       _extraInfoFUIDs = computeFUIDs tab,
-      _extraInfoCIDs = computeCIDs tab
+      _extraInfoCIDs = computeCIDs tab,
+      _extraInfoCairoDepFuns = computeCairoDepFuns tab
     }
+
+computeCairoDepFuns :: InfoTable -> HashSet Symbol
+computeCairoDepFuns tab =
+  computeAncestors callGraph startNodes
+  where
+    callGraph :: CallGraph
+    callGraph = createCallGraph tab
+
+    startNodes :: [Symbol]
+    startNodes =
+      map (^. functionSymbol)
+        . filter (usesCairoBuiltins . (^. functionCode))
+        . HashMap.elems
+        $ (tab ^. infoFunctions)
+
+    usesCairoBuiltins :: Block -> Bool
+    usesCairoBuiltins block =
+      maybe False usesCairoBuiltins (block ^. blockNext)
+        || any isCairo (block ^. blockBody)
+        || any usesCairoBuiltins (getSubBlocks block)
+      where
+        isCairo :: Instruction -> Bool
+        isCairo = \case
+          Cairo {} -> True
+          _ -> False
