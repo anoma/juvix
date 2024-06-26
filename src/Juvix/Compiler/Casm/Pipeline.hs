bc1d840029970372b48848fca8135d6586935147
Lukasz Czajka <lukasz@heliax.dev>
2024-06-26 13:54:20 +0200

    pipeline

---
+++ src/Juvix/Compiler/Casm/Pipeline.hs
@@ -0,0 +1,17 @@
+module Juvix.Compiler.Casm.Pipeline
+  ( module Juvix.Compiler.Casm.Pipeline,
+    Options,
+    Code,
+  )
+where
+
+import Juvix.Compiler.Casm.Transformation
+import Juvix.Compiler.Pipeline.EntryPoint (EntryPoint)
+
+-- | Perform transformations on CASM necessary before the translation to Cairo
+-- bytecode
+toCairo' :: (Member (Reader Options) r) => Code -> Sem r Code
+toCairo' = applyTransformations toCairoTransformations
+
+toCairo :: (Member (Reader EntryPoint) r) => Code -> Sem r Code
+toCairo = mapReader fromEntryPoint . toCairo'
--- src/Juvix/Compiler/Casm/Transformation.hs
+++ src/Juvix/Compiler/Casm/Transformation.hs
@@ -9,9 +9,10 @@ import Juvix.Compiler.Casm.Data.TransformationId
 import Juvix.Compiler.Casm.Transformation.Base
 import Juvix.Compiler.Casm.Transformation.Optimize.Peephole
 
-applyTransformations :: forall r. (Member (Reader Options) r) => [TransformationId] -> Code -> Sem r Code
+applyTransformations :: forall r. [TransformationId] -> Code -> Sem r Code
 applyTransformations ts tbl = foldM (flip appTrans) tbl ts
   where
     appTrans :: TransformationId -> Code -> Sem r Code
     appTrans = \case
+      IdentityTrans -> return
       Peephole -> return . peephole
