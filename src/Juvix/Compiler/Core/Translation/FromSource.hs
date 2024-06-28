f2e19d0d2baea6657b72f789935e046a2436e90f
Paul Cadman <git@paulcadman.dev>
2024-06-28 11:27:13 +0100

    Add support for builtin Maybe to jvc parser

--- src/Juvix/Compiler/Core/Translation/FromSource.hs
+++ src/Juvix/Compiler/Core/Translation/FromSource.hs
@@ -78,6 +78,7 @@ parseToplevel = do
   lift declareIOBuiltins
   lift declareBoolBuiltins
   lift declareNatBuiltins
+  lift declareMaybeBuiltins
   space
   P.endBy statement (kw delimSemicolon)
   r <- optional expression
