e0c4d01c858e79b3ed560283e861dbee48bb547b
Jan Mas Rovira <janmasrovira@gmail.com>
2024-07-11 15:46:17 +0200

    improve error message in scoping negative tests. Fix more tests

--- src/Juvix/Prelude/Generic.hs
+++ src/Juvix/Prelude/Generic.hs
@@ -1,25 +1,33 @@
 module Juvix.Prelude.Generic
   ( genericConstructorName,
+    genericSameConstructor,
+    GenericHasConstructor,
   )
 where
 
 import GHC.Generics qualified as G
 import Juvix.Prelude.Base.Foundation
 
+genericSameConstructor :: (Generic a, GenericHasConstructor (G.Rep a)) => a -> a -> Bool
+genericSameConstructor x y = genericConstructorName @String x == genericConstructorName y
+
 genericConstructorName ::
-  (HasConstructor (G.Rep a), Generic a, IsString str) => a -> str
+  forall str a.
+  (GenericHasConstructor (G.Rep a), Generic a, IsString str) =>
+  a ->
+  str
 genericConstructorName = fromString . genericConstrName . G.from
 
-class HasConstructor (f :: GHCType -> GHCType) where
+class GenericHasConstructor (f :: GHCType -> GHCType) where
   genericConstrName :: f x -> String
 
-instance (HasConstructor f) => HasConstructor (G.D1 c f) where
+instance (GenericHasConstructor f) => GenericHasConstructor (G.D1 c f) where
   genericConstrName (G.M1 x) = genericConstrName x
 
-instance (HasConstructor x, HasConstructor y) => HasConstructor (x G.:+: y) where
+instance (GenericHasConstructor x, GenericHasConstructor y) => GenericHasConstructor (x G.:+: y) where
   genericConstrName = \case
     G.L1 l -> genericConstrName l
     G.R1 r -> genericConstrName r
 
-instance (G.Constructor c) => HasConstructor (G.C1 c f) where
+instance (G.Constructor c) => GenericHasConstructor (G.C1 c f) where
   genericConstrName x = G.conName x
--- test/Scope/Negative.hs
+++ test/Scope/Negative.hs
@@ -38,345 +38,260 @@ allTests =
     ( map (mkTest . testDescr) scoperErrorTests
     )
 
-wrongError :: Maybe FailMsg
-wrongError = Just "Incorrect error"
+negTest ::
+  String ->
+  Path Rel Dir ->
+  Path Rel File ->
+  (Path Abs File -> ScoperError -> Maybe FailMsg) ->
+  NegTest ScoperError
+negTest tname rdir rfile chk =
+  NegTest
+    { _name = tname,
+      _relDir = rdir,
+      _file = rfile,
+      _checkErr = chk (root <//> rdir <//> rfile)
+    }
 
 scoperErrorTests :: [NegTest ScoperError]
 scoperErrorTests =
-  [ NegTest
+  [ negTest
       "Not in scope"
       $(mkRelDir ".")
       $(mkRelFile "NotInScope.juvix")
-      $ \case
-        ErrSymNotInScope {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrSymNotInScope,
+    negTest
       "Qualified not in scope"
       $(mkRelDir ".")
       $(mkRelFile "QualSymNotInScope.juvix")
-      $ \case
-        ErrQualSymNotInScope {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrQualSymNotInScope,
+    negTest
       "Multiple declarations"
       $(mkRelDir ".")
       $(mkRelFile "MultipleDeclarations.juvix")
-      $ \case
-        ErrMultipleDeclarations {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrMultipleDeclarations,
+    negTest
       "Import cycle"
       $(mkRelDir "ImportCycle")
       $(mkRelFile "A.juvix")
-      $ \case
-        ErrImportCycleNew {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrImportCycleNew,
+    negTest
       "Binding group conflict (function clause)"
       $(mkRelDir "BindGroupConflict")
       $(mkRelFile "Clause.juvix")
-      $ \case
-        ErrMultipleDeclarations {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrMultipleDeclarations,
+    negTest
       "Binding group conflict (lambda clause)"
       $(mkRelDir "BindGroupConflict")
       $(mkRelFile "Lambda.juvix")
-      $ \case
-        ErrMultipleDeclarations {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrMultipleDeclarations,
+    negTest
       "Infix error (expression)"
       $(mkRelDir ".")
       $(mkRelFile "InfixError.juvix")
-      $ \case
-        ErrInfixParser {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrInfixParser,
+    negTest
       "Infix error (pattern)"
       $(mkRelDir ".")
       $(mkRelFile "InfixErrorP.juvix")
-      $ \case
-        ErrInfixPattern {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrInfixPattern,
+    negTest
       "Duplicate operator declaration"
       $(mkRelDir ".")
       $(mkRelFile "DuplicateOperator.juvix")
-      $ \case
-        ErrDuplicateOperator {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrDuplicateOperator,
+    negTest
       "Multiple export conflict"
       $(mkRelDir ".")
       $(mkRelFile "MultipleExportConflict.juvix")
-      $ \case
-        ErrMultipleExport {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrMultipleExport,
+    negTest
       "Module not in scope"
       $(mkRelDir ".")
       $(mkRelFile "ModuleNotInScope.juvix")
-      $ \case
-        ErrModuleNotInScope {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrModuleNotInScope,
+    negTest
       "Unused operator syntax definition"
       $(mkRelDir ".")
       $(mkRelFile "UnusedOperatorDef.juvix")
-      $ \case
-        ErrUnusedOperatorDef {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrUnusedOperatorDef,
+    negTest
       "Ambiguous symbol"
       $(mkRelDir ".")
       $(mkRelFile "AmbiguousSymbol.juvix")
-      $ \case
-        ErrAmbiguousSym {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrAmbiguousSym,
+    negTest
       "Ambiguous export"
       $(mkRelDir ".")
       $(mkRelFile "AmbiguousExport.juvix")
-      $ \case
-        ErrMultipleExport {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrMultipleExport,
+    negTest
       "Ambiguous nested modules"
       $(mkRelDir ".")
       $(mkRelFile "AmbiguousModule.juvix")
-      $ \case
-        ErrAmbiguousModuleSym {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrAmbiguousModuleSym,
+    negTest
       "Ambiguous nested constructors"
       $(mkRelDir ".")
       $(mkRelFile "AmbiguousConstructor.juvix")
-      $ \case
-        ErrAmbiguousSym {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrAmbiguousSym,
+    negTest
       "Implicit argument on the left of an application"
       $(mkRelDir ".")
       $(mkRelFile "AppLeftImplicit.juvix")
-      $ \case
-        ErrAppLeftImplicit {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrAppLeftImplicit,
+    negTest
       "issue 230"
       $(mkRelDir "230")
       $(mkRelFile "Prod.juvix")
-      $ \case
-        ErrQualSymNotInScope {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrQualSymNotInScope,
+    negTest
       "Double braces in pattern"
       $(mkRelDir ".")
       $(mkRelFile "NestedPatternBraces.juvix")
-      $ \case
-        ErrDoubleBracesPattern {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrDoubleBracesPattern,
+    negTest
       "As-Pattern aliasing variable"
       $(mkRelDir ".")
       $(mkRelFile "AsPatternAlias.juvix")
-      $ \case
-        ErrAliasBinderPattern {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrAliasBinderPattern,
+    negTest
       "Nested As-Patterns"
       $(mkRelDir ".")
       $(mkRelFile "NestedAsPatterns.juvix")
-      $ \case
-        ErrDoubleBinderPattern {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrDoubleBinderPattern,
+    negTest
       "Pattern matching an implicit argument on the left of an application"
       $(mkRelDir ".")
       $(mkRelFile "ImplicitPatternLeftApplication.juvix")
-      $ \case
-        ErrImplicitPatternLeftApplication {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrImplicitPatternLeftApplication,
+    negTest
       "Constructor expected on the left of a pattern application"
       $(mkRelDir ".")
       $(mkRelFile "ConstructorExpectedLeftApplication.juvix")
-      $ \case
-        ErrConstructorExpectedLeftApplication {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrConstructorExpectedLeftApplication,
+    negTest
       "A type parameter name occurs twice when declaring an inductive type"
       $(mkRelDir ".")
       $(mkRelFile "DuplicateInductiveParameterName.juvix")
-      $ \case
-        ErrNameSignature (ErrDuplicateName DuplicateName {}) -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrNameSignature,
+    negTest
       "Using symbol that is not exported"
       $(mkRelDir "UsingHiding")
       $(mkRelFile "Main.juvix")
-      $ \case
-        ErrModuleDoesNotExportSymbol {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrModuleDoesNotExportSymbol,
+    negTest
       "Wrong number of iterator initializers"
       $(mkRelDir ".")
       $(mkRelFile "Iterators1.juvix")
-      $ \case
-        ErrIteratorInitializer {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrIteratorInitializer,
+    negTest
       "Wrong number of iterator ranges"
       $(mkRelDir ".")
       $(mkRelFile "Iterators2.juvix")
-      $ \case
-        ErrIteratorRange {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrIteratorRange,
+    negTest
       "Undeclared iterator"
       $(mkRelDir ".")
       $(mkRelFile "Iterators3.juvix")
-      $ \case
-        ErrIteratorUndefined {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrIteratorUndefined,
+    negTest
       "Duplicate iterator declaration"
       $(mkRelDir ".")
       $(mkRelFile "Iterators4.juvix")
-      $ \case
-        ErrDuplicateIterator {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrDuplicateIterator,
+    negTest
       "Unused iterator declaration"
       $(mkRelDir ".")
       $(mkRelFile "Iterators5.juvix")
-      $ \case
-        ErrUnusedIteratorDef {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrUnusedIteratorDef,
+    negTest
       "Repeated name in named application"
       $(mkRelDir ".")
       $(mkRelFile "DuplicateArgument.juvix")
-      $ \case
-        ErrNamedArgumentsError (ErrDuplicateArgument {}) -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrMultipleDeclarations,
+    negTest
       "Unexpected named argument after wildcard"
       $(mkRelDir ".")
       $(mkRelFile "UnexpectedArgumentWildcard.juvix")
-      $ \case
-        ErrNamedArgumentsError ErrUnexpectedArguments {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrUnexpectedArgument,
+    negTest
       "Unexpected named argument"
       $(mkRelDir ".")
       $(mkRelFile "UnexpectedArgument.juvix")
-      $ \case
-        ErrNamedArgumentsError ErrUnexpectedArguments {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrUnexpectedArgument,
+    negTest
       "Missing argument"
       $(mkRelDir ".")
       $(mkRelFile "MissingArgument.juvix")
-      $ \case
-        ErrNamedArgumentsError ErrMissingArguments {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrNamedArgumentsError,
+    negTest
       "Repeated name in name signature"
       $(mkRelDir ".")
       $(mkRelFile "RepeatedNameSignature.juvix")
-      $ \case
-        ErrNameSignature ErrDuplicateName {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrNameSignature,
+    negTest
       "No named arguments"
       $(mkRelDir ".")
       $(mkRelFile "NoNamedArguments.juvix")
-      $ \case
-        ErrNamedArgumentsError ErrUnexpectedArguments {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrNoNameSignature,
+    negTest
       "Not a record"
       $(mkRelDir ".")
       $(mkRelFile "NotARecord.juvix")
-      $ \case
-        ErrNotARecord NotARecord {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrNotARecord,
+    negTest
       "Unexpected field in record update"
       $(mkRelDir ".")
       $(mkRelFile "UnexpectedFieldUpdate.juvix")
-      $ \case
-        ErrUnexpectedField UnexpectedField {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrUnexpectedField,
+    negTest
       "Repeated field in record pattern"
       $(mkRelDir ".")
       $(mkRelFile "RepeatedFieldPattern.juvix")
-      $ \case
-        ErrRepeatedField RepeatedField {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrRepeatedField,
+    negTest
       "Missing fields in record creation"
       $(mkRelDir ".")
       $(mkRelFile "MissingFields.juvix")
-      $ \case
-        ErrMissingArgs {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrMissingArgs,
+    negTest
       "Unexpected argument"
       $(mkRelDir ".")
       $(mkRelFile "UnexpectedArgumentNew.juvix")
-      $ \case
-        ErrUnexpectedArgument UnexpectedArgument {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrUnexpectedArgument,
+    negTest
       "Incomparable precedences"
       $(mkRelDir ".")
       $(mkRelFile "IncomparablePrecedences.juvix")
-      $ \case
-        ErrIncomparablePrecedences {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrIncomparablePrecedences,
+    negTest
       "Precedence inconsistency"
       $(mkRelDir ".")
       $(mkRelFile "PrecedenceInconsistency.juvix")
-      $ \case
-        ErrPrecedenceInconsistency {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrPrecedenceInconsistency,
+    negTest
       "Alias cycle"
       $(mkRelDir ".")
       $(mkRelFile "AliasCycle.juvix")
-      $ \case
-        ErrAliasCycle {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrAliasCycle,
+    negTest
       "Invalid range number in iterator definition"
       $(mkRelDir ".")
       $(mkRelFile "InvalidRangeNumber.juvix")
-      $ \case
-        ErrInvalidRangeNumber {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrInvalidRangeNumber,
+    negTest
       "Dangling double brace"
       $(mkRelDir "Internal")
       $(mkRelFile "DanglingDoubleBrace.juvix")
-      $ \case
-        ErrDanglingDoubleBrace {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrDanglingDoubleBrace,
+    negTest
       "Nested let open shadowing"
       $(mkRelDir ".")
       $(mkRelFile "LetOpen.juvix")
-      $ \case
-        ErrAmbiguousSym {} -> Nothing
-        _ -> wrongError,
-    NegTest
+      $ wantsError ErrAmbiguousSym,
+    negTest
       "Invalid default"
       $(mkRelDir ".")
       $(mkRelFile "InvalidDefault.juvix")
-      $ \case
-        ErrWrongDefaultValue {} -> Nothing
-        _ -> wrongError
+      $ wantsError ErrWrongDefaultValue
   ]
