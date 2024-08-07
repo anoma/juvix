9c01bf18b6c88edf34121275a9ca445f03869a1e
Paul Cadman <git@paulcadman.dev>
2024-08-07 20:11:23 +0100

    Fix CheckNoByteArray check

--- src/Juvix/Compiler/Tree/Transformation/CheckNoByteArray.hs
+++ src/Juvix/Compiler/Tree/Transformation/CheckNoByteArray.hs
@@ -5,20 +5,20 @@ import Juvix.Compiler.Tree.Error
 import Juvix.Compiler.Tree.Extra.Recursors
 import Juvix.Compiler.Tree.Transformation.Base
 
-checkNoAnoma :: forall r. (Member (Error TreeError) r) => InfoTable -> Sem r ()
-checkNoAnoma = walkT checkNode
+checkNoByteArray :: forall r. (Member (Error TreeError) r) => InfoTable -> Sem r ()
+checkNoByteArray = walkT checkNode
   where
     checkNode :: Symbol -> Node -> Sem r ()
     checkNode _ = \case
       ByteArray NodeByteArray {..} -> case _nodeByteArrayOpcode of
         OpByteArraySize -> unsupportedErr "OpByteArraySize"
-        OpByteArraYFromListUInt8 -> unsupportedErr "OpByteArrayFromListUInt8"
+        OpByteArrayFromListUInt8 -> unsupportedErr "OpByteArrayFromListUInt8"
         where
           unsupportedErr :: Text -> Sem r ()
           unsupportedErr opName =
             throw
               TreeError
                 { _treeErrorMsg = opName <> " is unsupported",
-                  _treeErrorLoc = _nodeAnomaInfo ^. nodeInfoLocation
+                  _treeErrorLoc = _nodeByteArrayInfo ^. nodeInfoLocation
                 }
       _ -> return ()
