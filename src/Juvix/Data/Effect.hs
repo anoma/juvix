7119d3929bf1b6140f0e4564ffd5180db074d65d
Jan Mas Rovira <janmasrovira@gmail.com>
2024-08-30 16:27:08 +0200

    Add `PartialDo` effect (#2978)
    This effect is a different name for [Effectful's
    `Fail`](https://hackage.haskell.org/package/effectful-core-2.3.1.0/docs/Effectful-Fail.html).
    It gives an instance of
    [`MonadFail`](https://hackage.haskell.org/package/base-4.18.1.0/docs/Control-Monad-Fail.html#t:MonadFail)
    to `Sem`

--- src/Juvix/Data/Effect.hs
+++ src/Juvix/Data/Effect.hs
@@ -1,5 +1,6 @@
 module Juvix.Data.Effect
   ( module Juvix.Data.Effect.Fail,
+    module Juvix.Data.Effect.PartialDo,
     module Juvix.Data.Effect.Files,
     module Juvix.Data.Effect.Cache,
     module Juvix.Data.Effect.NameIdGen,
@@ -18,5 +19,6 @@ import Juvix.Data.Effect.Forcing
 import Juvix.Data.Effect.Internet
 import Juvix.Data.Effect.Log
 import Juvix.Data.Effect.NameIdGen
+import Juvix.Data.Effect.PartialDo
 import Juvix.Data.Effect.TaggedLock
 import Juvix.Data.Effect.Visit
