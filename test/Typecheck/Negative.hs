470f282c37a01b634d8d55a0388f82b5b81a87c4
Jan Mas Rovira <janmasrovira@gmail.com>
2024-10-01 16:14:25 +0200

    fix rebase

--- test/Typecheck/Negative.hs
+++ test/Typecheck/Negative.hs
@@ -364,15 +364,15 @@ arityTests =
         _ -> wrongError,
     negTest "Evil: issue 2540" $(mkRelDir "Internal/Positivity") $(mkRelFile "Evil.juvix") $
       \case
-        ErrNonStrictlyPositive ErrTypeAsArgumentOfBoundVar {} -> Nothing
+        ErrNonStrictlyPositive {} -> Nothing
         _ -> wrongError,
     negTest "Evil: issue 2540 using Axiom" $(mkRelDir "Internal/Positivity") $(mkRelFile "EvilWithAxiom.juvix") $
       \case
-        ErrNonStrictlyPositive (ErrTypeAsArgumentOfBoundVar {}) -> Nothing
+        ErrNonStrictlyPositive {} -> Nothing
         _ -> wrongError,
     negTest "FreeT: issue 2540" $(mkRelDir "Internal/Positivity") $(mkRelFile "FreeT.juvix") $
       \case
-        ErrNonStrictlyPositive (ErrTypeAsArgumentOfBoundVar {}) -> Nothing
+        ErrNonStrictlyPositive {} -> Nothing
         _ -> wrongError
   ]
 
