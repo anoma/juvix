36e8dab87f1a848bf0dc3e86b4271a3cee4b8c2e
Paul Cadman <git@paulcadman.dev>
2024-10-15 18:34:44 +0100

    Update nock stdlib
    The nock stdlib code was obtained from anoma release v0.24.1
    
    https://github.com/anoma/anoma/blob/a20b5e78380b4dcf554fda6f4c75758cc1b70063/apps/anoma_lib/lib/nock.ex#L556
    
    The nock cells in StdlibFunction.hs were obtained in the urbit dojo
    using:
    
    https://github.com/anoma/anoma/blob/a20b5e78380b4dcf554fda6f4c75758cc1b70063/hoon/anoma.hoon

--- src/Juvix/Compiler/Nockma/StdlibFunction.hs
+++ src/Juvix/Compiler/Nockma/StdlibFunction.hs
@@ -11,37 +11,41 @@ import Juvix.Prelude.Base
 --      [9 20 0 15]
 stdlibPath :: StdlibFunction -> Term Natural
 stdlibPath = \case
-  StdlibDec -> [nock| [9 342 0 63] |]
-  StdlibAdd -> [nock| [9 20 0 63] |]
-  StdlibSub -> [nock| [9 47 0 63] |]
-  StdlibMul -> [nock| [9 4 0 63] |]
-  StdlibDiv -> [nock| [9 170 0 63] |]
-  StdlibMod -> [nock| [9 46 0 63] |]
-  StdlibLe -> [nock| [9 84 0 63] |]
-  StdlibLt -> [nock| [9 343 0 63] |]
-  StdlibPow2 -> [nock| [9 4 0 7] |]
-  StdlibEncode -> [nock| [9 22 0 3] |]
-  StdlibDecode -> [nock| [9 94 0 3] |]
-  StdlibVerifyDetached -> [nock| [9 22 0 1] |]
-  StdlibSign -> [nock| [9 10 0 1] |]
-  StdlibSignDetached -> [nock| [9 23 0 1] |]
-  StdlibVerify -> [nock| [9 4 0 1] |]
-  StdlibLengthList -> [nock| [9 1.406 0 31] |]
-  StdlibCurry -> [nock| [9 4 0 31] |]
+  StdlibDec -> [nock| [9 342 0 255] |]
+  StdlibAdd -> [nock| [9 20 0 255] |]
+  StdlibSub -> [nock| [9 47 0 255] |]
+  StdlibMul -> [nock| [9 4 0 255] |]
+  StdlibDiv -> [nock| [9 170 0 255] |]
+  StdlibMod -> [nock| [9 46 0 255] |]
+  StdlibLe -> [nock| [9 84 0 255] |]
+  StdlibLt -> [nock| [9 343 0 255] |]
+  -- pow2 is called bex in hoon
+  StdlibPow2 -> [nock| [9 4 0 31] |]
+  -- encode is called jam in hoon
+  StdlibEncode -> [nock| [9 22 0 15] |]
+  -- decode is called cue in hoon
+  StdlibDecode -> [nock| [9 94 0 15] |]
+  -- verifyDetached is called verify-detatched in hoon
+  StdlibVerifyDetached -> [nock| [9 22 0 7] |]
+  StdlibSign -> [nock| [9 10 0 7] |]
+  StdlibSignDetached -> [nock| [9 23 0 7] |]
+  StdlibVerify -> [nock| [9 4 0 7] |]
+  StdlibLengthList -> [nock| [9 1.406 0 127] |]
+  StdlibCurry -> [nock| [9 4 0 127] |]
   -- Obtained from the urbit dojo using:
   --
   -- =>  anoma  !=(~(met block 3))
   --
   -- The `3` here is because we want to treat each atom as sequences of 2^3
   -- bits, i.e bytes.
-  StdlibLengthBytes -> [nock| [8 [9 10 0 7] 9 190 10 [6 7 [0 3] 1 3] 0 2] |]
+  StdlibLengthBytes -> [nock| [8 [9 10 0 31] 9 190 10 [6 7 [0 3] 1 3] 0 2] |]
   -- Obtained from the urbit dojo using:
   --
   -- =>  anoma  !=(~(cat block 3))
   --
   -- The `3` here is because we want to treat each atom as sequences of 2^3
   -- bits, i.e bytes.
-  StdlibCatBytes -> [nock| [8 [9 10 0 7] 9 4 10 [6 7 [0 3] 1 3] 0 2] |]
+  StdlibCatBytes -> [nock| [8 [9 10 0 31] 9 4 10 [6 7 [0 3] 1 3] 0 2] |]
   -- Obtained from the urbit dojo using:
   --
   -- =>(anoma !=(|=([l=(list @)] (foldr l |=([fst=@ snd=@] (add (~(lsh block 3) 1 snd) fst))))))
@@ -53,7 +57,7 @@ stdlibPath = \case
               [1 0]
               [ 1
                 8
-                [9 46 0 127]
+                [9 46 0 511]
                 9
                 2
                 10
@@ -65,11 +69,11 @@ stdlibPath = \case
                   [1 0 0]
                   [ 1
                     8
-                    [9 20 0 1.023]
+                    [9 20 0 4.095]
                     9
                     2
                     10
-                    [6 [7 [0 3] 8 [8 [9 10 0 127] 9 90 10 [6 7 [0 3] 1 3] 0 2] 9 2 10 [6 [7 [0 3] 1 1] 0 29] 0 2] 0 28]
+                    [6 [7 [0 3] 8 [8 [9 10 0 511] 9 90 10 [6 7 [0 3] 1 3] 0 2] 9 2 10 [6 [7 [0 3] 1 1] 0 29] 0 2] 0 28]
                     0
                     2
                   ]
