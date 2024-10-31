b12cd943f2e26bdfdf449613ca488c9bae93ca35
Jan Mas Rovira <janmasrovira@gmail.com>
2024-10-31 19:10:29 +0100

    automatically get listenPort

--- src/Anoma/Effect/Base.hs
+++ src/Anoma/Effect/Base.hs
@@ -12,7 +12,7 @@ module Anoma.Effect.Base
     anomaClientHandle,
     anomaPath,
     runAnoma,
-    runAnomaTest,
+    -- runAnomaTest,
     module Anoma.Rpc.Base,
     module Juvix.Compiler.Nockma.Translation.FromTree,
   )
@@ -32,6 +32,8 @@ data AnomaProcesses = AnomaProcesses
     _anomaClientHandle :: ProcessHandle
   }
 
+newtype ListenPort = ListenPort Int
+
 data Anoma :: Effect where
   -- | Keep the node and client running
   GetAnomaProcesses :: Anoma m AnomaProcesses
@@ -48,9 +50,6 @@ newtype GrpcPort = GrpcPort {_grpcPort :: Int}
 makeLenses ''AnomaPath
 makeLenses ''GrpcPort
 
-listenPort :: Int
-listenPort = 50051
-
 relativeToAnomaDir :: (Members '[Reader AnomaPath] r) => Path Rel x -> Sem r (Path Abs x)
 relativeToAnomaDir p = do
   anoma <- asks (^. anomaPath)
@@ -58,18 +57,19 @@ relativeToAnomaDir p = do
 
 withSpawnAnomaClient ::
   (Members '[Process, Logger, EmbedIO, Reader AnomaPath, Reader GrpcPort, Error SimpleError] r) =>
-  (ProcessHandle -> Sem r a) ->
+  (Int -> ProcessHandle -> Sem r a) ->
   Sem r a
 withSpawnAnomaClient body = do
   cprocess <- mkProcess
   withCreateProcess cprocess $ \_stdin mstdout _stderr procHandle -> do
     let out = fromJust mstdout
     txt <- hGetLine out
-    case takeWhile (/= '.') (unpack txt) of
-      "Connected to node" -> do
+    case span (/= '.') (unpack txt) of
+      ("Connected to node", rest) -> do
+        let port = readJust (last (nonEmpty' (words rest)))
         logInfo "Anoma client successfully started"
-        logInfo (mkAnsiText ("Listening on port " <> annotate AnnImportant (pretty listenPort)))
-        body procHandle
+        logInfo (mkAnsiText ("Listening on port " <> annotate AnnImportant (pretty port)))
+        body port procHandle
       _ -> throw (SimpleError (mkAnsiText @Text "Something went wrong when starting the anoma client"))
   where
     mkProcess :: (Members '[Reader AnomaPath, Reader GrpcPort] r') => Sem r' CreateProcess
@@ -80,7 +80,7 @@ withSpawnAnomaClient body = do
         ( proc
             (toFilePath anomaClient)
             [ "--listen-port",
-              show listenPort,
+              "0",
               "--node-host",
               "localhost",
               "--node-port",
@@ -117,7 +117,7 @@ withSpawnAnomaNode body = withSystemTempFile "start.exs" $ \fp tmpHandle -> do
           }
 
 anomaRpc' ::
-  (Members '[Reader AnomaPath, Process, EmbedIO, Error SimpleError] r) =>
+  (Members '[Reader ListenPort, Reader AnomaPath, Process, EmbedIO, Error SimpleError] r) =>
   GrpcMethodUrl ->
   Value ->
   Sem r Value
@@ -134,9 +134,10 @@ anomaRpc' method payload = do
       Right r -> return r
       Left err -> throw (SimpleError (mkAnsiText err))
 
-grpcCliProcess :: (Members '[Reader AnomaPath] r) => GrpcMethodUrl -> Sem r CreateProcess
+grpcCliProcess :: (Members '[Reader ListenPort, Reader AnomaPath] r) => GrpcMethodUrl -> Sem r CreateProcess
 grpcCliProcess method = do
   importPath <- relativeToAnomaDir relProtoDir
+  ListenPort listenPort <- ask
   return
     ( proc
         "grpcurl"
@@ -156,17 +157,16 @@ grpcCliProcess method = do
       }
 
 -- | Assumes the node and client are already running
-runAnomaTest :: forall r a. (Members '[Logger, EmbedIO, Error SimpleError] r) => AnomaPath -> Sem (Anoma ': r) a -> Sem r a
-runAnomaTest anomapath body = runReader anomapath . runProcess $
-  (`interpret` inject body) $ \case
-    GetAnomaProcesses -> error "unsupported"
-    AnomaRpc method i -> anomaRpc' method i
-
+-- runAnomaTest :: forall r a. (Members '[Reader ListenPort, Logger, EmbedIO, Error SimpleError] r) => AnomaPath -> Sem (Anoma ': r) a -> Sem r a
+-- runAnomaTest anomapath body = runReader anomapath . runProcess $
+--   (`interpret` inject body) $ \case
+--     GetAnomaProcesses -> error "unsupported"
+--     AnomaRpc method i -> anomaRpc' method i
 runAnoma :: forall r a. (Members '[Logger, EmbedIO, Error SimpleError] r) => AnomaPath -> Sem (Anoma ': r) a -> Sem r a
 runAnoma anomapath body = runReader anomapath . runProcess $
   withSpawnAnomaNode $ \grpcport _nodeOut nodeH ->
     runReader (GrpcPort grpcport) $
-      withSpawnAnomaClient $ \clientH -> do
+      withSpawnAnomaClient $ \listenPort clientH -> runReader (ListenPort listenPort) $ do
         (`interpret` inject body) $ \case
           GetAnomaProcesses ->
             return
