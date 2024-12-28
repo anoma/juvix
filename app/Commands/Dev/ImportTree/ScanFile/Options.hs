d1260cf580d7a3f9562041c105a898207afb29b5
Jan Mas Rovira <janmasrovira@gmail.com>
2024-12-28 09:44:40 +0100

    check

--- app/Commands/Dev/ImportTree/ScanFile/Options.hs
+++ app/Commands/Dev/ImportTree/ScanFile/Options.hs
@@ -6,6 +6,7 @@ import Juvix.Compiler.Concrete.Translation.ImportScanner
 data ScanFileOptions = ScanFileOptions
   { _scanFileFile :: AppPath File,
     _scanFilePrintLoc :: Bool,
+    _scanFileCheck :: Bool,
     _scanFileStrategy :: ImportScanStrategy
   }
   deriving stock (Data)
@@ -20,5 +21,10 @@ parseScanFile = do
     switch
       ( long "print-loc"
           <> help "Print the location of each import"
+      )
+  _scanFileCheck <-
+    switch
+      ( long "check"
+          <> help "Checks that the rest of the backends coincide"
       )
   pure ScanFileOptions {..}
