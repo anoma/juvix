96111dd706d5aac285fab7d48ba9f2ce1e26a4a1
Jan Mas Rovira <janmasrovira@gmail.com>
2025-02-20 21:57:49 +0100

    migrate tests/Compilation/positive

--- migrate.fish
+++ migrate.fish
@@ -1,11 +1,12 @@
 #!/usr/bin/env fish
 
-cd ./tests/positive
+cd ./tests/Compilation/positive
 for dir in (fd -a -t f "Package.juvix" | xargs -I{} dirname "{}")
     set witness "$dir/.migrated"
     if test -e $witness
-        echo "Already processed: $dir"
+        # echo "Already processed: $dir"
     else
+        set --export JUVIX_TEST_PATH "other dep"
         echo "Processing $dir"
         if juvix format  $dir --migration export-constructors --in-place
             touch $witness
