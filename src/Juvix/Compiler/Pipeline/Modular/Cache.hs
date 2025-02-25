5aa6abec5c161f4c345cfb6e6f27865db034578c
Lukasz Czajka <lukasz@heliax.dev>
2025-02-25 11:29:54 +0100

    remove unused code

--- src/Juvix/Compiler/Pipeline/Modular/Cache.hs
+++
@@ -1,7 +0,0 @@
-module Juvix.Compiler.Pipeline.Modular.Cache where
-
-import Juvix.Compiler.Pipeline.Modular.Result
-import Juvix.Data.Effect.Cache
-import Juvix.Data.ModuleId
-
-type ModuleCache m = Cache ModuleId (PipelineResult m)
