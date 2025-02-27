16e1b1ef305d174a00b84543fd21a26982b2dbe5
Lukasz Czajka <lukasz@heliax.dev>
2025-02-27 17:22:16 +0100

    add Dump.hs

---
+++ src/Juvix/Compiler/Core/Extra/Dump.hs
@@ -0,0 +1,22 @@
+module Juvix.Compiler.Core.Extra.Dump
+  ( Dumper,
+    module Juvix.Compiler.Core.Extra.Dump,
+  )
+where
+
+import Juvix.Compiler.Core.Data.Module
+import Juvix.Compiler.Core.Options
+import Juvix.Compiler.Verification.Core.Dump qualified as Dump
+import Juvix.Compiler.Verification.Dumper (Dumper)
+import Juvix.Prelude
+
+dump ::
+  (Members '[Dumper, Reader CoreOptions] r) =>
+  Text ->
+  Module ->
+  Sem r Module
+dump phase md = do
+  v <- asks (^. optVerify)
+  if
+      | v -> Dump.dump' phase md
+      | otherwise -> return md
