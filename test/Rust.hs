b894df404918e4d02a97e5ca74d0ef720ca77c78
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2025-03-20 14:31:42 +0100

    Temporarily disable RISC0 in the CI (#3365)
    We will re-enable it once the RISC0 toolchain is more stable and we
    figure out how to reliably lock the versions.

--- test/Rust.hs
+++ test/Rust.hs
@@ -2,9 +2,8 @@ module Rust where
 
 import Base
 import Rust.Compilation qualified as Compilation
-import Rust.RiscZero qualified as RiscZero
 
 allTests :: IO TestTree
 allTests =
   sequentialTestGroup "Juvix to Rust tests" AllFinish
-    <$> sequence [Compilation.allTests, RiscZero.allTests]
+    <$> sequence [Compilation.allTests]
