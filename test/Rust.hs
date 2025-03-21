7204c14de3bcd5c13cfc051a22ed27e6e083cf0e
Lukasz Czajka <lukasz@heliax.dev>
2025-03-21 16:34:36 +0100

    re-enable risc0 tests

--- test/Rust.hs
+++ test/Rust.hs
@@ -2,8 +2,9 @@ module Rust where
 
 import Base
 import Rust.Compilation qualified as Compilation
+import Rust.RiscZero qualified as RiscZero
 
 allTests :: IO TestTree
 allTests =
   sequentialTestGroup "Juvix to Rust tests" AllFinish
-    <$> sequence [Compilation.allTests]
+    <$> sequence [Compilation.allTests, RiscZero.allTests]
