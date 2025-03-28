4db56f76c2f2109325739dc34b6ce5ba06e40e88
Jan Mas Rovira <janmasrovira@gmail.com>
2025-03-28 16:23:29 +0100

    add Noun representation

--- src/Juvix/Compiler/Nockma/Translation/FromTree.hs
+++ src/Juvix/Compiler/Nockma/Translation/FromTree.hs
@@ -75,9 +75,18 @@ data NockmaMemRepMaybeConstr
   | NockmaMemRepMaybeConstrJust
   deriving stock (Eq)
 
+-- | A type that has two constructors:
+-- 1st constructor has a single argument. This argument must be representable as an atom.
+-- 2nd constructor has *at least* two arguments
+data NockmaMemRepNounConstr
+  = NockmaMemRepAtom
+  | NockmaMemRepCell
+  deriving stock (Eq)
+
 data NockmaMemRep
   = NockmaMemRepConstr
   | NockmaMemRepTuple
+  | NockmaMemRepNoun NockmaMemRepNounConstr
   | NockmaMemRepList NockmaMemRepListConstr
   | NockmaMemRepMaybe NockmaMemRepMaybeConstr
 
@@ -469,38 +478,44 @@ compile = \case
             path :: Sem r Path
             path = do
               fr <- directRefPath _fieldRef
-              return $ case memrep of
-                NockmaMemRepConstr ->
-                  fr
-                    ++ constructorPath ConstructorArgs
-                    ++ indexStack argIx
-                NockmaMemRepTuple ->
-                  fr
-                    ++ indexTuple
-                      IndexTupleArgs
-                        { _indexTupleArgsLength = arity,
-                          _indexTupleArgsIndex = argIx
-                        }
-                NockmaMemRepList constr -> case constr of
-                  NockmaMemRepListConstrNil -> impossible
-                  NockmaMemRepListConstrCons ->
-                    fr
-                      ++ indexTuple
+              return $
+                fr
+                  ++ case memrep of
+                    NockmaMemRepConstr ->
+                      constructorPath ConstructorArgs
+                        ++ indexStack argIx
+                    NockmaMemRepTuple ->
+                      indexTuple
                         IndexTupleArgs
-                          { _indexTupleArgsLength = 2,
+                          { _indexTupleArgsLength = arity,
                             _indexTupleArgsIndex = argIx
                           }
-                NockmaMemRepMaybe constr -> case constr of
-                  NockmaMemRepMaybeConstrNothing -> impossible
-                  -- just x is represented as [nil x] so argument index is offset by 1.
-                  -- argIx will always be 0 because just has one argument
-                  NockmaMemRepMaybeConstrJust ->
-                    fr
-                      ++ indexTuple
-                        IndexTupleArgs
-                          { _indexTupleArgsLength = 2,
-                            _indexTupleArgsIndex = argIx + 1
-                          }
+                    NockmaMemRepList constr -> case constr of
+                      NockmaMemRepListConstrNil -> impossible
+                      NockmaMemRepListConstrCons ->
+                        indexTuple
+                          IndexTupleArgs
+                            { _indexTupleArgsLength = 2,
+                              _indexTupleArgsIndex = argIx
+                            }
+                    NockmaMemRepNoun n -> case n of
+                      NockmaMemRepAtom -> []
+                      NockmaMemRepCell ->
+                        indexTuple
+                          IndexTupleArgs
+                            { _indexTupleArgsLength = arity,
+                              _indexTupleArgsIndex = argIx
+                            }
+                    NockmaMemRepMaybe constr -> case constr of
+                      NockmaMemRepMaybeConstrNothing -> impossible
+                      -- just x is represented as [nil x] so argument index is offset by 1.
+                      -- argIx will always be 0 because just has one argument
+                      NockmaMemRepMaybeConstrJust ->
+                        indexTuple
+                          IndexTupleArgs
+                            { _indexTupleArgsLength = 2,
+                              _indexTupleArgsIndex = argIx + 1
+                            }
         (opAddress "constrRef") <$> path
       where
         goDirectRef :: Tree.DirectRef -> Sem r (Term Natural)
@@ -980,8 +995,7 @@ goConstructor mr t args = assert (all isCell args) $
         makeConstructor $ \case
           ConstructorTag -> OpQuote # (fromIntegral (tag ^. Tree.tagUserWord) :: Natural)
           ConstructorArgs -> remakeList args
-      NockmaMemRepTuple ->
-        foldTerms (nonEmpty' args)
+      NockmaMemRepTuple -> foldTerms (nonEmpty' args)
       NockmaMemRepList constr -> case constr of
         NockmaMemRepListConstrNil
           | null args -> remakeList []
@@ -989,6 +1003,11 @@ goConstructor mr t args = assert (all isCell args) $
         NockmaMemRepListConstrCons -> case args of
           [l, r] -> TCell l r
           _ -> impossible
+      NockmaMemRepNoun constr -> case constr of
+        NockmaMemRepAtom -> case args of
+          [arg@(TermAtom _)] -> arg
+          _ -> impossible
+        NockmaMemRepCell -> foldTerms (nonEmpty' args)
       NockmaMemRepMaybe constr -> case constr of
         NockmaMemRepMaybeConstrNothing
           | null args -> (OpQuote # nockNilTagged "maybe-constr-nothing")
@@ -1200,7 +1219,7 @@ constructorTagToTerm = \case
   Tree.UserTag t -> OpQuote # toNock (fromIntegral (t ^. Tree.tagUserWord) :: Natural)
   Tree.BuiltinTag b -> builtinTagToTerm (nockmaBuiltinTag b)
 
--- Creates a case command from the reference `ref` to the compiled value and the
+-- | Creates a case command from the reference `ref` to the compiled value and the
 -- compiled branches.
 caseCmd ::
   forall r.
@@ -1257,6 +1276,15 @@ caseCmd ref defaultBranch = \case
           _ -> impossible
       Tree.BuiltinTag {} -> impossible
 
+    asNockmaMemRepNounConstr :: Tree.Tag -> Sem r NockmaMemRepNounConstr
+    asNockmaMemRepNounConstr tag = case tag of
+      Tree.UserTag {} -> do
+        rep <- getConstructorMemRep tag
+        case rep of
+          NockmaMemRepNoun constr -> return constr
+          _ -> impossible
+      Tree.BuiltinTag {} -> impossible
+
     asNockmaMemRepMaybeConstr :: Tree.Tag -> Sem r NockmaMemRepMaybeConstr
     asNockmaMemRepMaybeConstr tag = case tag of
       Tree.UserTag {} -> do
@@ -1296,6 +1324,18 @@ caseCmd ref defaultBranch = \case
         NockmaMemRepListConstrNil -> branch cond otherBranch b
       where
         f :: (NockmaMemRepListConstr, Term Natural) -> Maybe (Term Natural)
+        f (c', br) = guard (c /= c') $> br
+
+    goRepNoun :: NonEmpty (NockmaMemRepNounConstr, Term Natural) -> Sem r (Term Natural)
+    goRepNoun ((c, b) :| bs) = do
+      arg <- addressTempRef ref
+      let cond = OpIsCell # arg
+          otherBranch = fromMaybe crash (firstJust f bs <|> defaultBranch)
+      return $ case c of
+        NockmaMemRepCell -> branch cond b otherBranch
+        NockmaMemRepAtom -> branch cond otherBranch b
+      where
+        f :: (NockmaMemRepNounConstr, Term Natural) -> Maybe (Term Natural)
         f (c', br) = guard (c /= c') $> br
 
     goRepMaybe :: NonEmpty (NockmaMemRepMaybeConstr, Term Natural) -> Sem r (Term Natural)
