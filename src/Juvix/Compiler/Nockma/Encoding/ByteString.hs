520d70a75a2eb6df34a1b012f13384ce9cd42ed3
Jan Mas Rovira <janmasrovira@gmail.com>
2025-04-05 12:14:38 +0200

    improve Natural -> ByteString

--- src/Juvix/Compiler/Nockma/Encoding/ByteString.hs
+++ src/Juvix/Compiler/Nockma/Encoding/ByteString.hs
@@ -1,17 +1,18 @@
+{-# LANGUAGE MagicHash #-}
+
 module Juvix.Compiler.Nockma.Encoding.ByteString where
 
 import Crypto.Hash.SHA256 qualified as SHA256
 import Data.Bit (Bit)
 import Data.Bit qualified as Bit
-import Data.Bits
 import Data.ByteString qualified as BS
 import Data.ByteString.Base64 qualified as Base64
-import Data.ByteString.Builder qualified as BS
 import Juvix.Compiler.Nockma.Encoding.Base
 import Juvix.Compiler.Nockma.Encoding.Effect.BitReader
 import Juvix.Compiler.Nockma.Encoding.Effect.BitWriter
 import Juvix.Compiler.Nockma.Language
 import Juvix.Prelude.Base
+import Juvix.Prelude.Bytes
 
 -- | Encode an atom to little-endian bytes
 atomToByteString :: (NockNatural a, Member (Error (ErrNockNatural a)) r) => Atom a -> Sem r ByteString
@@ -29,9 +30,6 @@ byteStringToAtom = fmap mkEmptyAtom . fromNatural . byteStringToNatural
 
 byteStringToNatural :: ByteString -> Natural
 byteStringToNatural = fromInteger . byteStringToIntegerLE
-
-naturalToByteString :: Natural -> ByteString
-naturalToByteString = naturalToByteStringLE
 
 naturalToBase64 :: Natural -> Text
 naturalToBase64 = decodeUtf8 . Base64.encode . naturalToByteString
@@ -58,18 +56,6 @@ byteStringToIntegerLEChunked = foldr' go 0 . map (first byteStringChunkToInteger
     byteStringChunkToInteger :: ByteString -> Integer
     byteStringChunkToInteger = BS.foldr' (\b acc -> acc `shiftL` 8 .|. fromIntegral b) 0
 
--- | TODO: this is quadratic (`shiftR` is O(n))
-naturalToByteStringLE :: Natural -> ByteString
-naturalToByteStringLE = BS.toStrict . BS.toLazyByteString . go
-  where
-    go :: Natural -> BS.Builder
-    go = \case
-      0 -> mempty
-      n -> BS.word8 (fromIntegral n) <> go (n `shiftR` 8)
-
-naturalToByteStringLELen :: Int -> Natural -> ByteString
-naturalToByteStringLELen len = padByteString len . naturalToByteStringLE
-
 textToNatural :: Text -> Natural
 textToNatural = byteStringToNatural . encodeUtf8
 
@@ -89,12 +75,6 @@ mkEmptyAtom x =
     { _atomInfo = emptyAtomInfo,
       _atom = x
     }
-
--- | Pad a ByteString with zeros up to a specified length
-padByteString :: Int -> ByteString -> ByteString
-padByteString n bs
-  | BS.length bs >= n = bs
-  | otherwise = BS.append bs (BS.replicate (n - BS.length bs) 0)
 
 vectorBitsToInteger :: Bit.Vector Bit -> Integer
 vectorBitsToInteger = byteStringToIntegerLEChunked . vectorBitsToByteString
