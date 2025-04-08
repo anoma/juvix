58af0e1c8b961ab0e6fca8f1cfdf0c27f0c553da
Jan Mas Rovira <janmasrovira@gmail.com>
2025-04-08 19:38:07 +0200

    Improve efficiency of `Natural <-> ByteString` (#3386)
    I've improved efficiency of encoding/decoding Natural to Bytestring.
    
    With *very* big numbers, the gains are dramatic. With smaller numbers,
    the gains are more modest but still relevant.
    
    # Benchmarks
    ## 1,000,000 digits
    ### `Natural -> ByteString`
    ```
          Shift based:    OK
            1.697 s ±  13 ms
          Word64 based:   OK
            327  μs ±  21 μs
          Word8 based:    OK
            21.7 ms ± 1.2 ms
          Cereal library: OK
            1.874 s ± 3.6 ms
    ```
    ### `ByteString -> Natural`
    ```
        ByteString -> Natural
          Shift based:    OK
            13.7 ms ± 889 μs
          Word based:     OK
            671  μs ±  30 μs
    ```
    ## 500 digits
    ### `Natural -> ByteString`
    ```
        Natural -> ByteString
          Shift based:    OK
            4.99 μs ± 456 ns
          Word64 based:   OK
            267  ns ±  16 ns
          Word8 based:    OK
            1.86 μs ± 170 ns
          Cereal library: OK
            9.72 μs ± 771 ns
    ```
    ### `ByteString -> Natural`
    ```
        ByteString -> Natural
          Old:            OK
            4.31 μs ± 411 ns
          New:            OK
            219  ns ±  21 ns
    ```
    
    
    
    Unfortunately, I haven't measured any gains by measuring the execution
    time of the nock pipeline.

--- src/Juvix/Compiler/Nockma/Encoding/ByteString.hs
+++ src/Juvix/Compiler/Nockma/Encoding/ByteString.hs
@@ -3,15 +3,13 @@ module Juvix.Compiler.Nockma.Encoding.ByteString where
 import Crypto.Hash.SHA256 qualified as SHA256
 import Data.Bit (Bit)
 import Data.Bit qualified as Bit
-import Data.Bits
 import Data.ByteString qualified as BS
-import Data.ByteString.Base64 qualified as Base64
-import Data.ByteString.Builder qualified as BS
 import Juvix.Compiler.Nockma.Encoding.Base
 import Juvix.Compiler.Nockma.Encoding.Effect.BitReader
 import Juvix.Compiler.Nockma.Encoding.Effect.BitWriter
 import Juvix.Compiler.Nockma.Language
 import Juvix.Prelude.Base
+import Juvix.Prelude.Bytes
 
 -- | Encode an atom to little-endian bytes
 atomToByteString :: (NockNatural a, Member (Error (ErrNockNatural a)) r) => Atom a -> Sem r ByteString
@@ -26,49 +24,6 @@ sha256Atom = fmap sha256Natural . nockNatural
 
 byteStringToAtom :: forall a r. (NockNatural a, Member (Error (ErrNockNatural a)) r) => ByteString -> Sem r (Atom a)
 byteStringToAtom = fmap mkEmptyAtom . fromNatural . byteStringToNatural
-
-byteStringToNatural :: ByteString -> Natural
-byteStringToNatural = fromInteger . byteStringToIntegerLE
-
-naturalToByteString :: Natural -> ByteString
-naturalToByteString = naturalToByteStringLE
-
-naturalToBase64 :: Natural -> Text
-naturalToBase64 = decodeUtf8 . Base64.encode . naturalToByteString
-
-byteStringToIntegerLE :: ByteString -> Integer
-byteStringToIntegerLE = byteStringToIntegerLEChunked
-
-byteStringToIntegerLEChunked :: ByteString -> Integer
-byteStringToIntegerLEChunked = foldr' go 0 . map (first byteStringChunkToInteger) . chunkByteString
-  where
-    chunkSize :: Int
-    chunkSize = 1024
-
-    go :: (Integer, Int) -> Integer -> Integer
-    go (i, size) acc = acc `shiftL` (8 * size) .|. i
-
-    chunkByteString :: ByteString -> [(ByteString, Int)]
-    chunkByteString bs
-      | BS.null bs = []
-      | otherwise =
-          let (chunk, rest) = BS.splitAt chunkSize bs
-           in (chunk, BS.length chunk) : chunkByteString rest
-
-    byteStringChunkToInteger :: ByteString -> Integer
-    byteStringChunkToInteger = BS.foldr' (\b acc -> acc `shiftL` 8 .|. fromIntegral b) 0
-
--- | TODO: this is quadratic (`shiftR` is O(n))
-naturalToByteStringLE :: Natural -> ByteString
-naturalToByteStringLE = BS.toStrict . BS.toLazyByteString . go
-  where
-    go :: Natural -> BS.Builder
-    go = \case
-      0 -> mempty
-      n -> BS.word8 (fromIntegral n) <> go (n `shiftR` 8)
-
-naturalToByteStringLELen :: Int -> Natural -> ByteString
-naturalToByteStringLELen len = padByteString len . naturalToByteStringLE
 
 textToNatural :: Text -> Natural
 textToNatural = byteStringToNatural . encodeUtf8
@@ -89,12 +44,6 @@ mkEmptyAtom x =
     { _atomInfo = emptyAtomInfo,
       _atom = x
     }
-
--- | Pad a ByteString with zeros up to a specified length
-padByteString :: Int -> ByteString -> ByteString
-padByteString n bs
-  | BS.length bs >= n = bs
-  | otherwise = BS.append bs (BS.replicate (n - BS.length bs) 0)
 
 vectorBitsToInteger :: Bit.Vector Bit -> Integer
 vectorBitsToInteger = byteStringToIntegerLEChunked . vectorBitsToByteString
