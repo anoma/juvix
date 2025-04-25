5359cc34674848ae337e004d7e9621ebdc928a61
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-04-25 19:21:58 +0300

    chore: fix incorrect indentation with safer tab-based formatting

--- scripts/migrate-juvix-yaml.sh
+++ scripts/migrate-juvix-yaml.sh
@@ -2,7 +2,7 @@
 
 # Projects must be migrated depth-first because Package.juvix is searched before juvix.yaml
 # (so Package.juvix in parent directories are used before juvix.yaml in the current directory)
-find . -type f -name "juvix.yaml" | awk -F'/' '{print NF-1 " " $0}' | sort -nr | cut -d' ' -f2- | while IFS= read -r file; do
+find . -type f -name "juvix.yaml" | awk -F'/' '{print NF-1 "\t" $0}' | sort -nr | cut -f2- | while IFS= read -r file; do
     dir=$(dirname "$file")
     echo "migrating: $dir"
     (cd "$dir" && juvix dev migrate-juvix-yaml)
