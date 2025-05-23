a5ad4e0429dbe74f083cc8c0bd9f93ee7e10b875
Lukasz Czajka <lukasz@heliax.dev>
2025-05-23 19:26:45 +0200

    remove inductive params

--- src/Juvix/Compiler/Core/Transformation/RemoveInductiveParams.hs
+++ src/Juvix/Compiler/Core/Transformation/RemoveInductiveParams.hs
@@ -4,54 +4,46 @@ module Juvix.Compiler.Core.Transformation.RemoveInductiveParams
   )
 where
 
-import Juvix.Compiler.Core.Data.BinderList qualified as BL
 import Juvix.Compiler.Core.Extra
 import Juvix.Compiler.Core.Transformation.Base
 
 convertNode :: Module -> Node -> Node
-convertNode md = convert mempty
+convertNode md = rmap go
   where
-    convert :: BinderList Binder -> Node -> Node
-    convert vars = dmapLR' (vars, go)
-
-    go :: BinderList Binder -> Node -> Recur
-    go vars node = case node of
-      NTyp typeConstr -> End . NTyp $ set typeConstrArgs [] typeConstr
+    go :: ([BinderChange] -> Node -> Node) -> Node -> Node
+    go recur node = case node of
+      NTyp typeConstr -> NTyp $ set typeConstrArgs [] typeConstr
       NCtr Constr {..} ->
         let ci = lookupConstructorInfo md _constrTag
             ii = lookupInductiveInfo md (ci ^. constructorInductive)
             nParams = length (ii ^. inductiveParams)
-         in End $
-              mkConstr _constrInfo _constrTag (map (convert vars) (drop nParams _constrArgs))
+         in mkConstr _constrInfo _constrTag (map (go recur) (drop nParams _constrArgs))
       NCase Case {..} ->
-        End $ mkCase _caseInfo _caseInductive (convert vars _caseValue) (map convertBranch _caseBranches) (fmap (convert vars) _caseDefault)
+        mkCase _caseInfo _caseInductive (go recur _caseValue) (map convertBranch _caseBranches) (fmap (go recur) _caseDefault)
         where
-          -- ii = lookupInductiveInfo md _caseInductive
-          -- nParams = length (ii ^. inductiveParams)
+          ii = lookupInductiveInfo md _caseInductive
+          nParams = length (ii ^. inductiveParams)
 
           convertBranch :: CaseBranch -> CaseBranch
           convertBranch br@CaseBranch {..} =
             let tyargs = typeArgs (lookupConstructorInfo md _caseBranchTag ^. constructorType)
                 binders = zipWith (\b ty -> if isDynamic (b ^. binderType) && isTypeConstr md ty then set binderType ty b else b) _caseBranchBinders (tyargs ++ repeat mkDynamic')
-                binders' = filterBinders vars binders
-                body' =
-                  convert
-                    (BL.prependRev binders vars)
-                    _caseBranchBody
+                bcs = map (\b -> mkBCRemove b mkDynamic') $ take nParams binders
+                (binders', bcs') = filterBinders bcs (drop nParams binders)
+                body' = go (recur . (bcs' ++)) _caseBranchBody
              in br
                   { _caseBranchBinders = binders',
                     _caseBranchBindersNum = length binders',
                     _caseBranchBody = body'
                   }
 
-          filterBinders :: BinderList Binder -> [Binder] -> [Binder]
-          filterBinders vars' = \case
-            [] -> []
-            b : bs
-              | isTypeConstr md (b ^. binderType) ->
-                  filterBinders (BL.cons b vars') bs
-              | otherwise -> over binderType (convert vars') b : filterBinders (BL.cons b vars') bs
-      _ -> Recur node
+          filterBinders :: [BinderChange] -> [Binder] -> ([Binder], [BinderChange])
+          filterBinders bcs = \case
+            [] -> ([], bcs)
+            b : bs -> (over binderType (go (recur . (bcs ++))) b : bs', bcs')
+              where
+                (bs', bcs') = filterBinders (bcs ++ [BCKeep b]) bs
+      _ -> recur [] node
 
 convertConstructor :: Module -> ConstructorInfo -> ConstructorInfo
 convertConstructor md ci =
@@ -60,17 +52,19 @@ convertConstructor md ci =
       _constructorArgsNum = length (typeArgs ty')
     }
   where
-    ty' = convertNode md (ci ^. constructorType)
+    ii = lookupInductiveInfo md (ci ^. constructorInductive)
+    nParams = length (ii ^. inductiveParams)
+    tyargs' = drop nParams (typeArgs (ci ^. constructorType))
+    ty' = convertNode md (mkPis' tyargs' (typeTarget (ci ^. constructorType)))
 
 convertInductive :: Module -> InductiveInfo -> InductiveInfo
 convertInductive md ii =
   ii
     { _inductiveKind = ty',
-      _inductiveParams = map (over paramKind (convertNode md) . fst) $ filter (not . isTypeConstr md . snd) (zipExact (ii ^. inductiveParams) tyargs)
+      _inductiveParams = []
     }
   where
-    tyargs = typeArgs (ii ^. inductiveKind)
-    ty' = convertNode md (ii ^. inductiveKind)
+    ty' = convertNode md (typeTarget (ii ^. inductiveKind))
 
 -- | Removes parameters of inductive types from constructors.
 removeInductiveParams :: Module -> Module
