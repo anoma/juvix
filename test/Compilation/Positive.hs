b57bb896f416405e6340cf9fc0ba340eb2565318
Lukasz Czajka <lukasz@heliax.dev>
2025-05-26 20:12:45 +0200

    enable test

--- test/Compilation/Positive.hs
+++ test/Compilation/Positive.hs
@@ -535,5 +535,10 @@ tests =
         "Test092: Type level naturals"
         $(mkRelDir ".")
         $(mkRelFile "test092.juvix")
-        $(mkRelFile "out/test092.out")
+        $(mkRelFile "out/test092.out"),
+      posTest
+        "Test093: Pattern matching with indexed types"
+        $(mkRelDir ".")
+        $(mkRelFile "test093.juvix")
+        $(mkRelFile "out/test093.out")
     ]
